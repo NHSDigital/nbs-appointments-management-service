3dfb02fe6be3c11b75ef6010ec5cf28d1a9c1cf1
Joe Farina <joe.farina@airelogic.com>
2024-06-05 16:34:19 +0100

    standardises styling

--- src/client/src/Views/DailyBookings.tsx
+++ src/client/src/Views/DailyBookings.tsx
@@ -15,7 +15,7 @@ type DailyBookingsProps = {
 export const DailyBookingsCtx = () => {
     const { siteConfig } = useSiteContext();
     const { getBookings, setBookingStatus } = useBookingService();
-    
+
     return <DailyBookings siteConfig={siteConfig!} getBookings={getBookings} setBookingStatus={setBookingStatus} />
 }
 
@@ -46,12 +46,12 @@ export const DailyBookings = ({ siteConfig, getBookings, setBookingStatus }: Dai
 
     const toggleCheckedIn = (reference: string) => {
         const booking = bookingsList?.find(b => b.reference === reference);
-        const newStatus = booking?.outcome === "CheckedIn" ? "Waiting": "CheckedIn";
+        const newStatus = booking?.outcome === "CheckedIn" ? "Waiting" : "CheckedIn";
         setStatus("loading");
         setBookingStatus(siteConfig.siteId, reference, newStatus)
             .then(() => {
                 const bookings = bookingsList!.map(b => {
-                    if(b.reference === reference){
+                    if (b.reference === reference) {
                         b.outcome = newStatus;
                     }
                     return b;
@@ -82,27 +82,27 @@ export const DailyBookings = ({ siteConfig, getBookings, setBookingStatus }: Dai
             // sort by time
             return a.from.localeCompare(b.from);
         })
-        .filter((appt) => {
-            if (filterTerm) {
-            // Non-numeric values match against a combined name string so "John Smith" matches "John Smith" or "Smith, John".
-            // Strips hyphens from input and reference so they match, but still works for hyphenated names
-            const noHyphenTerm = filterTerm.replaceAll("-", "");
-            return isNaN(Number(noHyphenTerm))
-                ? filterTerm.toLowerCase()
-                    .replaceAll(/[^a-zA-Z '-]+/g, "")
-                    .split(" ")
-                    .map((subterm) => {
-                    return (
-                        appt.attendeeDetails.firstName.toLowerCase() +
-                        " " +
-                        appt.attendeeDetails.lastName.toLowerCase()
-                    ).includes(subterm);
-                    })
-                    .every((x) => x)
-                : appt.reference.replaceAll("-", "").includes(noHyphenTerm);
-            }
-            return true;
-        });
+            .filter((appt) => {
+                if (filterTerm) {
+                    // Non-numeric values match against a combined name string so "John Smith" matches "John Smith" or "Smith, John".
+                    // Strips hyphens from input and reference so they match, but still works for hyphenated names
+                    const noHyphenTerm = filterTerm.replaceAll("-", "");
+                    return isNaN(Number(noHyphenTerm))
+                        ? filterTerm.toLowerCase()
+                            .replaceAll(/[^a-zA-Z '-]+/g, "")
+                            .split(" ")
+                            .map((subterm) => {
+                                return (
+                                    appt.attendeeDetails.firstName.toLowerCase() +
+                                    " " +
+                                    appt.attendeeDetails.lastName.toLowerCase()
+                                ).includes(subterm);
+                            })
+                            .every((x) => x)
+                        : appt.reference.replaceAll("-", "").includes(noHyphenTerm);
+                }
+                return true;
+            });
     }, [filterTerm, bookingsList]);
 
     return (
@@ -134,39 +134,35 @@ export const DailyBookings = ({ siteConfig, getBookings, setBookingStatus }: Dai
             <table className="nhsuk-table">
                 <caption className="nhsuk-table__caption">
                     {dayjs(currentDay).format("DD/MM/YYYY - dddd")}
-                    <div className="flex-row-start-nowrap" style={{marginLeft: "-14px"}}>
+                    <div className="nhsuk-navigation">
                         <div className="nhsuk-back-link">
-                            <a className="nhsuk-back-link__link" aria-label="previous day" href="#!" onClick={gotoPreviousDay}>
-                            <svg viewBox="0 0 32 32" width="24" height="24" version="1.1" xmlns="http://www.w3.org/2000/svg" style={{float: "left"}}>
-                                    <g transform="translate(-423.000000, -1193.000000)" fill="#0066cc">
-                                        <path d="M428.115,1209 L437.371,1200.6 C438.202,1199.77 438.202,1198.43 437.371,1197.6 C436.541,1196.76 435.194,1196.76 434.363,1197.6 L423.596,1207.36 C423.146,1207.81 422.948,1208.41 422.985,1209 C422.948,1209.59 423.146,1210.19 423.596,1210.64 L434.363,1220.4 C435.194,1221.24 436.541,1221.24 437.371,1220.4 C438.202,1219.57 438.202,1218.23 437.371,1217.4 L428.115,1209"></path>
-                                    </g>
-                            </svg>
+                            <button className="nhsuk-back-link__link" type="button" aria-label="previous day" onClick={gotoPreviousDay}>
+                                <svg className="nhsuk-icon nhsuk-icon__chevron-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" height="24" width="24">
+                                    <path d="M8.5 12c0-.3.1-.5.3-.7l5-5c.4-.4 1-.4 1.4 0s.4 1 0 1.4L10.9 12l4.3 4.3c.4.4.4 1 0 1.4s-1 .4-1.4 0l-5-5c-.2-.2-.3-.4-.3-.7z"></path>
+                                </svg>
                                 Previous day
-                            </a>
+                            </button>
                         </div>
                         <div className="nhsuk-back-link">
-                            <a className="nhsuk-back-link__link" aria-label="today" href="#!" onClick={gotoToday}>
+                            <button className="nhsuk-back-link__link" type="button" aria-label="today" onClick={gotoToday}>
                                 Today
-                            </a>
+                            </button>
                         </div>
                         <div className="nhsuk-back-link">
-                        <a className="nhsuk-back-link__link" aria-label="next day" href="#!" onClick={gotoNextDay}>
-                            Next day
-                            <svg viewBox="0 0 32 32" width="24" height="24" style={{float: "right", marginLeft: "8px"}} version="1.1" xmlns="http://www.w3.org/2000/svg">
-                                <g transform="translate(-474.000000, -1229.000000) scale(1,1.03)" fill="#0066cc">
-                                    <path d="M488.404,1207.36 L477.637,1197.6 C476.806,1196.76 475.459,1196.76 474.629,1197.6 C473.798,1198.43 473.798,1199.77 474.629,1200.6 L483.885,1209 L474.629,1217.4 C473.798,1218.23 473.798,1219.57 474.629,1220.4 C475.459,1221.24 476.806,1221.24 477.637,1220.4 L488.404,1210.64 C488.854,1210.19 489.052,1209.59 489.015,1209 C489.052,1208.41 488.854,1207.81 488.404,1207.36"></path>
-                                    </g>
-                            </svg>
-                            </a>
+                            <button className="nhsuk-back-link__link" type="button" aria-label="previous day" onClick={gotoNextDay}>
+                                Next day
+                                <svg className="nhsuk-icon nhsuk-icon__chevron-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" height="24" width="24">
+                                    <path d="M8.5 12c0-.3.1-.5.3-.7l5-5c.4-.4 1-.4 1.4 0s.4 1 0 1.4L10.9 12l4.3 4.3c.4.4.4 1 0 1.4s-1 .4-1.4 0l-5-5c-.2-.2-.3-.4-.3-.7z"></path>
+                                </svg>
+                            </button>
                         </div>
                     </div>
                     <div className="nhsuk-hint">
                         Search by name or booking reference
                     </div>
                     <div className="nhsuk-form-group">
-                        <input type="text" className="nhsuk-input nhsuk-u-width-one-quarter" aria-label="Filter bookings list"
-                            value={filterTerm} onChange={e => setFilterTerm(e.target.value)}/>
+                        <input type="text" className="nhsuk-input nhsuk-input--width-20" aria-label="Filter bookings list"
+                            value={filterTerm} onChange={e => setFilterTerm(e.target.value)} />
                     </div>
                 </caption>
                 {filteredList?.length ?
@@ -216,7 +212,7 @@ export const DailyBookings = ({ siteConfig, getBookings, setBookingStatus }: Dai
                                     <div className="nhsuk-checkboxes__item">
                                         <input className="nhsuk-checkboxes__input" type="checkbox" id={booking.reference}
                                             value={booking.outcome ?? ""}
-                                            onChange={() => {toggleCheckedIn(booking.reference)}}
+                                            onChange={() => { toggleCheckedIn(booking.reference) }}
                                             checked={booking.outcome === "CheckedIn"} />
                                         <label className="nhsuk-label nhsuk-checkboxes__label" htmlFor={booking.reference}>
                                         </label>

54bda8de60678f2d7bbf7bdac94b3d36191d8ded
Vincent Crowe <vincent.crowe1@nhs.net>
2024-07-04 15:49:18 +0100

    fixed issue with qs being appended to copy of body

--- src/api/Nhs.Appointments.Api/Auth/RequestSigner.cs
+++ src/api/Nhs.Appointments.Api/Auth/RequestSigner.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.Functions.Worker.Http;
+﻿using Microsoft.Azure.Functions.Worker.Http;
 using Polly;
 using System;
 using System.Collections.Specialized;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Security.Cryptography;
@@ -19,16 +17,10 @@ namespace Nhs.Appointments.Api.Auth
         {            
             string method = requestData.Method;
             string path = requestData.Url.AbsolutePath;
-            string content = await requestData.ReadAsStringAsync();
+            string content = await requestData.ReadBodyAsStringAndLeaveIntactAsync();
             content += GetCanonicalQueryParameters(requestData.Url.ParseQueryString());
             var contentBytes = Encoding.UTF8.GetBytes(content);
-            var copiedStream = new MemoryStream(contentBytes);
-
-            // Reading from the requestData means that other parts of the processing chain cannot, hence we have to copy it and use reflection to reset it. 
-            var httpRequestField = requestData.GetType().GetField("_httpRequest", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-            var httpRequest = httpRequestField.GetValue(requestData) as HttpRequest;
-            httpRequest.Body = copiedStream;
-            
+                        
             using var hash = SHA256.Create();
             var hashedContentBytes = hash.ComputeHash(contentBytes);
             var hashedContentBase64 = Convert.ToBase64String(hashedContentBytes);

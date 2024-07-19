3bc3efc1eaea816a4356492dec5d5312c8d255a7
Kim Crowe <kim.crowe4@nhs.net>
2024-07-19 13:48:25 +0100

    Add request insepctor for sites in scope properties

--- src/api/Nhs.Appointments.Api/Validators/SetUserRoleValidator.cs
+++ src/api/Nhs.Appointments.Api/Validators/SetUserRoleValidator.cs
@@ -7,11 +7,13 @@ public class SetUserRoleValidator : AbstractValidator<SetUserRolesRequest>
 {
     public SetUserRoleValidator()
     {
-        RuleFor(x => x.Scope).NotEmpty().WithMessage("Provide a valid scope - site:<siteId>");
-        RuleFor(x => x.User)
+        RuleFor(x => x.Scope).Cascade(CascadeMode.Stop)
+            .NotEmpty().WithMessage("Provide a valid scope - 'site:<siteId>'")
+            .Matches(@"site:[A-Za-z0-9]+").WithMessage("Provide a valid scope - 'site:<siteId>'");
+        RuleFor(x => x.User).Cascade(CascadeMode.Stop)
             .NotEmpty().WithMessage("Provide a valid email address")
             .Matches(@"[\w-\.]+@([\w-]+\.)+[\w-]{2,4}").WithMessage("Provide a valid email address");
         RuleFor(x => x.Roles).NotEmpty().WithMessage("One or more roles must be provided");
-        RuleForEach(x => x.Roles).NotEmpty().WithMessage("All provided roles must be valid");
+        RuleForEach(x => x.Roles).NotEmpty().WithMessage("Provide at least one valid role");
     }
 }

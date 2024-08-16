972483d00ff7b7e48d3e9ad5ac915be457110e5c
sambiramairelogic <samuel.biram@airelogic.com>
2024-08-16 13:59:15 +0100

    Split svgs into separate files

---
+++ src/new-client/src/app/lib/components/nhs-icon.tsx
@@ -0,0 +1,27 @@
+const NhsIcon = () => {
+  return (
+    <svg
+      className="nhsuk-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      width="27"
+      height="27"
+      aria-hidden="true"
+      focusable="false"
+    >
+      <circle cx="13.333" cy="13.333" r="13.333" fill="" />
+      <g
+        data-name="Group 1"
+        fill="none"
+        stroke="#fff"
+        strokeLinecap="round"
+        strokeMiterlimit="10"
+        strokeWidth="2.667"
+      >
+        <path d="M15.438 13l-3.771 3.771" />
+        <path data-name="Path" d="M11.667 9.229L15.438 13" />
+      </g>
+    </svg>
+  );
+};
+
+export default NhsIcon;
--- src/new-client/src/app/lib/components/nhs-nav-card.tsx
+++ src/new-client/src/app/lib/components/nhs-nav-card.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import NhsIcon from '@components/nhs-icon';
 
 type Props = {
   href: string;
@@ -16,28 +17,7 @@ const NhsNavCard = ({ href, title, description }: Props) => {
           </Link>
         </h2>
         <p className="nhsuk-card__description">{description}</p>
-
-        <svg
-          className="nhsuk-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="27"
-          height="27"
-          aria-hidden="true"
-          focusable="false"
-        >
-          <circle cx="13.333" cy="13.333" r="13.333" fill="" />
-          <g
-            data-name="Group 1"
-            fill="none"
-            stroke="#fff"
-            strokeLinecap="round"
-            strokeMiterlimit="10"
-            strokeWidth="2.667"
-          >
-            <path d="M15.438 13l-3.771 3.771" />
-            <path data-name="Path" d="M11.667 9.229L15.438 13" />
-          </g>
-        </svg>
+        <NhsIcon />
       </div>
     </div>
   );

644b35a87ff245e12d252c3ad61dff117e17770a
sambiramairelogic <samuel.biram@airelogic.com>
2024-08-19 13:51:38 +0100

    Add log out test

--- src/new-client/testing/log-in.spec.ts
+++ src/new-client/testing/log-in.spec.ts
@@ -32,12 +32,51 @@ test('logs in', async ({ page }) => {
 
   await page.getByLabel('Password').press('Enter');
 
-  // await page.waitForURL('/', { waitUntil: 'domcontentloaded' });
-
-  // await expect(
-  //   page.getByRole('heading', { name: 'Appointment Management Service' }),
-  // ).toBeVisible();
   await expect(
     page.getByRole('heading', { name: 'Choose a site' }),
   ).toBeVisible();
+});
+
+test('logs out', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('button', { name: 'Log In' }).click();
+  await page.getByLabel('Username').fill(config.testUser.username);
+  await page.getByLabel('Password').fill(config.testUser.password);
+  await page.getByLabel('Password').press('Enter');
+
+  await expect(
+    page.getByRole('heading', { name: 'Choose a site' }),
+  ).toBeVisible();
+
+  await expect(page.getByRole('button', { name: 'Log Out' })).toBeVisible();
+  await page.getByRole('button', { name: 'Log Out' }).click();
+
+  await expect(
+    page.getByRole('heading', { name: 'You cannot access this site' }),
+  ).toBeVisible();
+  await expect(
+    page.getByText(
+      'You are currently not signed in. To use this site, please sign in.',
+    ),
+  ).toBeVisible();
+});
+
+test('Creates a token cookie upon sign in', async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  await page.goto('/');
+  await page.getByRole('button', { name: 'Log In' }).click();
+  await page.getByLabel('Username').fill(config.testUser.username);
+  await page.getByLabel('Password').fill(config.testUser.password);
+  await page.getByLabel('Password').press('Enter');
+
+  await expect(
+    page.getByRole('heading', { name: 'Choose a site' }),
+  ).toBeVisible();
+
+  const cookies = await context.cookies();
+  expect(
+    cookies.find(c => c.name === 'token' && c.value.startsWith('ey')),
+  ).not.toBeUndefined();
 });

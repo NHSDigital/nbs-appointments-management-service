ed5048d17243213f9786107d4598e887718d0d80
Kim Crowe <kim.crowe4@nhs.net>
2024-08-20 16:19:03 +0100

    Display site list in ascending alphabetical order

--- src/new-client/src/app/lib/components/site-list.test.tsx
+++ src/new-client/src/app/lib/components/site-list.test.tsx
@@ -13,6 +13,23 @@ describe('<SiteList>', () => {
     expect(screen.queryByRole('link', { name: 'Site Beta' })).toBeVisible();
   });
 
+  it('renders sites in ascending alphabetical order', async () => {
+    const testSites: Site[] = [
+      { id: '1001', name: 'Site Zulu', address: 'Alpha Street' },
+      { id: '1002', name: 'Site Lima', address: 'Beta Street' },
+      { id: '1003', name: 'Site November', address: 'Beta Street' },
+      { id: '1004', name: 'Site Beta', address: 'Beta Street' },
+    ];
+    render(<SiteList sites={testSites} />);
+    const list = screen.getByRole('list');
+    const siteNames = list.children;
+    expect(siteNames.length).toBe(4);
+    expect(siteNames.item(0)?.textContent).toEqual('Site Beta');
+    expect(siteNames.item(1)?.textContent).toEqual('Site Lima');
+    expect(siteNames.item(2)?.textContent).toEqual('Site November');
+    expect(siteNames.item(3)?.textContent).toEqual('Site Zulu');
+  });
+
   it('links to the correct page', async () => {
     const testSites: Site[] = [
       { id: '1000', name: 'Site Alpha', address: 'Alpha Street' },

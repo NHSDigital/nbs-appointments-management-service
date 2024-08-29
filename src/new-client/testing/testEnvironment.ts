eb3185487da0f55e9bf66c459722cddb12f8c0f3
Sam Biram <63285990+sambiramairelogic@users.noreply.github.com>
2024-08-29 11:20:44 +0100

    Introduce Page Object Model in Playwright tests (#130)

--- src/new-client/testing/testEnvironment.ts
+++ src/new-client/testing/testEnvironment.ts
@@ -3,10 +3,19 @@ import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, 'playwright.env') });
 
+const testUsers = {
+  default: {
+    username: process.env.TEST_USER_USERNAME ?? '',
+    password: process.env.TEST_USER_PASSWORD ?? '',
+  },
+  // TODO: Expand with multiple users for different roles
+  // auditer: { username: 'auditer', password: '1234abc' },
+  // checkIn: { username: 'checkin', password: '1234abc' },
+};
+
 const environment = {
   BASE_URL: process.env.BASE_URL ?? '',
-  TEST_USER_USERNAME: process.env.TEST_USER_USERNAME ?? '',
-  TEST_USER_PASSWORD: process.env.TEST_USER_PASSWORD ?? '',
+  TEST_USERS: testUsers,
 };
 
 export default environment;

35e0b6e5e685a447608dba7eb1136d20d32761a9
jrowett <jrowett@gmail.com>
2024-09-03 17:00:23 +0100

    fix service bus configuration from environment variable

--- src/api/Nhs.Appointments.Api/Notifications/ServiceRegistration.cs
+++ src/api/Nhs.Appointments.Api/Notifications/ServiceRegistration.cs
@@ -4,51 +4,44 @@ using Nhs.Appointments.Api.Consumers;
 using Nhs.Appointments.Api.Functions;
 using Nhs.Appointments.Core.Messaging;
 using Nhs.Appointments.Core.Messaging.Events;
-using Notify.Client;
-using Notify.Interfaces;
 using System;
 
-namespace Nhs.Appointments.Api.Notifications
+namespace Nhs.Appointments.Api.Notifications;
+
+public static class ServiceRegistration
 {
-    public static class ServiceRegistration
+    public static IServiceCollection AddUserNotifications(this IServiceCollection services)
     {
-        public static IServiceCollection AddUserNotifications(this IServiceCollection services)
+        var userNotificationsProvider = Environment.GetEnvironmentVariable("Notifications_Provider");
+
+        services.Configure<UserRolesChangedNotifier.Options>(opts =>
         {
-            var userNotificationsProvider = Environment.GetEnvironmentVariable("Notifications_Provider");
+            opts.EmailTemplateId = Environment.GetEnvironmentVariable("UserRolesChangedEmailTemplateId");
+        });
 
-            services.Configure<UserRolesChangedNotifier.Options>(opts =>
-            {
-                opts.EmailTemplateId = Environment.GetEnvironmentVariable("UserRolesChangedEmailTemplateId");
-            });
-
-            
-            if (userNotificationsProvider == "local")
-            {
-                services.AddScoped<IMessageBus, ConsoleLogNotifications>();
-            }
-            else if(userNotificationsProvider == "azure")
-            {
-                services
-                    .AddTransient<IUserRolesChangedNotifier, UserRolesChangedNotifier>()
-                    .AddScoped<IMessageBus, MassTransitBusWrapper>()                
-                    .AddScoped<NotifyUserRolesChangedFunction>()
-                    .AddMassTransitForAzureFunctions(cfg =>
-                    {
-                        cfg.UsingAzureServiceBus((context, c) =>
-                        {
-                            c.Host(Environment.GetEnvironmentVariable("ServiceBusConnectionString"));
-                        });
-                        cfg.AddConsumer<UserRolesChangedConsumer>();
-                        cfg.AddRequestClient<UserRolesChanged>(new Uri("queue:user-role-change"));
-                    })
-                    .AddTransient<IAsyncNotificationClient>(x => new NotificationClient(Environment.GetEnvironmentVariable("GovNotifyApiKey")));
-            }
-            else
-            {
-                throw new NotSupportedException("A null or unsupported notifications provider was specified in the configuration");
-            }
-
-            return services;
+        
+        if (userNotificationsProvider == "local")
+        {
+            services.AddScoped<IMessageBus, ConsoleLogNotifications>();
         }
+        else if(userNotificationsProvider == "azure")
+        {
+            services
+                .AddTransient<IUserRolesChangedNotifier, UserRolesChangedNotifier>()
+                .AddScoped<IMessageBus, MassTransitBusWrapper>()
+                .AddScoped<NotifyUserRolesChangedFunction>()
+                .AddMassTransitForAzureFunctions(cfg =>
+                {
+                    cfg.AddConsumer<UserRolesChangedConsumer>();
+                    cfg.AddRequestClient<UserRolesChanged>(new Uri("queue:user-role-change"));
+                },
+                connectionStringConfigurationKey: "ServiceBusConnectionString");
+        }
+        else
+        {
+            throw new NotSupportedException("A null or unsupported notifications provider was specified in the configuration");
+        }
+
+        return services;
     }
 }

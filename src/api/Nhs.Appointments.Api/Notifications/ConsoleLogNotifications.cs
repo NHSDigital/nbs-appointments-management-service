40ffa3c34b447ef30d86f6803440e6f09e53e25e
jrowett <jrowett@gmail.com>
2024-09-03 18:12:51 +0100

    added real message delivery to local test message bus

--- src/api/Nhs.Appointments.Api/Notifications/ConsoleLogNotifications.cs
+++ src/api/Nhs.Appointments.Api/Notifications/ConsoleLogNotifications.cs
@@ -1,5 +1,11 @@
-﻿using Nhs.Appointments.Core.Messaging;
+﻿using MassTransit;
+using Nhs.Appointments.Api.Consumers;
+using Nhs.Appointments.Core.Messaging;
+using Nhs.Appointments.Core.Messaging.Events;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Nhs.Appointments.Api.Notifications;
@@ -9,6 +15,238 @@ public class ConsoleLogNotifications : IMessageBus
     public Task Send<T>(T message)
     {
         Console.WriteLine(Json.JsonResponseWriter.Serialize(message));
+        ProcessMessage(message);
         return Task.CompletedTask;
+    }
+
+    protected virtual void ProcessMessage<T>(T message) { }
+}
+
+public class ConsoleLogWithMessageDelivery(IConsumer<UserRolesChanged> consumer) : ConsoleLogNotifications
+{
+    protected override void ProcessMessage<T>(T message)
+    {
+        var typedMessage = message as UserRolesChanged;
+
+        if(typedMessage != null)
+        {
+            consumer.Consume(new MyConsumeContext() { Message = typedMessage });
+        }
+    }
+
+    private class MyConsumeContext: ConsumeContext<UserRolesChanged>
+    {
+        public UserRolesChanged Message { get; set; }
+
+        public ReceiveContext ReceiveContext => throw new NotImplementedException();
+
+        public SerializerContext SerializerContext => throw new NotImplementedException();
+
+        public Task ConsumeCompleted => throw new NotImplementedException();
+
+        public IEnumerable<string> SupportedMessageTypes => throw new NotImplementedException();
+
+        public CancellationToken CancellationToken => throw new NotImplementedException();
+
+        public Guid? MessageId => throw new NotImplementedException();
+
+        public Guid? RequestId => throw new NotImplementedException();
+
+        public Guid? CorrelationId => throw new NotImplementedException();
+
+        public Guid? ConversationId => throw new NotImplementedException();
+
+        public Guid? InitiatorId => throw new NotImplementedException();
+
+        public DateTime? ExpirationTime => throw new NotImplementedException();
+
+        public Uri SourceAddress => throw new NotImplementedException();
+
+        public Uri DestinationAddress => throw new NotImplementedException();
+
+        public Uri ResponseAddress => throw new NotImplementedException();
+
+        public Uri FaultAddress => throw new NotImplementedException();
+
+        public DateTime? SentTime => throw new NotImplementedException();
+
+        public Headers Headers => throw new NotImplementedException();
+
+        public HostInfo Host => throw new NotImplementedException();
+
+        public void AddConsumeTask(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T AddOrUpdatePayload<T>(PayloadFactory<T> addFactory, UpdatePayloadFactory<T> updateFactory) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public ConnectHandle ConnectPublishObserver(IPublishObserver observer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ConnectHandle ConnectSendObserver(ISendObserver observer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetOrAddPayload<T>(PayloadFactory<T> payloadFactory) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ISendEndpoint> GetSendEndpoint(Uri address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasMessageType(Type messageType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasPayloadType(Type payloadType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NotifyConsumed(TimeSpan duration, string consumerType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NotifyConsumed<T>(ConsumeContext<T> context, TimeSpan duration, string consumerType) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NotifyFaulted(TimeSpan duration, string consumerType, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NotifyFaulted<T>(ConsumeContext<T> context, TimeSpan duration, string consumerType, Exception exception) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish<T>(T message, CancellationToken cancellationToken = default) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish<T>(T message, IPipe<PublishContext<T>> publishPipe, CancellationToken cancellationToken = default) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish<T>(T message, IPipe<PublishContext> publishPipe, CancellationToken cancellationToken = default) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish(object message, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish(object message, IPipe<PublishContext> publishPipe, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish(object message, Type messageType, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish(object message, Type messageType, IPipe<PublishContext> publishPipe, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish<T>(object values, CancellationToken cancellationToken = default) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish<T>(object values, IPipe<PublishContext<T>> publishPipe, CancellationToken cancellationToken = default) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Publish<T>(object values, IPipe<PublishContext> publishPipe, CancellationToken cancellationToken = default) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Respond<T>(T message) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RespondAsync<T>(T message) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RespondAsync<T>(T message, IPipe<SendContext<T>> sendPipe) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RespondAsync<T>(T message, IPipe<SendContext> sendPipe) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RespondAsync(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RespondAsync(object message, Type messageType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RespondAsync(object message, IPipe<SendContext> sendPipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RespondAsync(object message, Type messageType, IPipe<SendContext> sendPipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RespondAsync<T>(object values) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RespondAsync<T>(object values, IPipe<SendContext<T>> sendPipe) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RespondAsync<T>(object values, IPipe<SendContext> sendPipe) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetMessage<T>([NotNullWhen(true)] out ConsumeContext<T> consumeContext) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetPayload<T>([NotNullWhen(true)] out T payload) where T : class
+        {
+            throw new NotImplementedException();
+        }
     }
 }

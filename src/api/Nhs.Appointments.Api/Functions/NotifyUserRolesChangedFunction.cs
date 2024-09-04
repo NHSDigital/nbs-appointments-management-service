d83b4f693dd308bbb070e425a53a8b01a497b9b0
jrowett <jrowett@gmail.com>
2024-09-04 16:30:20 +0100

    allow anonymous function access for notification trigger

--- src/api/Nhs.Appointments.Api/Functions/NotifyUserRolesChangedFunction.cs
+++ src/api/Nhs.Appointments.Api/Functions/NotifyUserRolesChangedFunction.cs
@@ -4,6 +4,7 @@ using Microsoft.Azure.Functions.Worker;
 using Azure.Messaging.ServiceBus;
 using System.Threading;
 using Nhs.Appointments.Api.Consumers;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Nhs.Appointments.Api.Functions;
 
@@ -12,6 +13,7 @@ public class NotifyUserRolesChangedFunction(IMessageReceiver receiver)
     const string UserRolesChangedQueueName = "user-roles-changed";
 
     [Function("NotifyUserRolesChanged")]
+    [AllowAnonymous]
     public Task NotifyUserRolesChangedAsync([ServiceBusTrigger(UserRolesChangedQueueName, Connection = "ServiceBusConnectionString")] ServiceBusReceivedMessage message, CancellationToken cancellationToken)
     {
         return receiver.HandleConsumer<UserRolesChangedConsumer>(UserRolesChangedQueueName, message, cancellationToken);

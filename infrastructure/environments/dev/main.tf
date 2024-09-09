43bfc5babd6fdcc6895a49f0e9359a965ce8ae59
Kim Crowe <kim.crowe4@nhs.net>
2024-09-09 14:18:31 +0100

    Pin terrform azurerm version to last known working version

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.83.0"
+      version = "4.0.1"
     }
   }
 
@@ -13,7 +13,7 @@ terraform {
     key                  = "dev.tfstate"
   }
 
-  required_version = ">= 1.6.5"
+  required_version = "1.9.5"
 }
 
 provider "azurerm" {

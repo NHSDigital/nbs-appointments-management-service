0ceff45f954b56f6c130857b2df5bc5e73212e43
sambiramairelogic <samuel.biram@airelogic.com>
2024-09-10 15:51:06 +0100

    Put month view into NhsPage

--- src/new-client/src/app/site/[site]/availability/month/page.tsx
+++ src/new-client/src/app/site/[site]/availability/month/page.tsx
@@ -1,105 +1,32 @@
-const MonthOverviewPage = () => {
-  const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+import { fetchSite } from '@services/appointmentsService';
+import NhsPage from '@components/nhs-page';
+import MonthPage from './month-page';
 
-  const dates = [
-    [30, 7, 14, 21, 28],
-    [1, 8, 15, 22, 29],
-    [2, 9, 16, 23, 30],
-    [3, 10, 17, 24, 31],
-    [4, 11, 18, 25, 1],
-    [5, 12, 19, 26, 2],
-    [6, 13, 20, 27, 3],
-  ];
+type PageProps = {
+  params: {
+    site: string;
+  };
+};
+
+const Page = async ({ params }: PageProps) => {
+  const site = await fetchSite(params.site);
+  const siteMoniker = site?.name ?? `Site ${params.site}`;
 
   return (
-    <div
-      style={{
-        flexDirection: 'column',
-        margin: 'auto',
-        display: 'flex',
-        width: '960px',
-        justifyContent: 'space-evenly',
-      }}
+    <NhsPage
+      title="Week Availability"
+      breadcrumbs={[
+        { name: 'Home', href: '/' },
+        { name: siteMoniker, href: `/site/${params.site}` },
+        {
+          name: 'Availability Overview',
+          href: `/site/${params.site}/availability`,
+        },
+      ]}
     >
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'baseline',
-          justifyContent: 'flex-start',
-        }}
-      >
-        <h2 style={{ flexGrow: 1 }}>October 2024</h2>
-        <div>70% - 6750 / 9000 capacity</div>
-        <div style={{ marginLeft: '30px' }}>2250 free slots available</div>
-      </div>
-      <div
-        style={{
-          margin: 'auto',
-          display: 'flex',
-          width: '960px',
-          justifyContent: 'space-evenly',
-        }}
-      >
-        {days.map((d, i) => (
-          <DayColumn key={d} day={d} dates={dates[i]} />
-        ))}
-      </div>
-    </div>
+      <MonthPage />
+    </NhsPage>
   );
 };
 
-const DayColumn = ({ day, dates }: { day: string; dates: number[] }) => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        margin: '2px',
-      }}
-    >
-      <div>
-        <h4>{day}</h4>
-      </div>
-      {dates.map(d => (
-        <div
-          key={d}
-          className="nhsuk-card-group__item"
-          style={{ width: '100%', marginTop: '5px' }}
-        >
-          <div className="nhsuk-card nhsuk-card" style={{ padding: '8px' }}>
-            <div
-              className="nhsuk-card__content"
-              style={{
-                padding: '2px',
-                display: 'flex',
-                justifyContent: 'flex-start',
-                flexDirection: 'column',
-              }}
-            >
-              <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
-                <span>
-                  <b>{d}</b>
-                </span>
-                <span style={{ flexGrow: 1, textAlign: 'right' }}>78%</span>
-              </div>
-              <div
-                style={{
-                  flexGrow: 1,
-                  background: 'grey',
-                  height: '50px',
-                  borderBottom: '39px solid darkblue',
-                }}
-              >
-                &nbsp;
-              </div>
-              <div style={{ textWrap: 'nowrap' }}>200 / 300</div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default MonthOverviewPage;
+export default Page;

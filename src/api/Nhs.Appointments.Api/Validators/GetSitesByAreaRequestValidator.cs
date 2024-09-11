15cc26960fafee45c6cea6c7bf6c71a5c3a37100
Kim Crowe <kim.crowe4@nhs.net>
2024-09-11 14:37:38 +0100

    PR comment - remove redundnat validators

--- src/api/Nhs.Appointments.Api/Validators/GetSitesByAreaRequestValidator.cs
+++ src/api/Nhs.Appointments.Api/Validators/GetSitesByAreaRequestValidator.cs
@@ -8,22 +8,18 @@ public class GetSitesByAreaRequestValidator : AbstractValidator<GetSitesRequest>
     public GetSitesByAreaRequestValidator()
     {
         RuleFor(x => x.longitude)
-            .NotEmpty()
             .LessThanOrEqualTo(180)
             .GreaterThanOrEqualTo(-180)
             .Configure(rule => rule.MessageBuilder = _ => "Provide a valid longitude value (between -180 <-> 180 degrees).");
         RuleFor(x => x.latitude)
-            .NotEmpty()
             .LessThanOrEqualTo(90)
             .GreaterThanOrEqualTo(-90)
             .Configure(rule => rule.MessageBuilder = _ => "Provide a valid latitude value (between -90 <-> 90 degrees).");
         RuleFor(x => x.searchRadius)
-            .NotNull()
             .LessThanOrEqualTo(100000)
             .GreaterThanOrEqualTo(1000)
             .Configure(rule => rule.MessageBuilder = _ => "Provide a search radius in meters (between 1000 - 100,000m).");
         RuleFor(x => x.maximumRecords)
-            .NotNull()
             .LessThanOrEqualTo(50)
             .GreaterThan(0)
             .Configure(rule => rule.MessageBuilder = _ => "Provide a number of maximum records (between 1 - 50 records).");

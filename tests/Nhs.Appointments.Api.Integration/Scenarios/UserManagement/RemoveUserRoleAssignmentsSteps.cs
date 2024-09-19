a2bd93ec2a074a002a8fcd1d4f338742ef32b0b2
sambiramairelogic <samuel.biram@airelogic.com>
2024-09-19 12:29:29 +0100

    Add gherking tests

---
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/UserManagement/RemoveUserRoleAssignmentsSteps.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Nhs.Appointments.Api.Json;
+using Nhs.Appointments.Api.Models;
+using Xunit.Gherkin.Quick;
+using System.Text;
+using Microsoft.Azure.Cosmos;
+using Nhs.Appointments.Persistance.Models;
+
+namespace Nhs.Appointments.Api.Integration.Scenarios.UserManagement;
+
+[FeatureFile("./Scenarios/UserManagement/RemoveUserRoleAssignments.feature")]
+public sealed class RemoveUserRoleAssignmentsSteps : UserManagementBaseFeatureSteps
+{
+    private HttpResponseMessage? _response;
+    private HttpStatusCode _statusCode;
+    private RemoveUserResponse? _actualResponse;
+    
+    [When(@"I remove user '(.+)' from site '(.+)'")]
+    public async Task AssignRole(string user, string site)
+    {
+        // Check user exists before removing them for assurance this method
+        // actually works when it asserts that the removal has succeeded
+        var document = await GetUserDocument(Client, user);
+        document.Should().NotBeNull();
+
+        var userId = GetUserId(user);
+        var siteId = GetSiteId(site);
+
+        var requestBody = new RemoveUserRequest()
+        {
+            User = userId,
+            Site = siteId
+        };
+
+        _response = await Http.PostAsync($"http://localhost:7071/api/user/remove", new StringContent(JsonResponseWriter.Serialize(requestBody), Encoding.UTF8, "application/json"));
+        _statusCode = _response.StatusCode;
+        _actualResponse = await JsonRequestReader.ReadRequestAsync<RemoveUserResponse>(await _response.Content.ReadAsStreamAsync());
+    }
+    
+    [Then(@"'(.+)' does not exist in the database")]
+    public async Task Assert(string user)
+    {
+        var document = await GetUserDocument(Client, user);
+
+        document.Should().BeNull();
+    }
+
+    [Then(@"user '(.+)' would have the following role assignments")]
+    public async Task Assert(string user, Gherkin.Ast.DataTable dataTable)
+    {
+        _response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        var userId = GetUserId(user);
+        var expectedRoleAssignments = dataTable.Rows.Skip(1).Select(
+            (row) => new RoleAssignment
+            {
+                Scope = $"site:{GetSiteId(row.Cells.ElementAt(0).Value)}",
+                Role = row.Cells.ElementAt(1).Value
+            });
+        var actualResult = await Client.GetContainer("appts", "index_data").ReadItemAsync<UserDocument>(userId, new PartitionKey("user"));
+        actualResult.Resource.RoleAssignments.Should().BeEquivalentTo(expectedRoleAssignments);
+    }
+
+    private async Task<UserDocument?> GetUserDocument(CosmosClient cosmosClient, string user)
+    {
+        var container = cosmosClient.GetContainer("appts", "index_data");
+        var document = container.GetItemLinqQueryable<UserDocument>(true)
+            .Where(d => d.Id == GetUserId(user))
+            .AsEnumerable()
+            .FirstOrDefault();
+
+        return document;
+    }
+}

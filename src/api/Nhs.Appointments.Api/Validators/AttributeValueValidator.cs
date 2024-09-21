caae27aeb63ed3f6162ae91a06e655bf0369b743
Kim Crowe <kim.crowe4@nhs.net>
2024-09-21 15:47:36 +0100

    Attribute value validator and tests

---
+++ src/api/Nhs.Appointments.Api/Validators/AttributeValueValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using FluentValidation;
+using Nhs.Appointments.Api.Models;
+using Nhs.Appointments.Core;
+
+namespace Nhs.Appointments.Api.Validators;
+
+public class AttributeValueValidator : AbstractValidator<AttributeValue>
+{
+    public AttributeValueValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("Provide a valid attribute id");
+        RuleFor(x => x.Value)
+            .Must(value => value is "true" or "false")
+            .WithMessage("An attribute value must be provided (value must be true or false)");
+    }
+}
---
+++ tests/Nhs.Appointments.Api.UnitTests/Validators/SetSiteAttributeValuesValidatorTests.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using FluentValidation.TestHelper;
+using Nhs.Appointments.Api.Models;
+using Nhs.Appointments.Api.Validators;
+using Nhs.Appointments.Core;
+
+namespace Nhs.Appointments.Api.Tests.Validators;
+
+public class SetSiteAttributeValuesValidatorTests
+{
+    private readonly SetSiteAttributeValuesValidator _sut = new();
+    
+    [Theory]
+    [InlineData("true")]
+    [InlineData("false")]
+    public void Validate_ReturnSuccess_WhenRequestIsValid(string value)
+    {
+        var testRequest = new SetSiteAttributesRequest(
+            Site: "ABC01",
+            AttributeValues: new[]
+            {
+                new AttributeValue(
+                    Id: "accessibility/attribute_1",
+                    Value: value)
+            });
+        var result = _sut.Validate(testRequest);
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Validate_ReturnsError_WhenSiteIsInvalid(string? siteId)
+    {
+        var request = new SetSiteAttributesRequest(
+            Site: siteId,
+            AttributeValues: new[]
+            {
+                new AttributeValue(
+                    Id: "accessibility/attribute_1",
+                    Value: "true")
+            });
+        
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().PropertyName.Should().Be(nameof(SetSiteAttributesRequest.Site));
+    }
+    
+    [Fact]
+    public void Validate_ReturnsError_WhenAttributeValuesArrayIsNull()
+    {
+        var request = new SetSiteAttributesRequest(Site: "ABC01", AttributeValues: null);
+        var result = _sut.TestValidate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().PropertyName.Should().Contain(nameof(SetSiteAttributesRequest.AttributeValues));
+    }
+    
+    [Fact]
+    public void Validate_ReturnsError_WhenAttributeValuesArrayIsEmpty()
+    {
+        var request = new SetSiteAttributesRequest(Site: "ABC01", AttributeValues: Array.Empty<AttributeValue>());
+        var result = _sut.TestValidate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().PropertyName.Should().Contain(nameof(SetSiteAttributesRequest.AttributeValues));
+    }
+}

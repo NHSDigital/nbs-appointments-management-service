82fc9511f83e70007efb758e3451de10d14ecac4
Kim Crowe <kim.crowe4@nhs.net>
2024-10-18 20:10:21 +0100

    Support multi env pipeline

---
+++ infrastructure/environments/exp/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.5.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "nbs-mya-rg-exp-uks"
+    storage_account_name = "myatfexpuks"
+    container_name       = "tfstate"
+    key                  = "dev.tfstate"
+  }
+
+  required_version = ">= 1.6.5"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "api" {
+  environment                    = "exp"
+  source                         = "../../resources"
+  auth_provider_issuer           = var.AUTH_PROVIDER_ISSUER
+  auth_provider_authorize_uri    = var.AUTH_PROVIDER_AUTHORIZE_URI
+  auth_provider_token_uri        = var.AUTH_PROVIDER_TOKEN_URI
+  auth_provider_jwks_uri         = var.AUTH_PROVIDER_JWKS_URI
+  auth_provider_challenge_phrase = var.AUTH_PROVIDER_CHALLENGE_PHRASE
+  auth_provider_client_id        = var.AUTH_PROVIDER_CLIENT_ID
+  auth_provider_return_uri       = var.AUTH_PROVIDER_RETURN_URI
+  api_keys                       = var.API_KEYS
+  gov_notify_api_key             = var.GOV_NOTIFY_API_KEY
+}

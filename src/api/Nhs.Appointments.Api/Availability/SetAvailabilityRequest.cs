66cecdf639d9f4a5010bdff7c181e79412eef71b
ste-banks <stephen.banks4@nhs.net>
2024-10-21 09:02:43 +0100

    Adding SetAvailibility endpoint & request model - WIP

---
+++ src/api/Nhs.Appointments.Api/Availability/SetAvailabilityRequest.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Nhs.Appointments.Api.Availability;
+
+public record SetAvailabilityRequest(
+    [JsonProperty("date")]
+    string Date,
+    [JsonProperty("site")]
+    string Site,
+    [JsonProperty("sessions")]
+    Session[] Sessions)
+{
+    public DateOnly AvailabilityDate => DateOnly.ParseExact(Date, "yyyy-MM-dd");
+}
+
+public record Session(
+    [JsonProperty("from")]
+    string From,
+    [JsonProperty("until")]
+    string Until,
+    [JsonProperty("services")]
+    string[] Services,
+    [JsonProperty("slotLength")]
+    int SlotLength,
+    [JsonProperty("capacity")]
+    int Capacity);
---
+++ src/api/Nhs.Appointments.Api/Functions/SetAvailabilityFunction.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Nhs.Appointments.Api.Auth;
+using Nhs.Appointments.Api.Availability;
+using Nhs.Appointments.Api.Models;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Nhs.Appointments.Api.Functions;
+
+public class SetAvailabilityFunction : BaseApiFunction<SetAvailabilityRequest, EmptyResponse>
+{
+    public SetAvailabilityFunction(IValidator<SetAvailabilityRequest> validator, IUserContextProvider userContextProvider, ILogger logger)
+        : base(validator, userContextProvider, logger)
+    {
+    }
+
+    [OpenApiOperation(operationId: "SetAvailability", tags: new[] {"Appointment Availability"}, Summary = "Set appointment availability")]
+    [OpenApiRequestBody("text/json", typeof(SetAvailabilityRequest), Required = true)]
+    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.OK, Description = "Site availability successfully set or updated")]
+    [RequiresPermission("availability:query", typeof(NoSiteRequestInspector))]
+    [Function("SetAvailabilityFunction")]
+    public override Task<IActionResult> RunAsync(HttpRequest req)
+    {
+        return base.RunAsync(req);
+    }
+
+    protected override Task<ApiResult<EmptyResponse>> HandleRequest(SetAvailabilityRequest request, ILogger logger)
+    {
+        throw new NotImplementedException();
+    }
+}
+

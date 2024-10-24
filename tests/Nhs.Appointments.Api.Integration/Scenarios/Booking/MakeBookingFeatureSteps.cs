8b834e6e24ede4699a7c78fac264df4b7c7568c2
Kim Crowe <kim.crowe4@nhs.net>
2024-10-24 16:21:37 +0100

    Add contact details to booking document

--- tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/MakeBookingFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/MakeBookingFeatureSteps.cs
@@ -5,6 +5,9 @@ using Xunit.Gherkin.Quick;
 using FluentAssertions;
 using Newtonsoft.Json;
 using Nhs.Appointments.Api.Models;
+using Nhs.Appointments.Persistance.Models;
+using AttendeeDetails = Nhs.Appointments.Core.AttendeeDetails;
+using ContactItem = Nhs.Appointments.Core.ContactItem;
 
 namespace Nhs.Appointments.Api.Integration.Scenarios.Booking
 {
@@ -28,7 +31,6 @@ namespace Nhs.Appointments.Api.Integration.Scenarios.Booking
                 service = service,
                 duration = 5,
                 site = GetSiteId(siteDesignation),
-                sessionHolder = "default", 
                 attendeeDetails = new { 
                     nhsNumber = "1234678890", 
                     firstName = "Bill", 
@@ -48,10 +50,37 @@ namespace Nhs.Appointments.Api.Integration.Scenarios.Booking
         [Then(@"the booking is created and the reference number is returned containing '([\w:]+)'")]
         public async Task Assert(string bookingIncrement)
         {
-            var day = DateTime.Now.ToString("dd");
-            _response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            var siteId = GetSiteId();
             var result = JsonConvert.DeserializeObject<MakeBookingResponse>(await _response.Content.ReadAsStringAsync());
+            var bookingReference = result.BookingReference;
+            var expectedBooking = new BookingDocument()
+            {
+                Site = siteId,
+                Reference = bookingReference,
+                From = new DateTime(2077, 01, 01, 9, 20, 0),
+                Duration = 5,
+                Service = "COVID",
+                Outcome = null,
+                AttendeeDetails = new AttendeeDetails()
+                {
+                    NhsNumber = "1234678890",
+                    FirstName = "Bill",
+                    LastName = "Builder",
+                    DateOfBirth = new DateOnly(2000, 2, 1)
+                },
+                ContactDetails =
+                [
+                    new ContactItem { Type = "email", Value = "test@tempuri.org" },
+                    new ContactItem { Type = "phone", Value = "0123456789" }
+                ],
+                DocumentType = "booking",
+                Id = bookingReference
+            };
+            
+            _response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
             result.BookingReference.Should().MatchRegex($"([0-9]){{2}}-([0-9]{{2}})-{bookingIncrement}");
+            var actualBooking = await Client.GetContainer("appts", "booking_data").ReadItemAsync<BookingDocument>(bookingReference, new Microsoft.Azure.Cosmos.PartitionKey(siteId)); 
+            actualBooking.Resource.Should().BeEquivalentTo(expectedBooking);
         }
         
         [Then(@"I receive a message informing me that the appointment is no longer available")]

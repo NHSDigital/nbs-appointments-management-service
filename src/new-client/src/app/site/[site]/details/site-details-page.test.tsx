f1b1832b91876b0e4c03e8dbaa2db3010562a91d
ste-banks <stephen.banks4@nhs.net>
2024-10-28 09:16:07 +0000

    Fixing remaining front end tests

--- src/new-client/src/app/site/[site]/details/site-details-page.test.tsx
+++ src/new-client/src/app/site/[site]/details/site-details-page.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import SiteDetailsPage from './site-details-page';
-import { AttributeDefinition, AttributeValue } from '@types';
+import { AttributeDefinition, SiteWithAttributes } from '@types';
 import {
   fetchAttributeDefinitions,
-  fetchSiteAttributeValues,
+  fetchSite,
 } from '@services/appointmentsService';
 import {
   mockAttributeDefinitions,
-  mockAttributeValues,
   mockSite,
+  mockSiteWithAttributes,
 } from '@testing/data';
 
 jest.mock('@services/appointmentsService');
@@ -17,14 +17,12 @@ const fetchAttributeDefinitionsMock = fetchAttributeDefinitions as jest.Mock<
 >;
 
 jest.mock('@services/appointmentsService');
-const fetchSiteAttributeValuesMock = fetchSiteAttributeValues as jest.Mock<
-  Promise<AttributeValue[]>
->;
+const fetchSiteMock = fetchSite as jest.Mock<Promise<SiteWithAttributes>>;
 
 describe('Manage Attributes Page', () => {
   beforeEach(() => {
     fetchAttributeDefinitionsMock.mockResolvedValue(mockAttributeDefinitions);
-    fetchSiteAttributeValuesMock.mockResolvedValue(mockAttributeValues);
+    fetchSiteMock.mockResolvedValue(mockSiteWithAttributes);
   });
 
   it('renders', async () => {
--- src/new-client/src/app/testing/data.ts
+++ src/new-client/src/app/testing/data.ts
@@ -112,6 +112,7 @@ const mockSiteWithAttributes: SiteWithAttributes = {
   name: mockSites[0].name,
   attributeValues: [
     { id: 'site_details/info_for_citizen', value: 'Test information' },
+    { id: 'accessibility/attr_1', value: 'true' },
   ],
 };
 

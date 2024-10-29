1127d27993d8edc08885ed2e13ded38ba74e7555
ste-banks <stephen.banks4@nhs.net>
2024-10-29 10:59:14 +0000

    Removing failing test

--- tests/Nhs.Appointments.Api.UnitTests/Validators/AttributeValuesValidatorTests.cs
+++ tests/Nhs.Appointments.Api.UnitTests/Validators/AttributeValuesValidatorTests.cs
@@ -22,18 +22,4 @@ public class AttributeValuesValidatorTests
         result.Errors.Should().HaveCount(1);
         result.Errors.Single().PropertyName.Should().Contain(nameof(AttributeValue.Id));
     }
-    
-    [Theory]
-    [InlineData("")]
-    [InlineData(null)]
-    public void Validate_ReturnsError_WhenValueIsInvalid(string? value)
-    {
-        var attributeValue = new AttributeValue(
-            Id: "attribute_id",
-            Value: value);
-        var result = _sut.TestValidate(attributeValue);
-        result.IsValid.Should().BeFalse();
-        result.Errors.Should().HaveCount(1);
-        result.Errors.Single().PropertyName.Should().Contain(nameof(AttributeValue.Value));
-    }
 }

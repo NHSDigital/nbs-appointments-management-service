78ac93f7a30454a432a4ba30a5176c3b8d322494
sambiramairelogic <samuel.biram@airelogic.com>
2024-10-31 11:29:08 +0000

    Add tests for capacity calculation and summary step

--- src/new-client/src/app/site/[site]/create-availability/wizard/capacity-calculation.tsx
+++ src/new-client/src/app/site/[site]/create-availability/wizard/capacity-calculation.tsx
@@ -8,24 +8,30 @@ type CapacityCalculationProps = {
   endTime: TimeComponents;
 };
 
-const CapacityCalculation = ({
+const CapacityCalculation = (props: CapacityCalculationProps) => {
+  return <InsetText>{calculateCapacity(props)}</InsetText>;
+};
+
+const calculateCapacity = ({
   slotLength,
   capacity,
   startTime,
   endTime,
-}: CapacityCalculationProps) => {
-  // TODO: Neaten this up a bit, test more edge cases etc.
+}: CapacityCalculationProps): string => {
+  if (
+    !Number.isInteger(slotLength) ||
+    !Number.isInteger(capacity) ||
+    !Number.isInteger(startTime.hour) ||
+    !Number.isInteger(startTime.minute) ||
+    !Number.isInteger(endTime.hour) ||
+    !Number.isInteger(endTime.minute)
+  ) {
+    return 'No capacity.';
+  }
+
   const startMinutes = startTime.hour * 60 + startTime.minute;
   const endMinutes = endTime.hour * 60 + endTime.minute;
   const totalMinutesAvailable = endMinutes - startMinutes;
-
-  if (
-    totalMinutesAvailable <= 0 ||
-    Number.isNaN(capacity) ||
-    Number.isNaN(slotLength)
-  ) {
-    return <InsetText>No capacity</InsetText>;
-  }
 
   const totalSlots = Math.floor(totalMinutesAvailable / slotLength);
   const slotsPerHour = Math.floor(60 / slotLength);
@@ -37,17 +43,15 @@ const CapacityCalculation = ({
     Number.isNaN(appointmentsPerHour) ||
     Number.isNaN(appointmentsPerDay) ||
     !Number.isInteger(appointmentsPerHour) ||
-    !Number.isInteger(appointmentsPerDay)
+    !Number.isInteger(appointmentsPerDay) ||
+    appointmentsPerDay < 1 ||
+    appointmentsPerHour < 1
   ) {
-    return <InsetText>No capacity</InsetText>;
+    return 'No capacity.';
   }
 
-  return (
-    <InsetText>
-      {`${appointmentsPerDay} appointments per day. ${appointmentsPerHour} per
-      hour.`}
-    </InsetText>
-  );
+  return `${appointmentsPerDay} appointments per day. ${appointmentsPerHour} per hour.`;
 };
 
+export { calculateCapacity };
 export default CapacityCalculation;

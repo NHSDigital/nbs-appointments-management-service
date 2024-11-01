7633a0de72d9b3f57374558a4c9d1dcafd27d091
jrowett <jrowett@gmail.com>
2024-11-01 11:08:59 +0000

    added manual trigger endpoint for removing expired provisional bookings

--- tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/ConfirmBooking.feature
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/ConfirmBooking.feature
@@ -11,3 +11,15 @@
       When I confirm the booking
       Then the call should be successful
       And the booking is no longer marked as provisional
+
+    Scenario: Removed an expired provisional appointment
+      Given the site is configured for MYA
+      And the following sessions
+        | Date       | From  | Until | Services | Slot Length | Capacity |
+        | 2077-01-01 | 09:00 | 10:00 | COVID    | 5           | 1        |
+      And the following expired provisional bookings have been made
+        | Date       | Time  | Duration | Service | 
+        | 2077-01-01 | 09:00 | 5        | COVID   |
+      When the expired provisional bookings are swept
+      Then the call should be successful
+      And the booking no longer exists
--- tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/ConfirmBookingFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/ConfirmBookingFeatureSteps.cs
@@ -35,5 +35,24 @@ public sealed class ConfirmBookingFeatureSteps : BaseFeatureSteps
         var actualBookingIndex = await Client.GetContainer("appts", "index_data").ReadItemAsync<BookingIndexDocument>(bookingReference, new Microsoft.Azure.Cosmos.PartitionKey("booking_index"));
         actualBookingIndex.Resource.Provisional.Should().BeFalse();
     }
+
+    [When("the expired provisional bookings are swept")]
+    public async Task RemovedExpiredProvisionalBooking()
+    {
+        var bookingReference = GetBookingReference("0", BookingType.Provisional);
+        _response = await Http.PostAsJsonAsync($"http://localhost:7071/api/system/run-provisional-sweep", new StringContent(""));
+    }
+
+    [And("the booking no longer exists")]
+    public async Task AssertBookingRemoved()
+    {
+        var siteId = GetSiteId();
+        var bookingReference = GetBookingReference("0", BookingType.Provisional);
+        var actualBooking = await Client.GetContainer("appts", "booking_data").ReadItemAsync<BookingDocument>(bookingReference, new Microsoft.Azure.Cosmos.PartitionKey(siteId));
+        actualBooking.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+
+        var actualBookingIndex = await Client.GetContainer("appts", "index_data").ReadItemAsync<BookingIndexDocument>(bookingReference, new Microsoft.Azure.Cosmos.PartitionKey("booking_index"));
+        actualBookingIndex.StatusCode.Should().Be(System.Net.HttpStatusCode.NotFound);
+    }
 }
 

8f9fa541de78da439d20d837fec5c13ebc200d56
Kim Crowe <kim.crowe4@nhs.net>
2024-11-08 10:24:18 +0000

    Remove unused method

--- tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/CancelFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/CancelFeatureSteps.cs
@@ -14,13 +14,5 @@ namespace Nhs.Appointments.Api.Integration.Scenarios.Booking
             var bookingReference = BookingReferences.GetBookingReference(0, BookingType.Confirmed);
             Response = await Http.PostAsync($"http://localhost:7071/api/booking/{bookingReference}/cancel", null);
         }
-
-        private DayOfWeek[] ParseDays(string pattern)
-        {
-            if (pattern == "All")
-                return new DayOfWeek[] { DayOfWeek.Sunday, DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday, DayOfWeek.Saturday };
-            else
-                return pattern.Split(",").Select(d => Enum.Parse(typeof(DayOfWeek), d, true)).Cast<DayOfWeek>().ToArray();
-        }
     }
 }

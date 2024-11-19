13dadc5f4b7d6655d1ee1ac3968a0da5d921f073
sambiramairelogic <samuel.biram@airelogic.com>
2024-11-19 15:49:54 +0000

    Add Function unit test

---
+++ tests/Nhs.Appointments.Api.UnitTests/Functions/GetAvailabilityCreatedEventsFunctionTestscs.cs
@@ -0,0 +1,80 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using Nhs.Appointments.Api.Auth;
+using Nhs.Appointments.Api.Functions;
+using Nhs.Appointments.Api.Models;
+using Nhs.Appointments.Core;
+using FluentValidation.Results;
+
+namespace Nhs.Appointments.Api.Tests.Functions;
+
+public class GetAvailabilityCreatedEventsFunctionTests
+{
+    private readonly Mock<IAvailabilityService> availabilityService = new();
+    private readonly Mock<IValidator<SiteBasedResourceRequest>> _validator = new();
+    private readonly Mock<IUserContextProvider> _userContextProvider = new();
+    private readonly Mock<ILogger<GetAvailabilityCreatedEventsFunction>> _logger = new();
+    private readonly Mock<IMetricsRecorder> _metricsRecorder = new();
+    private readonly GetAvailabilityCreatedEventsFunction _sut;
+
+    public GetAvailabilityCreatedEventsFunctionTests()
+    {
+        _sut = new GetAvailabilityCreatedEventsFunction(
+            availabilityService.Object,
+            _validator.Object,
+            _userContextProvider.Object,
+            _logger.Object,
+            _metricsRecorder.Object);
+        _validator
+            .Setup(x => x.ValidateAsync(It.IsAny<SiteBasedResourceRequest>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new ValidationResult());
+    }
+
+    private static HttpRequest CreateRequest()
+    {
+        var context = new DefaultHttpContext();
+        var request = context.Request;
+        request.QueryString = new QueryString($"?site=1000");
+        return request;
+    }
+
+    [Fact]
+    public async Task RunsAsync_Gets_Availability_Created_Events()
+    {
+        availabilityService.Setup(
+            x => x.GetAvailabilityCreatedEventsAsync("1000"))
+            .ReturnsAsync([new AvailabilityCreatedEvent()
+            {
+                Created = DateTime.UtcNow,
+                By = "test@test.com",
+                Site = "1000",
+                From = DateOnly.FromDateTime(DateTime.Now),
+            }]);
+
+        var testPrincipal = UserDataGenerator.CreateUserPrincipal("test@test.com");
+        _userContextProvider.Setup(x => x.UserPrincipal).Returns(testPrincipal);
+        var request = CreateRequest();
+
+        var result = await _sut.RunAsync(request) as ContentResult;
+
+        result?.StatusCode.Should().Be(200);
+        var response = await ReadResponseAsync<IEnumerable<AvailabilityCreatedEvent>>(result.Content);
+
+        response.Single().By.Should().Be("test@test.com");
+        availabilityService.Verify(
+            x => x.GetAvailabilityCreatedEventsAsync("1000"),
+            Times.Once);
+    }
+
+
+    private static async Task<TRequest> ReadResponseAsync<TRequest>(string response)
+    {
+        var body = await new StringReader(response).ReadToEndAsync();
+        return JsonConvert.DeserializeObject<TRequest>(body);
+    }
+}

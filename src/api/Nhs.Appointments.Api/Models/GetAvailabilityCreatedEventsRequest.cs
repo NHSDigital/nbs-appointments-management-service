31d96944e5dadc73a0878742319700296d717520
sambiramairelogic <samuel.biram@airelogic.com>
2024-11-21 15:09:02 +0000

    Add from DateOnly to function

---
+++ src/api/Nhs.Appointments.Api/Models/GetAvailabilityCreatedEventsRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Nhs.Appointments.Api.Models;
+public record GetAvailabilityCreatedEventsRequest(
+    [JsonProperty("site")]
+    string Site,
+    [JsonProperty("from")]
+    string From
+)
+
+{
+    public DateOnly FromDate => DateOnly.ParseExact(From, "yyyy-MM-dd");
+};
---
+++ src/api/Nhs.Appointments.Api/Validators/GetAvailabilityCreatedEventsRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FluentValidation;
+using Nhs.Appointments.Api.Models;
+
+namespace Nhs.Appointments.Api.Validators;
+
+public class GetAvailabilityCreatedEventsRequestValidator : AbstractValidator<GetAvailabilityCreatedEventsRequest>
+{
+    public GetAvailabilityCreatedEventsRequestValidator()
+    {
+        RuleFor(x => x.Site)
+            .NotEmpty()
+            .WithMessage("Provide a valid site");
+
+        RuleFor(x => x.From)
+            .NotEmpty().WithMessage("Provide a date in 'yyyy-MM-dd'")
+            .Must(x => DateOnly.TryParseExact(x, "yyyy-MM-dd", out var _))
+            .WithMessage("Provide a date in the format 'yyyy-MM-dd'");
+    }
+}

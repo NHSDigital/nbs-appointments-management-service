498a68401ca9e65f41b337324f8fc391b84b506e
sambiramairelogic <samuel.biram@airelogic.com>
2024-11-21 15:26:52 +0000

    Add validator tests

--- src/api/Nhs.Appointments.Api/Validators/GetAvailabilityCreatedEventsRequestValidator.cs
+++ src/api/Nhs.Appointments.Api/Validators/GetAvailabilityCreatedEventsRequestValidator.cs
@@ -13,6 +13,7 @@ public class GetAvailabilityCreatedEventsRequestValidator : AbstractValidator<Ge
             .WithMessage("Provide a valid site");
 
         RuleFor(x => x.From)
+            .Cascade(CascadeMode.Stop)
             .NotEmpty().WithMessage("Provide a date in 'yyyy-MM-dd'")
             .Must(x => DateOnly.TryParseExact(x, "yyyy-MM-dd", out var _))
             .WithMessage("Provide a date in the format 'yyyy-MM-dd'");
---
+++ tests/Nhs.Appointments.Api.UnitTests/Validators/GetAvailabilityCreatedEventsRequestValidatorTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using Nhs.Appointments.Api.Models;
+using Nhs.Appointments.Api.Validators;
+
+namespace Nhs.Appointments.Api.Tests.Validators;
+
+public class GetAvailabilityCreatedEventsRequestValidatorTests
+{
+    private readonly GetAvailabilityCreatedEventsRequestValidator _sut = new();
+
+    [Fact]
+    public void Validate_ReturnsError_WhenSiteIsBlank()
+    {
+        var testRequest = new GetAvailabilityCreatedEventsRequest(string.Empty, "2020-01-01");
+        var result = _sut.Validate(testRequest);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().PropertyName.Should().Be(nameof(GetAvailabilityCreatedEventsRequest.Site));
+    }
+
+    [Fact]
+    public void Validate_ReturnsError_WhenSiteIsNull()
+    {
+        var testRequest = new GetAvailabilityCreatedEventsRequest(null, "2020-01-01");
+        var result = _sut.Validate(testRequest);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().PropertyName.Should().Be(nameof(GetAvailabilityCreatedEventsRequest.Site));
+    }
+
+    [Fact]
+    public void Validate_ReturnsError_WhenFromIsBlank()
+    {
+        var testRequest = new GetAvailabilityCreatedEventsRequest("ABC02", "");
+        var result = _sut.Validate(testRequest);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().PropertyName.Should().Be(nameof(GetAvailabilityCreatedEventsRequest.From));
+    }
+
+    [Fact]
+    public void Validate_ReturnsError_WhenFromIsNull()
+    {
+        var testRequest = new GetAvailabilityCreatedEventsRequest("ABC02", null);
+        var result = _sut.Validate(testRequest);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().PropertyName.Should().Be(nameof(GetAvailabilityCreatedEventsRequest.From));
+    }
+
+    [Fact]
+    public void Validate_ReturnsError_WhenFromIsInWrongFormat()
+    {
+        var testRequest = new GetAvailabilityCreatedEventsRequest("ABC02", "20:01:2031");
+        var result = _sut.Validate(testRequest);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().PropertyName.Should().Be(nameof(GetAvailabilityCreatedEventsRequest.From));
+    }
+
+    [Fact]
+    public void Validate_ReturnsSuccess_WhenRequestIsValid()
+    {
+        var testRequest = new GetAvailabilityCreatedEventsRequest("ABC02", "2020-01-01");
+        var result = _sut.Validate(testRequest);
+        result.IsValid.Should().BeTrue();
+        result.Errors.Should().HaveCount(0);
+    }
+}
+

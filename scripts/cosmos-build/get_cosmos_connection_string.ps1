cff3d9c95a4353bb3fa90e91a76541c97121b851
Kim Crowe <kim.crowe4@nhs.net>
2024-11-22 12:04:43 +0000

    Update variable name

--- scripts/cosmos-build/get_cosmos_connection_string.ps1
+++ scripts/cosmos-build/get_cosmos_connection_string.ps1
@@ -23,5 +23,5 @@ $NameValue=$connectionString -replace ";", "`n" | ConvertFrom-StringData
 $cosmosEndpoint = $NameValue.AccountEndpoint
 $cosmosPrimaryKey = $NameValue.AccountKey
 
-Write-Host "##vso[task.setvariable variable=CosmosEndpoint]$cosmosEndpoint"
-Write-Host "##vso[task.setvariable variable=CosmosToken]$cosmosPrimaryKey"
+Write-Host "##vso[task.setvariable variable=COSMOS_ENDPOINT]$cosmosEndpoint"
+Write-Host "##vso[task.setvariable variable=COSMOS_TOKEN]$cosmosPrimaryKey"

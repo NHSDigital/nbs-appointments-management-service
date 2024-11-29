c16564d08541684a2a63e1842770478b0e3f4125
sarithalakkyreddy <saritha.lakkyreddy1@nhs.net>
2024-11-29 14:15:26 +0000

    added tests to creatavailability wizard

--- src/client/testing/create-availability.spec.ts
+++ src/client/testing/create-availability.spec.ts
@@ -31,3 +31,53 @@ test('A user can navigate to the Create Availability flow from the site page', a
   await page.waitForURL('**/site/ABC02/create-availability');
   await expect(createAvailabilityPage.title).toBeVisible();
 });
+
+test('E2E Create single session of RSV availability', async ({ page }) => {
+  await rootPage.goto();
+  await rootPage.pageContentLogInButton.click();
+  await oAuthPage.signIn();
+  await siteSelectionPage.selectSite('Church Lane Pharmacy');
+  await sitePage.createAvailabilityCard.click();
+  await page.waitForURL('**/site/ABC02/create-availability');
+  await createAvailabilityPage.btnCreateAvailability.click();
+  await expect(createAvailabilityPage.sessionTittle).toBeVisible();
+  await createAvailabilityPage.selectSession('Single date session');
+  await createAvailabilityPage.btnContinue.click();
+  await createAvailabilityPage.enterSingleDateSessionDate('27', '10', '2025');
+  await createAvailabilityPage.btnContinue.click();
+  await createAvailabilityPage.enterStartTime('09', '00');
+  await createAvailabilityPage.enterEndtTime('10', '00');
+  await createAvailabilityPage.noOfVaccinators('1');
+  await createAvailabilityPage.appointmentLength('5');
+  await createAvailabilityPage.btnContinue.click();
+  await createAvailabilityPage.addServices('RSV (Adult)');
+  await createAvailabilityPage.btnContinue.click();
+  await createAvailabilityPage.btnSaveSession.click();
+  await expect(createAvailabilityPage.sessionSuccessMsg).toBeVisible();
+});
+
+test('E2E Create weekly session of RSV availability', async ({ page }) => {
+  await rootPage.goto();
+  await rootPage.pageContentLogInButton.click();
+  await oAuthPage.signIn();
+  await siteSelectionPage.selectSite('Church Lane Pharmacy');
+  await sitePage.createAvailabilityCard.click();
+  await createAvailabilityPage.btnCreateAvailability.click();
+  await expect(createAvailabilityPage.sessionTittle).toBeVisible();
+  await createAvailabilityPage.selectSession('Weekly sessions');
+  await createAvailabilityPage.btnContinue.click();
+  await createAvailabilityPage.enterWeeklySessionStartDate('27', '10', '2025');
+  await createAvailabilityPage.enterWeeklySessionEndDate('28', '10', '2025');
+  await createAvailabilityPage.btnContinue.click();
+  await createAvailabilityPage.selectDay('Select all days');
+  await createAvailabilityPage.btnContinue.click();
+  await createAvailabilityPage.enterStartTime('09', '00');
+  await createAvailabilityPage.enterEndtTime('10', '00');
+  await createAvailabilityPage.noOfVaccinators('1');
+  await createAvailabilityPage.appointmentLength('5');
+  await createAvailabilityPage.btnContinue.click();
+  await createAvailabilityPage.addServices('RSV (Adult)');
+  await createAvailabilityPage.btnContinue.click();
+  await createAvailabilityPage.btnSaveSession.click();
+  await expect(createAvailabilityPage.sessionSuccessMsg).toBeVisible();
+});

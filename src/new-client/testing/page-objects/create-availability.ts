c333f3e97abc141e5ebf0227081c634ea74291e2
sarithalakkyreddy <saritha.lakkyreddy1@nhs.net>
2024-11-29 12:13:54 +0000

    sync with main

---
+++ src/new-client/testing/page-objects/create-availability.ts
@@ -0,0 +1,105 @@
+import { type Locator, type Page } from '@playwright/test';
+import RootPage from './root';
+
+export default class CreateAvailabilityPage extends RootPage {
+  readonly title: Locator;
+  readonly btnCreateAvailability: Locator;
+  readonly sessionTittle: Locator;
+  readonly btnContinue: Locator;
+  readonly setAndCapacityTittle: Locator;
+  readonly btnSaveSession: Locator;
+  readonly sessionSuccessMsg: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.title = page.getByRole('heading', {
+      name: 'Create availability',
+    });
+
+    this.btnCreateAvailability = page.getByRole('button', {
+      name: 'Create availability',
+    });
+
+    this.sessionTittle = page.getByRole('heading', {
+      name: 'What type of session do you want to create?',
+    });
+
+    this.btnContinue = page.getByRole('button', {
+      name: 'Continue',
+    });
+
+    this.setAndCapacityTittle = page.getByRole('heading', {
+      name: 'Set time and capacity for your session',
+    });
+
+    this.btnSaveSession = page.getByRole('button', {
+      name: 'Save session',
+    });
+
+    this.sessionSuccessMsg = page.getByText(
+      'You have successfully created availability for the current site.',
+    );
+  }
+
+  async selectSession(sessionType: string) {
+    await this.page.getByRole('radio', { name: sessionType }).click();
+  }
+
+  async enterSingleDateSessionDate(day: string, month: string, year: string) {
+    const sessionDateFormGroup = this.page
+      .getByRole('group')
+      .filter({ has: this.page.getByText('Session date') });
+
+    await sessionDateFormGroup.getByLabel('Day').fill(day);
+    await sessionDateFormGroup.getByLabel('Month').fill(month);
+    await sessionDateFormGroup.getByLabel('Year').fill(year);
+  }
+
+  async enterWeeklySessionStartDate(day: string, month: string, year: string) {
+    const startDateFormGroup = this.page
+      .getByRole('group')
+      .filter({ has: this.page.getByText('Start date') });
+
+    await startDateFormGroup.getByLabel('Day').fill(day);
+    await startDateFormGroup.getByLabel('Month').fill(month);
+    await startDateFormGroup.getByLabel('Year').fill(year);
+  }
+
+  async enterWeeklySessionEndDate(day: string, month: string, year: string) {
+    const endDateFormGroup = this.page
+      .getByRole('group')
+      .filter({ has: this.page.getByText('End date') });
+
+    await endDateFormGroup.getByLabel('Day').fill(day);
+    await endDateFormGroup.getByLabel('Month').fill(month);
+    await endDateFormGroup.getByLabel('Year').fill(year);
+  }
+
+  async enterStartTime(hour: string, minute: string) {
+    await this.page.getByLabel('Session start time - hour').fill(hour);
+    await this.page.getByLabel('Session start time - minute').fill(minute);
+  }
+
+  async enterEndtTime(hour: string, minute: string) {
+    await this.page.getByLabel('Session end time - hour').fill(hour);
+    await this.page.getByLabel('Session end time - minute').fill(minute);
+  }
+
+  async noOfVaccinators(numberof: string) {
+    await this.page
+      .getByLabel('How many vaccinators or vaccination spaces do you have?')
+      .fill(numberof);
+  }
+
+  async appointmentLength(length: string) {
+    await this.page.getByLabel('How long are your appointments?').fill(length);
+  }
+
+  async addServices(serviceName: string) {
+    await this.page.getByRole('checkbox', { name: serviceName }).click();
+  }
+
+  async selectDay(Day: string) {
+    await this.page.getByRole('checkbox', { name: Day }).click();
+  }
+}

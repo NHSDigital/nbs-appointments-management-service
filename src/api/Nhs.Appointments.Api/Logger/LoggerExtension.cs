ddf50215e8c471e9d999fa658c3768f77e9a3c1a
Dave-O-NHS <david.olsavsky1@nhs.net>
2024-12-04 13:34:32 +0000

    adding splunk for local development

---
+++ src/api/Nhs.Appointments.Api/Logger/LoggerExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Nhs.Appointments.Api.Logger
+{
+    public static class LoggerExtension
+    {
+        public static IHostBuilder UseAppointmentsSerilog(this IHostBuilder builder)
+        {
+            return builder.UseSerilog((hostingContext, loggerConfiguration) =>
+            {
+                loggerConfiguration.SetupDefaultLoggerConfiguration(hostingContext);
+            });
+        }
+    }
+}
---
+++ src/api/Nhs.Appointments.Api/Logger/SerilogSetupExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+
+namespace Nhs.Appointments.Api.Logger
+{
+    internal static class SerilogSetupExtension
+    {
+        internal static LoggerConfiguration SetupDefaultLoggerConfiguration(this LoggerConfiguration loggerConfiguration, HostBuilderContext hostingContext)
+        {
+            var (splunkHost, eventCollectorToken) = GetSplunkEventCollectorConfig(hostingContext.Configuration);
+
+            loggerConfiguration = loggerConfiguration
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.EventCollector(
+                    splunkHost,
+                    eventCollectorToken,
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information
+                );
+
+            return loggerConfiguration;
+        }
+
+        private static (string splunkHost, string eventCollectorToken) GetSplunkEventCollectorConfig(IConfiguration config)
+        {
+            var splunkHost = Environment.GetEnvironmentVariable("SPLUNK_HOST_URL");
+            var eventCollectorToken = Environment.GetEnvironmentVariable("SPLUNK_HEC_TOKEN");
+
+            return (splunkHost, eventCollectorToken);
+        }
+    }
+}

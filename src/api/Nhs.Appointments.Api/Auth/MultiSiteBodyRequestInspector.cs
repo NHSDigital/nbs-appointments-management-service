7c6ee33af3b527a8822c4fc4bff56de9e6ccfa11
vicr1 <124670733+vicr1@users.noreply.github.com>
2024-12-12 14:00:41 +0000

    Add multi site request inspector (#350)
    * add multi site request inspector
    
    * marked two tests a fixme.
    
    ---------
    
    Co-authored-by: ste-banks <124577664+ste-banks@users.noreply.github.com>
    Co-authored-by: Kim Crowe <131988292+kicr1@users.noreply.github.com>

--- src/api/Nhs.Appointments.Api/Auth/MultiSiteBodyRequestInspector.cs
+++ src/api/Nhs.Appointments.Api/Auth/MultiSiteBodyRequestInspector.cs
@@ -4,10 +4,9 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker.Http;
 
-
 namespace Nhs.Appointments.Api.Auth;
 
-public class SiteFromScopeInspector : IRequestInspector
+public class MultiSiteBodyRequestInspector : IRequestInspector
 {
     public async Task<IEnumerable<string>> GetSiteIds(HttpRequestData httpRequest)
     {
@@ -18,10 +17,9 @@ public class SiteFromScopeInspector : IRequestInspector
             try
             {
                 using var jsonDocument = JsonDocument.Parse(body);
-                if (jsonDocument.RootElement.TryGetProperty("scope", out var scopeValue))
+                if (jsonDocument.RootElement.TryGetProperty("sites", out var sitesElement))
                 {
-                    if (scopeValue.ToString().StartsWith("site:"))
-                        return [scopeValue.ToString().Replace("site:", "")];
+                    return sitesElement.Deserialize<string[]>();
                 }
             }
             catch (JsonException)

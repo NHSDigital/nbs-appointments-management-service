e7e458dcbab3c237a3257abecd8c8bcd129fdafe
sambiramairelogic <samuel.biram@airelogic.com>
2025-01-06 15:44:45 +0000

    Abstract Availability Calculator method so it can be used elsewhere

--- src/api/Nhs.Appointments.Core/AvailabilityCalculator.cs
+++ src/api/Nhs.Appointments.Core/AvailabilityCalculator.cs
@@ -3,10 +3,18 @@
 public interface IAvailabilityCalculator
 {
     Task<IEnumerable<SessionInstance>> CalculateAvailability(string site, string service, DateOnly from, DateOnly until);
+
+    IEnumerable<SessionInstance> GetAvailableSlots(IEnumerable<SessionInstance> sessions,
+        IEnumerable<Booking> bookings);
 }
 
 public class AvailabilityCalculator(IAvailabilityStore availabilityStore, IBookingsDocumentStore bookingDocumentStore, TimeProvider time) : IAvailabilityCalculator
 {
+    private readonly AppointmentStatus[] _liveStatuses = [AppointmentStatus.Booked, AppointmentStatus.Provisional];
+
+    private bool IsExpiredProvisional(Booking b) =>
+        b.Status == AppointmentStatus.Provisional && b.Created < time.GetUtcNow().AddMinutes(-5);
+
     public async Task<IEnumerable<SessionInstance>> CalculateAvailability(string site, string service, DateOnly from, DateOnly until)
     {
         var allSessions = await availabilityStore.GetSessions(site, from, until);
@@ -14,33 +22,42 @@ public class AvailabilityCalculator(IAvailabilityStore availabilityStore, IBooki
             ? allSessions
             : allSessions.Where(s => s.Services.Contains(service));
 
-        if (filteredSessions.Any())
+        var bookings = await bookingDocumentStore.GetInDateRangeAsync(from.ToDateTime(new TimeOnly(0, 0)),
+            until.ToDateTime(new TimeOnly(23, 59)), site);
+
+        return GetAvailableSlots(filteredSessions, bookings);
+    }
+
+    public IEnumerable<SessionInstance> GetAvailableSlots(IEnumerable<SessionInstance> sessions,
+        IEnumerable<Booking> bookings)
+    {
+        var slots = new List<SessionInstance>();
+        foreach (var session in sessions)
         {
-            var slots = new List<SessionInstance>();
-            foreach (var session in filteredSessions)
-            {
-                slots.AddRange(session.Divide(TimeSpan.FromMinutes(session.SlotLength)).Select(sl => new SessionInstance(sl) { Services = session.Services, Capacity = session.Capacity }));
-            }
-
-            var bookings = await bookingDocumentStore.GetInDateRangeAsync(from.ToDateTime(new TimeOnly(0, 0)), until.ToDateTime(new TimeOnly(23, 59)), site);
-
-            var liveStatuses = new[] { AppointmentStatus.Booked, AppointmentStatus.Provisional };
-            var isExpiredProvisional = (Booking b) => b.Status == AppointmentStatus.Provisional && b.Created < time.GetUtcNow().AddMinutes(-5);
-            var liveBookings = bookings
-                .Where(b => liveStatuses.Contains(b.Status))
-                .Where(b => isExpiredProvisional(b) == false);
-
-            foreach (var booking in liveBookings)
-            {
-                var slot = slots.FirstOrDefault(s => s.Capacity > 0 && s.From == booking.From && s.Duration.TotalMinutes == booking.Duration && s.Services.Contains(booking.Service));
-                if(slot != null)
-                {
-                    slot.Capacity--;
-                }
-            }
-
-            return slots.Where(s => s.Capacity > 0);
+            slots.AddRange(session.Divide(TimeSpan.FromMinutes(session.SlotLength)).Select(sl =>
+                new SessionInstance(sl) { Services = session.Services, Capacity = session.Capacity }));
         }
-        return Enumerable.Empty<SessionInstance>();
+
+        if (slots.Count == 0)
+        {
+            return [];
+        }
+
+        var liveBookings = bookings
+            .Where(b => _liveStatuses.Contains(b.Status))
+            .Where(b => IsExpiredProvisional(b) == false);
+
+        foreach (var booking in liveBookings)
+        {
+            var slot = slots.FirstOrDefault(s =>
+                s.Capacity > 0 && s.From == booking.From && (int)s.Duration.TotalMinutes == booking.Duration &&
+                s.Services.Contains(booking.Service));
+            if (slot != null)
+            {
+                slot.Capacity--;
+            }
+        }
+
+        return slots.Where(s => s.Capacity > 0);
     }
 }

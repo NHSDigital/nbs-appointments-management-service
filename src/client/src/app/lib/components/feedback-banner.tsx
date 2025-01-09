4e1526143b452e787f36198edbb6da1f7462d17a
Dave-O-NHS <david.olsavsky1@nhs.net>
2025-01-09 11:51:42 +0000

    replacing non-breaking space tag with white space

--- src/client/src/app/lib/components/feedback-banner.tsx
+++ src/client/src/app/lib/components/feedback-banner.tsx
@@ -4,16 +4,14 @@ import '../../global.css';
 const FeedbackBanner = () => {
   return (
     <div className="feedback-banner">
-      <Tag text="Feedback" />
-      &nbsp;
+      <Tag text="Feedback" />{' '}
       <a
         href="https://feedback.digital.nhs.uk/jfe/form/SV_5AROlphcOb5wfEq"
         target="_blank"
         rel="noopener noreferrer"
       >
         Give page or site feedback (opens in a new tab)
-      </a>
-      &nbsp;
+      </a>{' '}
       <span>
         - this is a new service, your feedback will help us improve it.
       </span>

54a66d9d5f58295abe00870fc02176b933664db5
sambiramairelogic <samuel.biram@airelogic.com>
2025-01-09 10:10:03 +0000

    Expand SetAvailabilityRequest to accept Edit mode

--- tests/Nhs.Appointments.Api.UnitTests/Validators/SetAvailabilityRequestValidatorTests.cs
+++ tests/Nhs.Appointments.Api.UnitTests/Validators/SetAvailabilityRequestValidatorTests.cs
@@ -1,8 +1,8 @@
 ﻿using FluentAssertions;
+using Moq;
 using Nhs.Appointments.Api.Availability;
 using Nhs.Appointments.Api.Validators;
 using Nhs.Appointments.Core;
-using Moq;
 
 namespace Nhs.Appointments.Api.Tests.Validators
 {
@@ -103,6 +103,67 @@ namespace Nhs.Appointments.Api.Tests.Validators
             result.Errors.Single().ErrorMessage.Should().Be("Date must be at least 1 day in the future");
         }
 
+        [Fact]
+        public void ReturnsValid_WhenRequestInEditModeIsValid()
+        {
+            var request = new SetAvailabilityRequest(
+                new DateOnly(2024, 10, 10),
+                "test-site",
+                [
+                    new Session
+                    {
+                        Capacity = 5,
+                        From = TimeOnly.FromDateTime(new DateTime(2024, 10, 10, 9, 0, 0)),
+                        Until = TimeOnly.FromDateTime(new DateTime(2024, 10, 10, 12, 0, 0)),
+                        Services = ["RSV", "COVID"],
+                        SlotLength = 5
+                    }
+                ],
+                ApplyAvailabilityMode.Edit,
+                SetupValidSessions().First());
+
+            var result = _sut.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ReturnsError_IfModeIsEditButNoSessionToEditIsProvided()
+        {
+            var request = new SetAvailabilityRequest(
+                new DateOnly(2024, 10, 10),
+                "test-site",
+                [SetupValidSessions().First()],
+                ApplyAvailabilityMode.Edit);
+
+            var result = _sut.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().HaveCount(1);
+            result.Errors.Single().PropertyName.Should().Be(nameof(SetAvailabilityRequest.SessionToEdit));
+            result.Errors.Single().ErrorMessage.Should()
+                .Be("A session to edit must be provided when mode is set to Edit");
+        }
+
+        [Fact]
+        public void ReturnsError_IfModeIsEditAndMultipleSessionsAreProvided()
+        {
+            var request = new SetAvailabilityRequest(
+                new DateOnly(2024, 10, 10),
+                "test-site",
+                SetupValidSessions(),
+                ApplyAvailabilityMode.Edit,
+                SetupValidSessions().First());
+
+            var result = _sut.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().HaveCount(1);
+            result.Errors.Single().PropertyName.Should().Be(nameof(SetAvailabilityRequest.Sessions));
+            result.Errors.Single().ErrorMessage.Should()
+                .Be("Only one edited session can be provided when mode is set to Edit");
+        }
+
         private static Session[] SetupValidSessions()
         {
             return
@@ -111,8 +172,16 @@ namespace Nhs.Appointments.Api.Tests.Validators
                 {
                     Capacity = 1,
                     From = TimeOnly.FromDateTime(new DateTime(2024, 10, 10, 9, 0, 0)),
-                    Until = TimeOnly.FromDateTime(new DateTime(2024, 10, 10, 16, 0, 0)),
+                    Until = TimeOnly.FromDateTime(new DateTime(2024, 10, 10, 12, 0, 0)),
                     Services = ["RSV", "COVID"],
+                    SlotLength = 5
+                },
+                new Session
+                {
+                    Capacity = 2,
+                    From = TimeOnly.FromDateTime(new DateTime(2024, 10, 10, 13, 0, 0)),
+                    Until = TimeOnly.FromDateTime(new DateTime(2024, 10, 10, 17, 0, 0)),
+                    Services = ["RSV"],
                     SlotLength = 5
                 }
             ];

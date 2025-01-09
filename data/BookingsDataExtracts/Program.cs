efb2e6aa6679a2937b7e21cf6439aacbd7fa18b5
Vincent Crowe <vincent.crowe1@nhs.net>
2025-01-09 12:02:02 +0000

    moving env vars so they can bne used to locate the kv

--- data/BookingsDataExtracts/Program.cs
+++ data/BookingsDataExtracts/Program.cs
@@ -6,8 +6,8 @@ builder.Configuration.Sources.Clear();
 builder.Configuration
             .SetBasePath(AppContext.BaseDirectory)
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddNbsAzureKeyVault()
-            .AddEnvironmentVariables();
+            .AddEnvironmentVariables()
+            .AddNbsAzureKeyVault();            
 
 builder.Services
     .AddDataExtractServices(builder.Configuration)

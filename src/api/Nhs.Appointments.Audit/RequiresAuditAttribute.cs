a391ba04f425f025ca2a387c4b155c9d31f03391
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-01-09 15:29:32 +0000

    Commit Audit Files

---
+++ src/api/Nhs.Appointments.Audit/RequiresAuditAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace Nhs.Appointments.Audit;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class RequiresAuditAttribute(Type? requestSiteInspector) : Attribute
+{
+    public Type? RequestSiteInspector { get; } = requestSiteInspector;
+}

5d55b290910f7c915a2cb77f4960628b70d27328
Dave-O-NHS <david.olsavsky1@nhs.net>
2025-01-09 12:20:13 +0000

    component alignment changes

--- src/client/src/app/lib/components/feedback-banner.tsx
+++ src/client/src/app/lib/components/feedback-banner.tsx
@@ -3,17 +3,21 @@ import Tag from '@components/nhsuk-frontend/tag';
 const FeedbackBanner = () => {
   return (
     <div className="feedback-banner">
-      <Tag text="Feedback" />{' '}
-      <a
-        href="https://feedback.digital.nhs.uk/jfe/form/SV_5AROlphcOb5wfEq"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Give page or site feedback (opens in a new tab)
-      </a>{' '}
-      <span>
-        - this is a new service, your feedback will help us improve it.
-      </span>
+      <div className="feedback-banner__container">
+        <div>
+          <Tag text="Feedback" />{' '}
+          <a
+            href="https://feedback.digital.nhs.uk/jfe/form/SV_5AROlphcOb5wfEq"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Give page or site feedback (opens in a new tab)
+          </a>{' '}
+          <span>
+            - this is a new service, your feedback will help us improve it.
+          </span>
+        </div>
+      </div>
     </div>
   );
 };

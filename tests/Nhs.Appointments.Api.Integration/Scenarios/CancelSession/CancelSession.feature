ac5e6f2eeb81d024f31efbac5d1c5160cc2d5e89
ste-banks <stephen.banks4@nhs.net>
2025-01-10 12:59:56 +0000

    Small test update

--- tests/Nhs.Appointments.Api.Integration/Scenarios/CancelSession/CancelSession.feature
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/CancelSession/CancelSession.feature
@@ -3,11 +3,11 @@ Feature: Cancel a session
   Scenario: Cancel a session
     Given the following sessions
       | Date     | From  | Until | Services | Slot Length | Capacity |
-      | Tomorrow | 09:00 | 17:00 | COVID    | 5           | 2        |
+      | Tomorrow | 09:00 | 10:00 | COVID    | 5           | 1        |
     And the following bookings have been made
       | Date     | Time  | Duration | Service | Reference   |
-      | Tomorrow | 09:30 | 5        | COVID   | 68374-29374 |
+      | Tomorrow | 09:45 | 5        | COVID   | 68537-45913 |
     When I cancel the following session
       | Date     | From  | Until | Services | Slot Length | Capacity |
-      | Tomorrow | 09:00 | 17:00 | COVID    | 5           | 2        |
-    Then the booking with reference '68374-29374' has been 'Orphaned'
+      | Tomorrow | 09:00 | 10:00 | COVID    | 5           | 1        |
+    Then the booking with reference '68537-45913' has been 'Orphaned'
--- tests/Nhs.Appointments.Api.Integration/Scenarios/CancelSession/CancelSessionFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/CancelSession/CancelSessionFeatureSteps.cs
@@ -16,16 +16,17 @@ public class CancelSessionFeatureSteps : BaseFeatureSteps
     public async Task CancelSession(DataTable dataTable)
     {
         var cells = dataTable.Rows.ElementAt(1).Cells;
+        var date = DateTime.ParseExact(ParseNaturalLanguageDateOnly(cells.ElementAt(0).Value).ToString("yyyy-MM-dd"), "yyyy-MM-dd", null);
 
         object payload = new
         {
             site = GetSiteId(),
-            date = DateTime.ParseExact(ParseNaturalLanguageDateOnly(cells.ElementAt(0).Value).ToString("yyyy-MM-dd"), "yyyy-MM-dd", null),
-            from = cells.ElementAt(1).Value,
+            date = DateOnly.FromDateTime(date),
             until = cells.ElementAt(2).Value,
+            from = cells.ElementAt(1).Value,
             services = new string[] { cells.ElementAt(3).Value },
-            slotLength = cells.ElementAt(4).Value,
-            capacity = cells.ElementAt(5).Value
+            slotLength = int.Parse(cells.ElementAt(4).Value),
+            capacity = int.Parse(cells.ElementAt(5).Value)
         };
 
         Response = await Http.PostAsJsonAsync("http://localhost:7071/api/session/cancel", payload);

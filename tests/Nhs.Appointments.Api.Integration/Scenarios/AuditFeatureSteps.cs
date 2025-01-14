1ee933c371adb08b6bda39171f17762273f79a19
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-01-14 11:40:23 +0000

    Add retry with timeout option for fetching the audit data from DB.

--- tests/Nhs.Appointments.Api.Integration/Scenarios/AuditFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/AuditFeatureSteps.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Microsoft.Azure.Cosmos;
 using Nhs.Appointments.Audit.Persistance;
 using Xunit.Gherkin.Quick;
 
@@ -21,13 +22,33 @@ public abstract class AuditFeatureSteps : BaseFeatureSteps
         var timestamp = DateTime.UtcNow;
         var siteId = GetSiteId();
         var container = Client.GetContainer("appts", "audit_data");
-        var auditFunctionDocuments = await RunQueryAsync<AuditFunctionDocument>(container,
-            d => d.User == user && d.FunctionName == functionName && d.Site == siteId);
 
-        var latestDocument = auditFunctionDocuments.OrderByDescending(d => d.Timestamp).Single();
+        var auditFunctionDocument = await FindItemWithRetryAsync(container, user, functionName, siteId,
+            TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(1));
 
         //confirm that the document was created recently, as cannot easily verify via id query
-        latestDocument.Timestamp.Should().BeBefore(timestamp);
-        latestDocument.Timestamp.Should().BeOnOrAfter(timestamp.AddSeconds(-30));
+        auditFunctionDocument.Timestamp.Should().BeBefore(timestamp);
+        auditFunctionDocument.Timestamp.Should().BeOnOrAfter(timestamp.AddSeconds(-10));
+    }
+
+    private static async Task<AuditFunctionDocument> FindItemWithRetryAsync(Container container, string user,
+        string functionName, string siteId, TimeSpan timeout, TimeSpan delay)
+    {
+        var startTime = DateTime.UtcNow;
+
+        while (DateTime.UtcNow - startTime < timeout)
+        {
+            var documentsFound = (await RunQueryAsync<AuditFunctionDocument>(container,
+                d => d.User == user && d.FunctionName == functionName && d.Site == siteId)).ToList();
+
+            if (documentsFound.Count > 0)
+            {
+                return documentsFound.Single(); //only one item should be found
+            }
+
+            await Task.Delay(delay); // Wait before retrying
+        }
+
+        throw new TimeoutException("AuditFunctionDocument not found within the timeout period.");
     }
 }

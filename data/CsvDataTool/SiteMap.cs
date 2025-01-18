301530b69988f121b8fd81fe3013c3595ef918f0
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-01-18 13:18:43 +0000

    Generate a random Guid string for the SiteID in the CSVProcessor. Move the first field to be the ODSCode as a new property.

--- data/CsvDataTool/SiteMap.cs
+++ data/CsvDataTool/SiteMap.cs
@@ -8,7 +8,10 @@ public class SiteMap : ClassMap<SiteDocument>
 {
     public SiteMap()
     {
-        Map(m => m.Id).Name("ID");
+        //ignore ID property and generate programatically
+        Map(m => m.Id).Ignore();
+
+        Map(m => m.OdsCode).Name("OdsCode");
         Map(m => m.Name).Name("Name");
         Map(m => m.Address).Name("Address");
         Map(m => m.PhoneNumber).Name("PhoneNumber");
@@ -16,23 +19,32 @@ public class SiteMap : ClassMap<SiteDocument>
             new Location(
                 "Point",
                 [x.Row.GetField<double>("Longitude"), x.Row.GetField<double>("Latitude")]
-                ));
+            ));
         Map(m => m.IntegratedCareBoard).Name("ICB");
         Map(m => m.Region).Name("Region");
         Map(m => m.DocumentType).Constant("site");
         Map(m => m.AttributeValues).Convert(x =>
         {
-            AttributeValue[] result = [
-                new AttributeValue("accessibility/accessible_toilet", ParseUserEnteredBoolean(x.Row["accessible_toilet"]).ToString()),
-                new AttributeValue("accessibility/braille_translation_service", ParseUserEnteredBoolean(x.Row["braille_translation_service"]).ToString()),
-                new AttributeValue("accessibility/disabled_car_parking", ParseUserEnteredBoolean(x.Row["disabled_car_parking"]).ToString()),
-                new AttributeValue("accessibility/car_parking", ParseUserEnteredBoolean(x.Row["car_parking"]).ToString()),
-                new AttributeValue("accessibility/induction_loop", ParseUserEnteredBoolean(x.Row["induction_loop"]).ToString()),
-                new AttributeValue("accessibility/sign_language_service", ParseUserEnteredBoolean(x.Row["sign_language_service"]).ToString()),
-                new AttributeValue("accessibility/step_free_access", ParseUserEnteredBoolean(x.Row["step_free_access"]).ToString()),
+            AttributeValue[] result =
+            [
+                new AttributeValue("accessibility/accessible_toilet",
+                    ParseUserEnteredBoolean(x.Row["accessible_toilet"]).ToString()),
+                new AttributeValue("accessibility/braille_translation_service",
+                    ParseUserEnteredBoolean(x.Row["braille_translation_service"]).ToString()),
+                new AttributeValue("accessibility/disabled_car_parking",
+                    ParseUserEnteredBoolean(x.Row["disabled_car_parking"]).ToString()),
+                new AttributeValue("accessibility/car_parking",
+                    ParseUserEnteredBoolean(x.Row["car_parking"]).ToString()),
+                new AttributeValue("accessibility/induction_loop",
+                    ParseUserEnteredBoolean(x.Row["induction_loop"]).ToString()),
+                new AttributeValue("accessibility/sign_language_service",
+                    ParseUserEnteredBoolean(x.Row["sign_language_service"]).ToString()),
+                new AttributeValue("accessibility/step_free_access",
+                    ParseUserEnteredBoolean(x.Row["step_free_access"]).ToString()),
                 new AttributeValue("accessibility/text_relay", ParseUserEnteredBoolean(x.Row["text_relay"]).ToString()),
-                new AttributeValue("accessibility/wheelchair_access", ParseUserEnteredBoolean(x.Row["wheelchair_access"]).ToString()),
-                ];
+                new AttributeValue("accessibility/wheelchair_access",
+                    ParseUserEnteredBoolean(x.Row["wheelchair_access"]).ToString()),
+            ];
 
             return result;
         });
@@ -44,6 +56,3 @@ public class SiteMap : ClassMap<SiteDocument>
         return possibleBool == "true" || possibleBool == "yes";
     }
 }
-
-
-

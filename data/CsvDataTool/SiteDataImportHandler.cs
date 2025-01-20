fbc4f08c932008868c53506ff081a50769ba777a
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-01-20 12:06:33 +0000

    Refactor CSVProcessor to include optional mutateDocument action

--- data/CsvDataTool/SiteDataImportHandler.cs
+++ data/CsvDataTool/SiteDataImportHandler.cs
@@ -6,15 +6,21 @@ public class SiteDataImportHandler(IFileOperations fileOperations) : IDataImport
 {
     public Task<IEnumerable<ReportItem>> ProcessFile(FileInfo inputFile, DirectoryInfo outputFolder)
     {
-        var processor = new CsvProcessor<SiteDocument, SiteMap>(s => WriteSiteDocument(s, outputFolder), s => s.Name);
+        var processor = new CsvProcessor<SiteDocument, SiteMap>(s => WriteSiteDocument(s, outputFolder), s => s.Name, MutateSiteDocument);
         using var fileReader = fileOperations.OpenText(inputFile);
         return processor.ProcessFile(fileReader);
     }
 
-    protected virtual Task WriteSiteDocument(SiteDocument siteDocument, DirectoryInfo outputDirectory)
+    private Task WriteSiteDocument(SiteDocument siteDocument, DirectoryInfo outputDirectory)
     {
         fileOperations.CreateFolder(outputDirectory);
         var filePath = Path.Combine(outputDirectory.FullName, $"site_{siteDocument.Id}.json");
         return fileOperations.WriteDocument(siteDocument, filePath);
+    }
+    
+    private static SiteDocument MutateSiteDocument(SiteDocument siteDocument)
+    {
+        siteDocument.Id = Guid.NewGuid().ToString();
+        return siteDocument;
     }
 }

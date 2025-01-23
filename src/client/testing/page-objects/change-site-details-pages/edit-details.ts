dea80dcfd7333014c2b9710cdb7dc8badd9ab0e2
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-01-23 16:33:12 +0000

    Edit details spec and page object

---
+++ src/client/testing/page-objects/change-site-details-pages/edit-details.ts
@@ -0,0 +1,50 @@
+import { type Locator, type Page } from '@playwright/test';
+import RootPage from '../root';
+
+export default class EditDetailsPage extends RootPage {
+  readonly title: Locator;
+  readonly saveAndContinueButton: Locator;
+  readonly backLink: Locator;
+  readonly closeNotificationBannerButton: Locator;
+  readonly nameInput: Locator;
+  readonly addressInput: Locator;
+  readonly latitudeInput: Locator;
+  readonly longitudeInput: Locator;
+  readonly phoneNumberInput: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.title = page.getByRole('heading', {
+      name: 'Edit site details',
+    });
+    this.saveAndContinueButton = page.getByRole('button', {
+      name: 'Save and continue',
+    });
+    this.backLink = page
+      .getByRole('link')
+      .filter({ hasText: 'Back to site details' });
+    this.closeNotificationBannerButton = page.getByRole('button', {
+      name: 'Close',
+    });
+
+    this.nameInput = page.getByRole('textbox', {
+      name: 'name',
+    });
+
+    this.addressInput = page.getByRole('textbox', {
+      name: 'Site address',
+    });
+
+    this.latitudeInput = page.getByRole('textbox', {
+      name: 'Latitude',
+    });
+
+    this.longitudeInput = page.getByRole('textbox', {
+      name: 'Longitude',
+    });
+
+    this.phoneNumberInput = page.getByRole('textbox', {
+      name: 'phoneNumber',
+    });
+  }
+}

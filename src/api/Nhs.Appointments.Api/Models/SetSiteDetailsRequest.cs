84c8a5e3e29263a39818ec69ee30908576a39294
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-01-24 16:37:15 +0000

    Update request class to parse result after validation

--- src/api/Nhs.Appointments.Api/Models/SetSiteDetailsRequest.cs
+++ src/api/Nhs.Appointments.Api/Models/SetSiteDetailsRequest.cs
@@ -1,13 +1,38 @@
-﻿namespace Nhs.Appointments.Api.Models;
+﻿using static System.Decimal;
 
-public record SetSiteDetailsRequest(
-    string Site,
-    string Name,
-    string Address,
-    string PhoneNumber,
-    string Latitude,
-    string Longitude)
+namespace Nhs.Appointments.Api.Models;
+
+public sealed class SetSiteDetailsRequest
 {
-    public decimal LatitudeDecimal => decimal.Parse(Latitude);
-    public decimal LongitudeDecimal => decimal.Parse(Longitude);
-};
+    public SetSiteDetailsRequest(
+        string site,
+        string name,
+        string address,
+        string phoneNumber,
+        string latitude,
+        string longitude)
+    {
+        Site = site;
+        Name = name;
+        Address = address;
+        PhoneNumber = phoneNumber;
+        Latitude = latitude;
+        Longitude = longitude;
+
+        TryParse(Latitude, out var latDecimal);
+        LatitudeDecimal = latDecimal;
+        TryParse(Longitude, out var longDecimal);
+        LongitudeDecimal = longDecimal;
+    }
+
+    public string Site { get; }
+    public string Name { get; }
+    public string Address { get; }
+    public string PhoneNumber { get; }
+
+    public decimal LatitudeDecimal { get; }
+    public string Latitude { get; }
+
+    public decimal LongitudeDecimal { get; }
+    public string Longitude { get; }
+}

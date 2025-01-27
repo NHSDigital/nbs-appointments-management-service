7ef39f7f572dc6ce85420be158f64e5663afe9b5
Kim Crowe <kim.crowe4@nhs.net>
2025-01-27 15:24:43 +0000

    Pipeline and script to clean up any old cicd cosmos accounts

---
+++ scripts/cosmos-build/delete_cosmos_db_scheduled.ps1
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/env pwsh
+param (
+  [string][Parameter(Mandatory)]$resourceGroup,
+  [string][Parameter(Mandatory)]$subscriptionId
+)
+
+$ErrorActionPreference = "Stop"
+$DebugPreference = "Continue"
+
+$cosmosAccountNames = @(az cosmosdb list --resource-group $resourceGroup --query "[].name" --output tsv)
+
+foreach ($cosmosAccountName in $cosmosAccountNames) {
+  Write-Host("Deleting cosmos account " + $cosmosAccountName)
+  az cosmosdb delete `
+      --name $cosmosAccountName `
+      --resource-group $resourceGroup `
+      --subscription $subscriptionId `
+      --yes `
+      --no-wait `
+}

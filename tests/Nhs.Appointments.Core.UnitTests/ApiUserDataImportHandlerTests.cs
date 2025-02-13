7aee041627d46cafb0dea248fb275e20d3bdc3b2
ste-banks <stephen.banks4@nhs.net>
2025-02-13 16:43:43 +0000

    WIP - Adding unit tests around data import handlers

---
+++ tests/Nhs.Appointments.Core.UnitTests/ApiUserDataImportHandlerTests.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Http.Internal;
+using System.Text;
+
+namespace Nhs.Appointments.Core.UnitTests;
+public class ApiUserDataImportHandlerTests
+{
+    private readonly Mock<IUserService> _userServiceMock = new();
+
+    private readonly ApiUserDataImportHandler _sut;
+    private const string ApiUserHeader = "ClientId,ApiSigningKey";
+
+    public ApiUserDataImportHandlerTests()
+    {
+        _sut = new ApiUserDataImportHandler(_userServiceMock.Object);
+    }
+
+    [Fact]
+    public async Task CanReadApiUserData()
+    {
+        string[] inputRows =
+        [
+            "test1,ABC123",
+            "test2,DEF345"
+        ];
+
+        var input = CsvFileBuilder.BuildInputCsv(ApiUserHeader, inputRows);
+
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(input));
+        var file = new FormFile(stream, 0, stream.Length, "Test", "test.csv");
+
+        var report = await _sut.ProcessFile(file);
+
+        report.Count().Should().Be(2);
+        report.All(r => r.Success).Should().BeTrue();
+    }
+}

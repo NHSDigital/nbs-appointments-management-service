aa8419fa851ffbed8dcce72f7a12e4e68f9ba663
pata9 <paul.tallet1@nhs.net>
2025-02-16 14:38:18 +0000

    typo

--- src/api/Nhs.Appointments.Core/SessionInstanceExtensions.cs
+++ src/api/Nhs.Appointments.Core/SessionInstanceExtensions.cs
@@ -1,7 +1,7 @@
 namespace Nhs.Appointments.Core;
 public static class SessionInstanceExtensions
 {
-    // Initialised added to support joint bookings
+    // Initially added to support joint bookings
     // This method will group consecutive availability by the consecutive input so that availability calculations will take into account consecutive bookings
     // Consecutive availability = availability that are one after another
     public static IEnumerable<SessionInstance> GroupByConsecutive(this IEnumerable<SessionInstance> slots, int consecutive)

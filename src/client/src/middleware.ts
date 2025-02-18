fa1865d06baa87e9ea1bf818f69c8850885fb1ea
sambiramairelogic <samuel.biram@airelogic.com>
2025-02-18 18:12:50 +0000

    Revert unnecessary middleware tests

--- src/client/src/middleware.ts
+++ src/client/src/middleware.ts
@@ -4,26 +4,22 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const pathAndQuery = `${request.nextUrl.pathname}${request.nextUrl.search}`;
 
-  const shouldHandle = !request.nextUrl.pathname.includes('/api/');
+  const nextArguments = request.nextUrl.pathname.endsWith('login')
+    ? {}
+    : {
+        headers: {
+          'mya-last-requested-path': pathAndQuery,
+        },
+      };
 
-  if (shouldHandle) {
-    const nextArguments = request.nextUrl.pathname.endsWith('login')
-      ? {}
-      : {
-          headers: {
-            'mya-last-requested-path': pathAndQuery,
-          },
-        };
+  const response = NextResponse.next(nextArguments);
 
-    const response = NextResponse.next(nextArguments);
+  response.headers.set(
+    'x-forwarded-host',
+    request.headers.get('origin')?.replace(/(http|https):\/\//, '') || '*',
+  );
 
-    response.headers.set(
-      'x-forwarded-host',
-      request.headers.get('origin')?.replace(/(http|https):\/\//, '') || '*',
-    );
-
-    return response;
-  }
+  return response;
 }
 
 export const config = {

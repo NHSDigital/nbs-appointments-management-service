76a1bbe93197d5ae45631759fec60f7afead2643
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-02-18 16:26:47 +0000

    Attempt 3

--- src/client/src/middleware.ts
+++ src/client/src/middleware.ts
@@ -4,27 +4,31 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const pathAndQuery = `${request.nextUrl.pathname}${request.nextUrl.search}`;
 
-  const nextArguments = request.nextUrl.pathname.endsWith('login')
-    ? {}
-    : {
-        headers: {
-          'mya-last-requested-path': pathAndQuery,
-        },
-      };
+  const shouldHandle = !request.nextUrl.pathname.includes('/api/');
 
-  const response = NextResponse.next(nextArguments);
+  if (shouldHandle) {
+    const nextArguments = request.nextUrl.pathname.endsWith('login')
+      ? {}
+      : {
+          headers: {
+            'mya-last-requested-path': pathAndQuery,
+          },
+        };
 
-  response.headers.set(
-    'x-forwarded-host',
-    request.headers.get('origin')?.replace(/(http|https):\/\//, '') || '*',
-  );
+    const response = NextResponse.next(nextArguments);
 
-  return response;
+    response.headers.set(
+      'x-forwarded-host',
+      request.headers.get('origin')?.replace(/(http|https):\/\//, '') || '*',
+    );
+
+    return response;
+  }
 }
 
 export const config = {
   matcher: [
     // All routes except /login, /api, /_next, and /favicon.ico
-    '/((?!manage-your-appointments/api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 };

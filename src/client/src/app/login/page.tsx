b0a9e36c37c65531605509db9fe301a700b1fb63
Jonny Dyson <193929923+jsed-nhs@users.noreply.github.com>
2025-02-18 21:22:47 +0000

    Appt xxx outputvars (#533)
    Revert
    https://github.com/NHSDigital/nbs-appointments-management-service/pull/531
    Output missing vars hidden
    
    ---------
    
    Co-authored-by: Sam Biram <63285990+sambiramairelogic@users.noreply.github.com>

--- src/client/src/app/login/page.tsx
+++ src/client/src/app/login/page.tsx
@@ -2,30 +2,35 @@ import { Metadata } from 'next';
 import LogInButton from './log-in-button';
 import NhsAnonymousPage from '@components/nhs-anonymous-page';
 
+export type LoginPageProps = {
+  searchParams?: {
+    redirectUrl?: string;
+  };
+};
+
 export const metadata: Metadata = {
   title: 'Manage your appointments',
   description: 'A National Booking Service site for managing NHS appointments',
 };
 
-const Page = async () => {
-  if (!process.env.AUTH_HOST) {
-    throw new Error('AUTH_HOST environment variable is not set');
-  }
-
+const Page = async ({ searchParams }: LoginPageProps) => {
+  const redirectUrl = searchParams?.redirectUrl ?? '/';
   return (
     <NhsAnonymousPage title="Manage your appointments" originPage="login">
       <p>
         You are currently not signed in. You must sign in to access this
         service.
       </p>
+      <p style={{ display: 'none' }}>
+        Auth: {process.env.AUTH_HOST} Base Url: {process.env.NBS_API_BASE_URL}
+      </p>
       <LogInButton
-        authHost={process.env.AUTH_HOST}
+        redirectUrl={redirectUrl}
         provider={'nhs-mail'}
         friendlyName={'NHS Mail'}
       />
-      <br />
       <LogInButton
-        authHost={process.env.AUTH_HOST}
+        redirectUrl={redirectUrl}
         provider={'okta'}
         friendlyName={'Other Email'}
       />

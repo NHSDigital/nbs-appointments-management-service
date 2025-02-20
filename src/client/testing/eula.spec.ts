74ff85ae27105bdfbc7e9187f93f0cdc38aeedca
sambiramairelogic <samuel.biram@airelogic.com>
2025-02-20 09:33:49 +0000

    Remove the fixme on the eula tests as they pass fine

--- src/client/testing/eula.spec.ts
+++ src/client/testing/eula.spec.ts
@@ -16,28 +16,28 @@ test.beforeEach(async ({ page }) => {
   eulaConsentPage = new EulaConsentPage(page);
 });
 
-test.fixme(
-  'A user with an out of date EULA consent version is prompted with the EULA consent page',
-  async ({ page, getTestUser }) => {
-    const user5 = getTestUser(5);
-    await rootPage.goto();
-    await rootPage.pageContentLogInButton.click();
+test('A user with an out of date EULA consent version is prompted with the EULA consent page', async ({
+  page,
+  getTestUser,
+}) => {
+  const user5 = getTestUser(5);
+  await rootPage.goto();
+  await rootPage.pageContentLogInButton.click();
 
-    await oAuthPage.page.getByLabel('Username').fill(user5.username);
-    await oAuthPage.page.getByLabel('Password').fill(user5.password);
-    await oAuthPage.page.getByLabel('Password').press('Enter');
+  await oAuthPage.page.getByLabel('Username').fill(user5.username);
+  await oAuthPage.page.getByLabel('Password').fill(user5.password);
+  await oAuthPage.page.getByLabel('Password').press('Enter');
 
-    await page.waitForURL('**/eula');
-    await expect(eulaConsentPage.title).toBeVisible();
+  await page.waitForURL('**/eula');
+  await expect(eulaConsentPage.title).toBeVisible();
 
-    // Try to bypass EULA consent
-    await page.goto('/');
+  // Try to bypass EULA consent
+  await page.goto('/');
 
-    await page.waitForURL('**/eula');
-    await expect(eulaConsentPage.title).toBeVisible();
-    await expect(siteSelectionPage.title).not.toBeVisible();
-  },
-);
+  await page.waitForURL('**/eula');
+  await expect(eulaConsentPage.title).toBeVisible();
+  await expect(siteSelectionPage.title).not.toBeVisible();
+});
 
 test('A user with an out of date EULA version is prompted with the EULA consent page on login, but not again after they have consented', async ({
   page,

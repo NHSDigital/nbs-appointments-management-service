4337b8bed730cb011ed5b0119102d480e0dd6f9b
sambiramairelogic <samuel.biram@airelogic.com>
2025-02-21 15:08:56 +0000

    Hit login page after Log Out

--- src/client/src/app/auth/signOut.ts
+++ src/client/src/app/auth/signOut.ts
@@ -5,6 +5,6 @@ import { redirect } from 'next/navigation';
 
 export const signOut = async () => {
   cookies().delete('token');
-  revalidatePath('/sites');
+  revalidatePath('/login');
   redirect('/login');
 };

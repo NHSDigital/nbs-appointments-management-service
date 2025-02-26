113589e9723b6dc0f4d3c6263a34d7646635b5fb
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-02-26 16:27:01 +0000

    FeatureFlag test, return different Eula version

--- src/api/Nhs.Appointments.Core/EulaService.cs
+++ src/api/Nhs.Appointments.Core/EulaService.cs
@@ -1,22 +1,32 @@
-﻿namespace Nhs.Appointments.Core;
+﻿using Microsoft.FeatureManagement;
+
+namespace Nhs.Appointments.Core;
 
 public interface IEulaService
 {
-        Task<EulaVersion> GetEulaVersionAsync();
-        Task ConsentToEula(string userId);
-}    
+    Task<EulaVersion> GetEulaVersionAsync();
+    Task ConsentToEula(string userId);
+}
 
-public class EulaService(IEulaStore eulaStore, IUserStore userStore) : IEulaService
+public class EulaService(IEulaStore eulaStore, IUserStore userStore, IFeatureManager featureManager) : IEulaService
 {
-        public async Task<EulaVersion> GetEulaVersionAsync()
+    public async Task<EulaVersion> GetEulaVersionAsync()
+    {
+        if (await featureManager.IsEnabledAsync(FeatureFlags.TestFeatureEnabled))
         {
-                return await eulaStore.GetLatestVersion();
+            return new EulaVersion
+            {
+                VersionDate = DateOnly.Parse("2025-02-26"),
+            };
         }
+        
+        return await eulaStore.GetLatestVersion();
+    }
 
-        public async Task ConsentToEula(string userId)
-        {
-                var latestVersion = await GetEulaVersionAsync();
+    public async Task ConsentToEula(string userId)
+    {
+        var latestVersion = await GetEulaVersionAsync();
 
-                await userStore.RecordEulaAgreementAsync(userId, latestVersion.VersionDate);
-        }
+        await userStore.RecordEulaAgreementAsync(userId, latestVersion.VersionDate);
+    }
 }
---
+++ src/api/Nhs.Appointments.Core/FeatureFlags.cs
@@ -0,0 +1,6 @@
+namespace Nhs.Appointments.Core;
+
+public static class FeatureFlags
+{
+    public const string TestFeatureEnabled = "TestFeatureEnabled";
+}

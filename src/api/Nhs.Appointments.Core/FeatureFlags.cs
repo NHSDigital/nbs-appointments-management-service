beaf7cf2e16911947bbe637cfdcb37f90bf8df87
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-02-27 14:05:19 +0000

    WIP

--- src/api/Nhs.Appointments.Core/FeatureFlags.cs
+++ src/api/Nhs.Appointments.Core/FeatureFlags.cs
@@ -2,5 +2,6 @@ namespace Nhs.Appointments.Core;
 
 public static class FeatureFlags
 {
-    public const string TestFeatureEnabled = "TestFeatureEnabled";
+    public const string TestFeatureOneEnabled = "TestFeatureOneEnabled";
+    public const string TestFeatureTwoEnabled = "TestFeatureTwoEnabled";
 }

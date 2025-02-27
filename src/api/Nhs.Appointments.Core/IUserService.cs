d4dd84d23e180eb66d13cfda8e6f1cc98e2bbd0c
pata9 <paul.tallet1@nhs.net>
2025-02-27 14:30:53 +0000

    revert changes back to production to hotfix nhsnumber changes (#555)
    Commits reverted
    
    
    https://github.com/NHSDigital/nbs-appointments-management-service/commit/aad7e2ed158358641bd4e2095407cc5d61554a7f
    
    https://github.com/NHSDigital/nbs-appointments-management-service/commit/f5437fe9de7716d2fb63e2e2922c3416480d878e
    
    https://github.com/NHSDigital/nbs-appointments-management-service/commit/0b35adcabae2182b81ed14b52678a01b4557a737
    
    https://github.com/NHSDigital/nbs-appointments-management-service/commit/0b282195a843dfc5e4ae027419d94b6f88c1cff3
    
    https://github.com/NHSDigital/nbs-appointments-management-service/commit/2af83e77fd4d709f0e8c9e3eb0336a29d835add9

--- src/api/Nhs.Appointments.Core/IUserService.cs
+++ src/api/Nhs.Appointments.Core/IUserService.cs
@@ -1,4 +1,4 @@
-namespace Nhs.Appointments.Core;
+﻿namespace Nhs.Appointments.Core;
 
 public interface IUserService
 {
@@ -8,5 +8,4 @@ public interface IUserService
     Task<UpdateUserRoleAssignmentsResult> UpdateUserRoleAssignmentsAsync(string userId, string scope, IEnumerable<RoleAssignment> roleAssignments);
     Task<IEnumerable<User>> GetUsersAsync(string site);
     Task<OperationResult> RemoveUserAsync(string userId, string site);
-    Task SaveUserAsync(string userId, string scope, IEnumerable<RoleAssignment> roleAssignments);
 }
--- src/api/Nhs.Appointments.Core/ReportItem.cs
+++
@@ -1,4 +0,0 @@
-namespace Nhs.Appointments.Core;
-public record ReportItem(int Index, string Name, bool Success, string Message)
-{
-}

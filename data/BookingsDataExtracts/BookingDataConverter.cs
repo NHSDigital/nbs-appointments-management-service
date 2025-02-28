9d7100f09a51d898f7d4580b36eee180b2876ab0
pata9 <paul.tallet1@nhs.net>
2025-02-28 16:44:28 +0000

    no whitespace in source string

--- data/BookingsDataExtracts/BookingDataConverter.cs
+++ data/BookingsDataExtracts/BookingDataConverter.cs
@@ -33,7 +33,7 @@ public class BookingDataConverter(IEnumerable<SiteDocument> sites)
 
     public static bool ExtractSelfReferral(NbsBookingDocument booking) => booking.AdditionalData?.ReferralType == "SelfReferred";
 
-    public static string ExtractSource(NbsBookingDocument booking) => booking.AdditionalData != null ? booking.AdditionalData.Source : "Unknown";
+    public static string ExtractSource(NbsBookingDocument booking) => booking.AdditionalData != null ? booking.AdditionalData.Source.Replace(" ", "_") : "Unknown";
 
     public static string ExtractDateOfBirth(BookingDocument booking) => booking.AttendeeDetails.DateOfBirth.ToString("yyyy-MM-dd");
 

bc91cc64626e205e0e55a0285b9afc0c25efbc56
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-03-03 14:40:07 +0000

    Add test flags and to static flags collection

--- src/api/Nhs.Appointments.Core/FeatureFlags.cs
+++ src/api/Nhs.Appointments.Core/FeatureFlags.cs
@@ -4,4 +4,6 @@ public static class FeatureFlags
 {
     public const string TestFeatureOneEnabled = "TestFeatureOneEnabled";
     public const string TestFeatureTwoEnabled = "TestFeatureTwoEnabled";
+    public const string TestFeatureThreeEnabled = "TestFeatureThreeEnabled";
+    public const string TestFeatureFourEnabled = "TestFeatureFourEnabled";
 }

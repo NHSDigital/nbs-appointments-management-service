11d51e2b527f1516737ca5d29f964cf8cb973ed3
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-03-04 16:08:43 +0000

    Local Feature Flags json. Test Flag Function Endpoints. Service registration for the 3 feature filter types.

--- src/api/Nhs.Appointments.Api/Program.cs
+++ src/api/Nhs.Appointments.Api/Program.cs
@@ -1,5 +1,9 @@
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.FeatureFilters;
 using Nhs.Appointments.Api;
 using Nhs.Appointments.Api.Auth;
 using Nhs.Appointments.Api.Logger;
@@ -7,9 +11,32 @@ using Nhs.Appointments.Api.Middleware;
 using Nhs.Appointments.Audit;
 
 var host = new HostBuilder()
+    .ConfigureAppConfiguration((ctx, cfg) =>
+    {
+        var azureAppConfigConnection = Environment.GetEnvironmentVariable("AzureAppConfigConnection");
+        if (azureAppConfigConnection == "local")
+        {
+            cfg.AddJsonFile("local.feature.flags.json", optional: false, reloadOnChange: true);
+        }
+        else
+        {
+            cfg.AddAzureAppConfiguration(options =>
+            {
+                options.Connect(azureAppConfigConnection)
+                    .UseFeatureFlags()
+                    .ConfigureRefresh(refresh => refresh.RegisterAll().SetRefreshInterval(TimeSpan.FromMinutes(10)));
+            });
+        }
+    })
     .ConfigureServices(services =>
     {
-        services.AddFeatureManagement();
+        services
+            .AddFeatureManagement()
+            .AddFeatureFilter<PercentageFilter>()
+            .AddFeatureFilter<TargetingFilter>()
+            .AddFeatureFilter<TimeWindowFilter>();
+        
+        services.AddSingleton<ITargetingContextAccessor, TargetingContextAccessor>();
     })
     .ConfigureFunctionsWebApplication(builder =>
     {

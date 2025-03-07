4eb3eecdf02419fb7500f93eca4783df38e79211
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-03-07 13:17:28 +0000

    Consolidate FeatureFlag functions into BySite and ByUser

--- src/api/Nhs.Appointments.Api/Features/Flags.cs
+++ src/api/Nhs.Appointments.Api/Features/Flags.cs
@@ -2,9 +2,16 @@ namespace Nhs.Appointments.Api.Features;
 
 public static class Flags
 {
+    #region TestFlags
+    //a simple on/off global flag
     public const string TestFeatureEnabled = "TestFeatureEnabled";
+    //a flag configured to be enabled for a pre-defined time period
     public const string TestFeatureTimeWindowEnabled = "TestFeatureTimeWindowEnabled";
+    //a flag configured to return enabled/disabled randomly on each request at the provided probability percentage
     public const string TestFeaturePercentageEnabled = "TestFeaturePercentageEnabled";
+    //a flag configured to be enabled for a specific cohort of users
     public const string TestFeatureUsersEnabled = "TestFeatureUsersEnabled";
+    //a flag configured to be enabled for a specific collection of sites
     public const string TestFeatureSitesEnabled = "TestFeatureSitesEnabled";
+    #endregion
 }

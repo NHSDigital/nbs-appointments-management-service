0dfaf3df1b9082a7781f3fbdb1146b285c09ec7c
sambiramairelogic <samuel.biram@airelogic.com>
2025-03-13 13:28:09 +0000

    First pass of integration tests

---
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/AvailabilityCalculations/BestFitFeatureSteps.cs
@@ -0,0 +1,8 @@
+﻿using Xunit.Gherkin.Quick;
+
+namespace Nhs.Appointments.Api.Integration.Scenarios.AvailabilityCalculations;
+
+[FeatureFile("./Scenarios/AvailabilityCalculations/BestFit.feature")]
+public sealed class BestFitFeatureSteps : BestFitBaseFeatureSteps
+{
+}

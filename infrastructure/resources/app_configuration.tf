3f65f95374d8159f3f023521503cbdcf9a9eb112
pata9 <paul.tallet1@nhs.net>
2025-03-19 15:55:42 +0000

    add yaml pipeline

--- infrastructure/resources/app_configuration.tf
+++ infrastructure/resources/app_configuration.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_configuration" "nbs_mya_app_configuration" {
-  name                = "${var.application_short}appcon${var.environment}${var.loc}"
+  name                = "${var.application}-config-${var.environment}-${var.loc}"
   resource_group_name = data.azurerm_resource_group.nbs_mya_resource_group.name
   location            = data.azurerm_resource_group.nbs_mya_resource_group.location
 }

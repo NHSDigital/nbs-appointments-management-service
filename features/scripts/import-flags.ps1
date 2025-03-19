91bead2f30046f6cca24e5d79f58e4aede9c2b50
pata9 <paul.tallet1@nhs.net>
2025-03-19 16:59:14 +0000

    add preview

--- features/scripts/import-flags.ps1
+++ features/scripts/import-flags.ps1
@@ -1,15 +1,24 @@
 #!/usr/bin/env pwsh
 param (
 [string][Parameter(Mandatory)]$appConfigName,
-[string][Parameter(Mandatory)]$sourceFile
+[string][Parameter(Mandatory)]$sourceFile,
+[bool][Parameter(Mandatory)]$confirmChanges
 )
 
 $ErrorActionPreference = "Stop"
 $DebugPreference = "Continue"
 
-az appconfig kv import `
-    --name $appConfigName `
-    --source file `
-    --path $sourceFile `
-    --format json `
-    --yes
+if ($confirmChanges) {
+    az appconfig kv import `
+        --name $appConfigName `
+        --source file `
+        --path $sourceFile `
+        --format json `
+        --yes
+} else {
+    az appconfig kv import `
+        --name $appConfigName `
+        --source file `
+        --path $sourceFile `
+        --format json `
+}

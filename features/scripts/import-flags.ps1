585a598dbcbc8b0ec4ac545f209d979f468c4805
pata9 <paul.tallet1@nhs.net>
2025-03-19 16:00:16 +0000

    add approval step

---
+++ features/scripts/import-flags.ps1
@@ -0,0 +1,15 @@
+#!/usr/bin/env pwsh
+param (
+[string][Parameter(Mandatory)]$appConfigName,
+[string][Parameter(Mandatory)]$sourceFile
+)
+
+$ErrorActionPreference = "Stop"
+$DebugPreference = "Continue"
+
+az appconfig kv import `
+    --name $appConfigName `
+    --source file `
+    --path $sourceFile `
+    --format json
+    --yes

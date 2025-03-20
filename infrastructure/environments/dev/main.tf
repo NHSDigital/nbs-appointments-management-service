bd799e38c6248d1a370882992d932b09a0f47c88
pata9 <paul.tallet1@nhs.net>
2025-03-20 17:12:06 +0000

    testing destroy

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -17,7 +17,12 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    app_configuration {
+      purge_soft_delete_on_destroy = true
+      recover_soft_deleted         = true
+    }
+  }
 }
 
 module "mya_application_dev" {

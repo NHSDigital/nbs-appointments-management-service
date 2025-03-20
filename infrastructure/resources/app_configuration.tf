25b547ae3c22e9246b44b9285b947b43bf6f73d2
pata9 <paul.tallet1@nhs.net>
2025-03-20 16:10:05 +0000

    move sku to standard

--- infrastructure/resources/app_configuration.tf
+++ infrastructure/resources/app_configuration.tf
@@ -2,4 +2,5 @@ resource "azurerm_app_configuration" "nbs_mya_app_configuration" {
   name                = "${var.application}-config-${var.environment}-${var.loc}"
   resource_group_name = data.azurerm_resource_group.nbs_mya_resource_group.name
   location            = data.azurerm_resource_group.nbs_mya_resource_group.location
+  sku                 = "standard"
 }

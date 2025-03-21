d4eca7b3c48d48fd89ba1fcac2042e9c82ffbfa3
Jonny Dyson <193929923+jsed-nhs@users.noreply.github.com>
2025-03-21 16:49:08 +0000

    Appt 672 Feature Toggle Management - Azure and Local Settings (#570)
    Ability to apply Feature Toggles with FeatureFilters using new Azure App
    Configuration resource.
    The supported Feature Filters available include:
    
    - Simple On/Off
    - Random % rollout for all
    - Enabled within a pre-defined time window.
    - Enabled for specific users
    - Enabled for specific sites
    
    Example wrap around for OKTA login button implementation.
    Register AzureAppConfigManagement in ServiceRegistration, or use
    local.feature.flags.json if in local run mode.
    Add SDK Microsoft.FeatureManagement.AspNetCore
    Add FeatureToggleHelper class that adds all the user and site context
    into the FeatureManager enabled check.
    Collection of test flags defined and how they might be used.
    Add unit tests that confirm FeatureToggleHelper applies the user and
    site to the context correctly.

---
+++ src/api/Nhs.Appointments.Api/Features/Flags.cs
@@ -0,0 +1,23 @@
+namespace Nhs.Appointments.Api.Features;
+
+public static class Flags
+{
+    public const string OktaLogin = "OktaLogin";
+    
+    #region TestFlags
+    //a simple on/off global flag
+    public const string TestFeatureEnabled = "TestFeatureEnabled";
+    //a flag configured to be enabled for a pre-defined time period
+    public const string TestFeatureTimeWindowEnabled = "TestFeatureTimeWindowEnabled";
+    //a flag configured to be enabled for a pre-defined time period
+    public const string TestFeatureTimeWindowDisabled = "TestFeatureTimeWindowDisabled";
+    //a flag configured to return enabled/disabled randomly on each request at the provided probability percentage
+    public const string TestFeaturePercentageEnabled = "TestFeaturePercentageEnabled";
+    //a flag configured to be enabled for a specific cohort of users
+    public const string TestFeatureUsersEnabled = "TestFeatureUsersEnabled";
+    //a flag configured to be enabled for a specific collection of sites
+    public const string TestFeatureSitesEnabled = "TestFeatureSitesEnabled";
+    //a flag configured to be enabled for a specific collection of sites OR users. this demonstrates that targeting is LOGICAL OR
+    public const string TestFeatureSiteOrUserEnabled = "TestFeatureSiteOrUserEnabled";
+    #endregion
+}

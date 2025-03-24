06a4ee8c63ddb6c80cfcbf2cf471c207329362ef
ste-banks <stephen.banks4@nhs.net>
2025-03-24 15:39:53 +0000

    APPT-712 - Prevent the bulk import from updating a site if the siteId already exists

--- src/api/Nhs.Appointments.Core/SiteDataImporterHandler.cs
+++ src/api/Nhs.Appointments.Core/SiteDataImporterHandler.cs
@@ -12,6 +12,7 @@ public class SiteDataImporterHandler(ISiteService siteService, IWellKnowOdsCodes
         var report = (await processor.ProcessFile(fileReader)).ToList();
 
         CheckForDuplicatedSites(siteRows, report);
+        await CheckIfAnySitesAlreadyExist(siteRows, report);
 
         var invalidOdsCodes = await GetSitesWithInvalidOdsCodes(siteRows);
         if (invalidOdsCodes.Count > 0)
@@ -77,6 +78,24 @@ public class SiteDataImporterHandler(ISiteService siteService, IWellKnowOdsCodes
         if (duplicateSiteNames.Count > 0)
         {
             report.AddRange(duplicateSiteNames.Select(dup => new ReportItem(-1, dup, false, $"Duplicate site name provided: {dup}. Site names must be unique.")));
+        }
+    }
+
+    private async Task CheckIfAnySitesAlreadyExist(List<SiteImportRow> siteRows, List<ReportItem> report)
+    {
+        var existingSiteIds = new List<string>();
+        var siteIds = siteRows.Select(s => s.Id).Distinct().ToList();
+        foreach (var siteId in siteIds)
+        {
+            if (await siteService.GetSiteByIdAsync(siteId) is not null)
+            {
+                existingSiteIds.Add(siteId);
+            }
+        }
+
+        if (existingSiteIds.Count > 0)
+        {
+            report.AddRange(existingSiteIds.Select(id => new ReportItem(-1, "Site already exists", false, $"Site with ID: {id} already exists in the system.")));
         }
     }
 

c804796b35e2d917e2e9422901f736a3abcd28ef
sambiramairelogic <samuel.biram@airelogic.com>
2025-04-02 13:46:51 +0100

    Remove commented lines from DR files to reduce diff with main

--- infrastructure/disaster-recovery/terraform/resources/applicationinsights.tf
+++ infrastructure/disaster-recovery/terraform/resources/applicationinsights.tf
@@ -3,7 +3,6 @@ resource "azurerm_application_insights" "nbs_mya_application_insights" {
   name                = "${var.application}-ai-${var.environment}-${var.loc}"
   resource_group_name = data.azurerm_resource_group.nbs_mya_resource_group.name
   location            = var.location
-  # location            = data.azurerm_resource_group.nbs_mya_resource_group.location
   application_type    = "web"
   sampling_percentage = var.app_insights_sampling_percentage
 }

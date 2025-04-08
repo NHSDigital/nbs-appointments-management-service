d8cd0e443f8155457de2b62cadb03e10224c0529
sambiramairelogic <samuel.biram@airelogic.com>
2025-04-08 16:55:35 +0100

    DR resources are now identical to main resources, so delete DR terraform folders

--- infrastructure/disaster-recovery/terraform/resources/app_configuration.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_app_configuration" "nbs_mya_app_configuration" {
-  name                = "${var.application}-config-${var.environment}-${var.loc}"
-  resource_group_name = data.azurerm_resource_group.nbs_mya_resource_group.name
-  location            = var.location
-  sku                 = "standard"
-  soft_delete_retention_days = 1
-}

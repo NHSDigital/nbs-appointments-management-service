0a2726a30d3f45aaad29f0f01c5da8915725017c
sambiramairelogic <samuel.biram@airelogic.com>
2025-04-10 16:40:39 +0100

    Provide temp workspace_id in full format

--- infrastructure/resources/applicationinsights.tf
+++ infrastructure/resources/applicationinsights.tf
@@ -5,5 +5,5 @@ resource "azurerm_application_insights" "nbs_mya_application_insights" {
   location            = var.location
   application_type    = "web"
   sampling_percentage = var.app_insights_sampling_percentage
-  workspace_id        = "managed-nbs-mya-ai-stag-ukw-ws"
+  workspace_id        = "/subscriptions/b0787d6a-56e3-4899-bc30-723f9d78899c/resourceGroups/ai_nbs-mya-ai-stag-ukw_managed/providers/Microsoft.OperationalInsights/workspaces/managed-nbs-mya-ai-stag-ukw-ws"
 }

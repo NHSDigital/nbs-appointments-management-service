8ec8eb4be4d6790018200471f62f0fcbfbd23c64
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-04-11 12:26:12 +0100

    Remove skips for the relevant tests

--- src/client/testing/tests/view-availability/view-month-availability.spec.ts
+++ src/client/testing/tests/view-availability/view-month-availability.spec.ts
@@ -11,66 +11,63 @@ let oAuthPage: OAuthLoginPage;
 let viewMonthAvailabilityPage: MonthViewAvailabilityPage;
 let site: Site;
 
-test.describe.skip(
-  'Daylight Savings Tests for the month view - Oct 2025',
-  () => {
-    test.beforeEach(async ({ page, getTestSite }) => {
-      site = getTestSite(2);
-      rootPage = new RootPage(page);
-      oAuthPage = new OAuthLoginPage(page);
-      viewMonthAvailabilityPage = new MonthViewAvailabilityPage(page);
+test.describe('Daylight Savings Tests for the month view - Oct 2025', () => {
+  test.beforeEach(async ({ page, getTestSite }) => {
+    site = getTestSite(2);
+    rootPage = new RootPage(page);
+    oAuthPage = new OAuthLoginPage(page);
+    viewMonthAvailabilityPage = new MonthViewAvailabilityPage(page);
 
-      await rootPage.goto();
-      await rootPage.pageContentLogInButton.click();
-      await oAuthPage.signIn();
+    await rootPage.goto();
+    await rootPage.pageContentLogInButton.click();
+    await oAuthPage.signIn();
 
-      //go to a specific month page that has a daylight savings change
-      await page.goto(
-        `manage-your-appointments/site/${site.id}/view-availability?date=2025-10-20`,
-      );
-    });
+    //go to a specific month page that has a daylight savings change
+    await page.goto(
+      `manage-your-appointments/site/${site.id}/view-availability?date=2025-10-20`,
+    );
+  });
 
-    test('All the view month page data is arranged in the week cards as expected', async () => {
-      await viewMonthAvailabilityPage.verifyViewNextMonthButtonDisplayed();
-      await viewMonthAvailabilityPage.verifyAllWeekCardInformationDisplayedCorrectly(
-        [
-          {
-            header: '29 September to 5 October',
-            services: [],
-            totalAppointments: 0,
-            booked: 0,
-            unbooked: 0,
-          },
-          {
-            header: '6 October to 12 October',
-            services: [],
-            totalAppointments: 0,
-            booked: 0,
-            unbooked: 0,
-          },
-          {
-            header: '13 October to 19 October',
-            services: [],
-            totalAppointments: 0,
-            booked: 0,
-            unbooked: 0,
-          },
-          {
-            header: '20 October to 26 October',
-            services: [{ serviceName: 'RSV (Adult)', bookedAppointments: 4 }],
-            totalAppointments: 840,
-            booked: 4,
-            unbooked: 836,
-          },
-          {
-            header: '27 October to 2 November',
-            services: [],
-            totalAppointments: 0,
-            booked: 0,
-            unbooked: 0,
-          },
-        ],
-      );
-    });
-  },
-);
+  test('All the view month page data is arranged in the week cards as expected', async () => {
+    await viewMonthAvailabilityPage.verifyViewNextMonthButtonDisplayed();
+    await viewMonthAvailabilityPage.verifyAllWeekCardInformationDisplayedCorrectly(
+      [
+        {
+          header: '29 September to 5 October',
+          services: [],
+          totalAppointments: 0,
+          booked: 0,
+          unbooked: 0,
+        },
+        {
+          header: '6 October to 12 October',
+          services: [],
+          totalAppointments: 0,
+          booked: 0,
+          unbooked: 0,
+        },
+        {
+          header: '13 October to 19 October',
+          services: [],
+          totalAppointments: 0,
+          booked: 0,
+          unbooked: 0,
+        },
+        {
+          header: '20 October to 26 October',
+          services: [{ serviceName: 'RSV (Adult)', bookedAppointments: 4 }],
+          totalAppointments: 840,
+          booked: 4,
+          unbooked: 836,
+        },
+        {
+          header: '27 October to 2 November',
+          services: [],
+          totalAppointments: 0,
+          booked: 0,
+          unbooked: 0,
+        },
+      ],
+    );
+  });
+});

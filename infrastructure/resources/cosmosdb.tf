05e94788c43b73a689f325cd35290acbaea779a3
sambiramairelogic <samuel.biram@airelogic.com>
2025-04-14 15:24:49 +0100

    Formatting

--- infrastructure/resources/cosmosdb.tf
+++ infrastructure/resources/cosmosdb.tf
@@ -31,14 +31,14 @@ resource "azurerm_cosmosdb_account" "nbs_mya_cosmos_db" {
 }
 
 resource "azurerm_cosmosdb_sql_database" "nbs_appts_database" {
-  count                    = var.create_cosmos_db ? 1 : 0
+  count               = var.create_cosmos_db ? 1 : 0
   name                = "appts"
   resource_group_name = data.azurerm_resource_group.nbs_mya_resource_group.name
   account_name        = azurerm_cosmosdb_account.nbs_mya_cosmos_db[0].name
 }
 
 resource "azurerm_cosmosdb_sql_container" "nbs_mya_booking_container" {
-  count                    = var.create_cosmos_db ? 1 : 0
+  count                  = var.create_cosmos_db ? 1 : 0
   name                   = "booking_data"
   resource_group_name    = data.azurerm_resource_group.nbs_mya_resource_group.name
   account_name           = azurerm_cosmosdb_account.nbs_mya_cosmos_db[0].name
@@ -103,7 +103,7 @@ resource "azurerm_cosmosdb_sql_container" "nbs_mya_booking_container" {
 }
 
 resource "azurerm_cosmosdb_sql_container" "nbs_mya_core_container" {
-  count                    = var.create_cosmos_db ? 1 : 0
+  count                  = var.create_cosmos_db ? 1 : 0
   name                   = "core_data"
   resource_group_name    = data.azurerm_resource_group.nbs_mya_resource_group.name
   account_name           = azurerm_cosmosdb_account.nbs_mya_cosmos_db[0].name
@@ -120,7 +120,7 @@ resource "azurerm_cosmosdb_sql_container" "nbs_mya_core_container" {
 }
 
 resource "azurerm_cosmosdb_sql_container" "nbs_mya_index_container" {
-  count                    = var.create_cosmos_db ? 1 : 0
+  count                  = var.create_cosmos_db ? 1 : 0
   name                   = "index_data"
   resource_group_name    = data.azurerm_resource_group.nbs_mya_resource_group.name
   account_name           = azurerm_cosmosdb_account.nbs_mya_cosmos_db[0].name
@@ -137,7 +137,7 @@ resource "azurerm_cosmosdb_sql_container" "nbs_mya_index_container" {
 }
 
 resource "azurerm_cosmosdb_sql_container" "nbs_mya_audit_container" {
-  count                    = var.create_cosmos_db ? 1 : 0
+  count                  = var.create_cosmos_db ? 1 : 0
   name                   = "audit_data"
   resource_group_name    = data.azurerm_resource_group.nbs_mya_resource_group.name
   account_name           = azurerm_cosmosdb_account.nbs_mya_cosmos_db[0].name

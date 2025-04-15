48a2906b55c80ee8087a935baae5d3eccbcd8e4c
sambiramairelogic <samuel.biram@airelogic.com>
2025-04-15 14:10:09 +0100

    Update script

--- scripts/disaster-recovery/import-flags-to-replica.ps1
+++ scripts/disaster-recovery/import-flags-to-replica.ps1
@@ -1,36 +1,31 @@
 #!/usr/bin/env pwsh
 param (
+[string][Parameter(Mandatory)]$environment,
 [string][Parameter(Mandatory)]$sourceFile
 )
 
 $ErrorActionPreference = "Stop"
 $DebugPreference = "Continue"
 
-## Log Hello World
-Write-Host "Entered import-flags-to-replica.ps1 script"
-Write-Host "Source file: $sourceFile"
+$UKSouthAppConfigAccountName = "nbs-mya-config-$environment-uks"
+$UKSouthAppConfigResourceGroupName = "nbs-mya-rg-$environment-uks"
 
 $UKSouthPrimaryConnectionString =
 az appconfig credential list `
-        --name $appConfigAccountName `
-        --resource-group $ResourceGroup `
+        --name $UKSouthAppConfigAccountName `
+        --resource-group $UKSouthAppConfigResourceGroupName `
         --query "[?name == 'Primary'].connectionString | [0]" `
         --output tsv
 
-Write-Host "Have fetched UK South Primary Connection String"
 
 $UKWestReplicaEndpoint = 
 az appconfig replica show `
-        --store-name $appConfigAccountName `
+        --store-name $UKSouthAppConfigAccountName `
         --name replicaukw `
         --query "endpoint" `
         --output tsv `
 
-Write-Host "Have fetched UK West Replica Connection String"
-
 $appConfigConnection = $UKSouthPrimaryConnectionString -replace "Endpoint=[^;]+", "Endpoint=$UKWestReplicaEndpoint"
-
-Write-Host "Have derived the new conn string"
 
 az appconfig kv import `
         --connection-string $appConfigConnection `

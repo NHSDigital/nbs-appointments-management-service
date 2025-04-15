5ed30abf8adedcd578c835805756d95ddb4fd3fa
sambiramairelogic <samuel.biram@airelogic.com>
2025-04-15 13:15:30 +0100

    Use Primary connection when updating feature toggles

--- scripts/disaster-recovery/import-flags-to-replica.ps1
+++ scripts/disaster-recovery/import-flags-to-replica.ps1
@@ -1,15 +1,29 @@
 #!/usr/bin/env pwsh
 param (
-[string][Parameter(Mandatory)]$appConfigConnectionString,
 [string][Parameter(Mandatory)]$sourceFile
 )
 
 $ErrorActionPreference = "Stop"
 $DebugPreference = "Continue"
 
+$UKSouthPrimaryConnectionString =
+az appconfig credential list `
+        --name $appConfigAccountName `
+        --resource-group $ResourceGroup `
+        --query "[?name == 'Primary'].connectionString | [0]" `
+        --output tsv
+
+$UKWestReplicaEndpoint = 
+az appconfig replica show `
+        --store-name $appConfigAccountName `
+        --name replicaukw `
+        --query "endpoint" `
+        --output tsv `
+
+$appConfigConnection = $UKSouthPrimaryConnectionString -replace "Endpoint=[^;]+", "Endpoint=$UKWestReplicaEndpoint"
 
 az appconfig kv import `
-        --connection-string $appConfigConnectionString `
+        --connection-string $appConfigConnection `
         --source file `
         --path $sourceFile `
         --format json `

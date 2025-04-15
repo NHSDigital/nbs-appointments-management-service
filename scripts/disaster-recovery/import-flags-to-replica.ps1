864338c57523d6076ce4aa8faa924a5504f10d3a
sambiramairelogic <samuel.biram@airelogic.com>
2025-04-15 13:52:53 +0100

    stupoid debugging because error message did not give line number

--- scripts/disaster-recovery/import-flags-to-replica.ps1
+++ scripts/disaster-recovery/import-flags-to-replica.ps1
@@ -6,12 +6,18 @@ param (
 $ErrorActionPreference = "Stop"
 $DebugPreference = "Continue"
 
+## Log Hello World
+Write-Host "Entered import-flags-to-replica.ps1 script"
+Write-Host "Source file: $sourceFile"
+
 $UKSouthPrimaryConnectionString =
 az appconfig credential list `
         --name $appConfigAccountName `
         --resource-group $ResourceGroup `
         --query "[?name == 'Primary'].connectionString | [0]" `
         --output tsv
+
+Write-Host "Have fetched UK South Primary Connection String"
 
 $UKWestReplicaEndpoint = 
 az appconfig replica show `
@@ -20,7 +26,11 @@ az appconfig replica show `
         --query "endpoint" `
         --output tsv `
 
+Write-Host "Have fetched UK West Replica Connection String"
+
 $appConfigConnection = $UKSouthPrimaryConnectionString -replace "Endpoint=[^;]+", "Endpoint=$UKWestReplicaEndpoint"
+
+Write-Host "Have derived the new conn string"
 
 az appconfig kv import `
         --connection-string $appConfigConnection `

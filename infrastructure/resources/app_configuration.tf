307d550c9da92bd8f1bca39e7cab43759030a8bf
Sam Biram <63285990+sambiramairelogic@users.noreply.github.com>
2025-04-22 12:49:20 +0100

    APPT-290: Stop trying to reduce soft retention days (#647)

--- infrastructure/resources/app_configuration.tf
+++ infrastructure/resources/app_configuration.tf
@@ -4,7 +4,6 @@ resource "azurerm_app_configuration" "nbs_mya_app_configuration" {
   resource_group_name = data.azurerm_resource_group.nbs_mya_resource_group.name
   location            = var.location
   sku                 = "standard"
-  soft_delete_retention_days = 1
 
   replica {
     name     = "replicaukw"

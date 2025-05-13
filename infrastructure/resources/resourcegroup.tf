b1beb1028bcb8a0d2034447dee578e2dd2181d39
pata9 <paul.tallet1@nhs.net>
2025-05-13 22:58:00 +0100

    need to tidy this up but should get us moving

--- infrastructure/resources/resourcegroup.tf
+++ infrastructure/resources/resourcegroup.tf
@@ -1,3 +1,7 @@
+locals {
+  is_env_standard = contains(["dev", "int", "stag", "prod"], var.environment)
+}
+
 data "azurerm_resource_group" "nbs_mya_resource_group" {
-  name = "${var.application}-rg-${var.environment}-${var.loc}"
+  name = local.is_env_standard ? "${var.application}-rg-${var.environment}-${var.loc}" : "${var.application}${var.environment}-rg-stag-${var.loc}"
 }

621f45477e9ad9e2824b6ac1986a29b3eee7c61c
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-05-13 17:53:28 +0100

    Continued wrapping of Allocation adjacent tests in MultipleServices flag and serial. Added dispose pattern to the tests to remove and availability/bookings created by a test, so they clean up after themselves. Other tests still need wrapping in this pattern.

--- tests/Nhs.Appointments.Api.Integration/Scenarios/CreateAvailability/SetAvailabilityFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/CreateAvailability/SetAvailabilityFeatureSteps.cs
@@ -1,9 +1,20 @@
-﻿using Xunit.Gherkin.Quick;
+﻿using Nhs.Appointments.Core.Features;
+using Xunit;
+using Xunit.Gherkin.Quick;
 
 namespace Nhs.Appointments.Api.Integration.Scenarios.CreateAvailability
 {
     [FeatureFile("./Scenarios/CreateAvailability/SetAvailability.feature")]
-    public class SetAvailabilityFeatureSteps : BaseCreateAvailabilityFeatureSteps
+    public abstract class SetAvailabilityFeatureSteps(string flag, bool enabled)
+        : BaseCreateAvailabilityFeatureSteps(flag, enabled)
     {
     }
+
+    [Collection("MultipleServicesSerialToggle")]
+    public class SetAvailabilityFeatureSteps_MultipleServicesEnabled()
+        : SetAvailabilityFeatureSteps(Flags.MultipleServices, true);
+
+    [Collection("MultipleServicesSerialToggle")]
+    public class SetAvailabilityFeatureSteps_MultipleServicesDisabled()
+        : SetAvailabilityFeatureSteps(Flags.MultipleServices, false);
 }

92f6283e69e8210e2fe7f1c0b2ee7d6d0818bab6
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-05-13 15:23:34 +0100

    Gherkin tests. MultipleServicesSerialCollection and FeatureToggledSteps to run relevant integration tests consistently in both enabled/disabled state without race conditions.

--- tests/Nhs.Appointments.Api.Integration/Scenarios/Availability/AvailabilityByHoursFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Availability/AvailabilityByHoursFeatureSteps.cs
@@ -1,10 +1,16 @@
-﻿using Xunit.Gherkin.Quick;
+﻿using Nhs.Appointments.Core.Features;
+using Xunit;
+using Xunit.Gherkin.Quick;
 
 namespace Nhs.Appointments.Api.Integration.Scenarios.Availability;
 
+[Collection("MultipleServicesSerialToggle")]
 [FeatureFile("./Scenarios/Availability/AvailabilityByHours.feature")]
-public sealed class AvailabilityByHoursFeatureSteps : AvailabilityBaseFeatureSteps
-{
-        
-}
+public class AvailabilityByHoursFeatureSteps_MultipleServicesEnabled()
+    : AvailabilityBaseFeatureSteps(Flags.MultipleServices, true);
+
+[Collection("MultipleServicesSerialToggle")]
+[FeatureFile("./Scenarios/Availability/AvailabilityByHours.feature")]
+public class AvailabilityByHoursFeatureSteps_MultipleServicesDisabled()
+    : AvailabilityBaseFeatureSteps(Flags.MultipleServices, false);
 
---
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/FeatureToggledSteps.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Nhs.Appointments.Api.Integration.Scenarios;
+
+public class FeatureToggledSteps(string flag, bool enabled) : BaseFeatureSteps, IAsyncLifetime
+{
+    private string Flag { get; } = flag;
+    private bool Enabled { get; } = enabled;
+
+    public async Task InitializeAsync()
+    {
+        await base.SetLocalFeatureToggleOverride(Flag, Enabled ? "True" : "False");
+    }
+
+    public async Task DisposeAsync()
+    {
+        await base.ClearLocalFeatureToggleOverrides();
+    }
+}

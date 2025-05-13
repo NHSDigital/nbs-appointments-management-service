5e1ee8dec15e275288e9c3fe96c48defa4692640
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-05-13 15:23:34 +0100

    Gherkin tests. MultipleServicesSerialCollection and FeatureToggledSteps to run relevant integration tests consistently in both enabled/disabled state without race conditions.

--- tests/Nhs.Appointments.Api.Integration/Scenarios/Availability/AvailabilityBaseFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Availability/AvailabilityBaseFeatureSteps.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -10,7 +11,7 @@ using Xunit.Gherkin.Quick;
 
 namespace Nhs.Appointments.Api.Integration.Scenarios.Availability;
 
-public abstract class AvailabilityBaseFeatureSteps : BaseFeatureSteps
+public abstract class AvailabilityBaseFeatureSteps(string flag, bool enabled) : FeatureToggledSteps(flag, enabled)
 {
     private HttpResponseMessage _response;
     private HttpStatusCode _statusCode;
@@ -19,6 +20,32 @@ public abstract class AvailabilityBaseFeatureSteps : BaseFeatureSteps
     protected HttpResponseMessage Response => _response;
     protected HttpStatusCode StatusCode => _statusCode;
     protected QueryAvailabilityResponse ActualReponse => _actualResponse;
+    
+    [Then(@"the following daily availability is returned")]
+    public void AssertDailyAvailability(Gherkin.Ast.DataTable expectedDailyAvailabilityTable)
+    {
+        var expectedAvailabilty = expectedDailyAvailabilityTable.Rows.Skip(1).Select(row => new QueryAvailabilityResponseInfo
+        (
+            ParseNaturalLanguageDateOnly(row.Cells.ElementAt(0).Value),
+            new List<QueryAvailabilityResponseBlock>()
+            {
+                new (new TimeOnly(0,0), new TimeOnly(12,00), int.Parse(row.Cells.ElementAt(1).Value)),
+                new (new TimeOnly(12,0), new TimeOnly(00,00), int.Parse(row.Cells.ElementAt(2).Value)),
+            }
+        ));
+
+        StatusCode.Should().Be(HttpStatusCode.OK);
+        ActualReponse
+            .First().availability
+            .Should().BeEquivalentTo(expectedAvailabilty);
+    }
+
+    [Then(@"the request is successful and no availability is returned")]
+    public void AssertNoAvailability()
+    {
+        StatusCode.Should().Be(HttpStatusCode.OK);
+        ActualReponse.Should().BeEmpty();
+    }
 
     [When(@"I check ([\w:]+) availability for '([\w:]+)' between '(.+)' and '(.+)'")]
     public async Task CheckAvailability(string queryType, string service, string from, string until)

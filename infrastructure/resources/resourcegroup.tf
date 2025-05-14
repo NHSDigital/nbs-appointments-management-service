719290e1976ffe95e10feaf24e3b95357e70b843
pata9 <paul.tallet1@nhs.net>
2025-05-14 14:14:58 +0100

    little tidy up

--- infrastructure/resources/resourcegroup.tf
+++ infrastructure/resources/resourcegroup.tf
@@ -1,7 +1,3 @@
-locals {
-  is_env_standard = contains(["dev", "int", "stag", "prod"], var.environment)
-}
-
 data "azurerm_resource_group" "nbs_mya_resource_group" {
-  name = local.is_env_standard ? "${var.application}-rg-${var.environment}-${var.loc}" : "${var.application}${var.environment}-rg-stag-${var.loc}"
+  name = "${var.application}${local.is_perf ? var.application : ""}-rg-${local.is_perf ? "stag" : var.environment}-${var.loc}"
 }

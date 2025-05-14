14c17eb24880d0abd2de468aafc3d44baebca14a
pata9 <paul.tallet1@nhs.net>
2025-05-14 15:32:57 +0100

    fix up and remove throughput from stag

--- infrastructure/resources/resourcegroup.tf
+++ infrastructure/resources/resourcegroup.tf
@@ -1,3 +1,3 @@
 data "azurerm_resource_group" "nbs_mya_resource_group" {
-  name = "${var.application}${local.is_perf ? var.application : ""}-rg-${local.is_perf ? "stag" : var.environment}-${var.loc}"
+  name = "${var.application}${local.is_perf ? var.environment : ""}-rg-${local.is_perf ? "stag" : var.environment}-${var.loc}"
 }

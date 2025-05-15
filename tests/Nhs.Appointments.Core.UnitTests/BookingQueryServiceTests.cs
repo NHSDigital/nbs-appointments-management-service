800c120d911631d4fa8910974de275b838106c52
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-05-15 07:38:47 +0100

    Add TODO to BWS tests for future PR. Add BQS tests for OrderedLiveBookings.

--- tests/Nhs.Appointments.Core.UnitTests/BookingQueryServiceTests.cs
+++ tests/Nhs.Appointments.Core.UnitTests/BookingQueryServiceTests.cs
@@ -4,10 +4,11 @@ public class BookingQueryServiceTests
 {
     private readonly BookingQueryService _bookingQueryService;
     private readonly Mock<IBookingsDocumentStore> _bookingsDocumentStore = new();
+    private readonly Mock<TimeProvider> _timeProvider = new();
 
     public BookingQueryServiceTests()
     {
-        _bookingQueryService = new BookingQueryService(_bookingsDocumentStore.Object, TimeProvider.System);
+        _bookingQueryService = new BookingQueryService(_bookingsDocumentStore.Object, _timeProvider.Object);
     }
 
     [Fact]
@@ -18,24 +19,28 @@ public class BookingQueryServiceTests
         {
             new Booking
             {
+                Created = new DateTime(2025, 01, 01, 09, 0, 0),
                 From = new DateTime(2025, 01, 01, 16, 0, 0),
                 Reference = "1",
                 AttendeeDetails = new AttendeeDetails { FirstName = "Daniel", LastName = "Dixon" },
             },
             new Booking
             {
+                Created = new DateTime(2025, 01, 01, 09, 0, 0),
                 From = new DateTime(2025, 01, 01, 14, 0, 0),
                 Reference = "2",
                 AttendeeDetails = new AttendeeDetails { FirstName = "Alexander", LastName = "Cooper" },
             },
             new Booking
             {
+                Created = new DateTime(2025, 01, 01, 09, 0, 0),
                 From = new DateTime(2025, 01, 01, 14, 0, 0),
                 Reference = "3",
                 AttendeeDetails = new AttendeeDetails { FirstName = "Alexander", LastName = "Brown" },
             },
             new Booking
             {
+                Created = new DateTime(2025, 01, 01, 09, 0, 0),
                 From = new DateTime(2025, 01, 01, 10, 0, 0),
                 Reference = "4",
                 AttendeeDetails = new AttendeeDetails { FirstName = "Bob", LastName = "Dawson" },
@@ -57,9 +62,141 @@ public class BookingQueryServiceTests
     }
 
     [Fact]
-    public Task GetOrderedLiveBookings()
+    public async Task GetOrderedLiveBookings_ExcludesUnknownAndCancelledBookings()
     {
-        //TODO
-        throw new NotImplementedException();
+        var date = DateTimeOffset.ParseExact("2025-01-01 00:00", "yyyy-MM-dd HH:mm", null);
+        _timeProvider.Setup(x => x.GetUtcNow()).Returns(date);
+
+        var site = "some-site";
+        IEnumerable<Booking> bookings = new List<Booking>
+        {
+            new Booking
+            {
+                Created = new DateTime(2025, 01, 01, 09, 0, 0),
+                From = new DateTime(2025, 01, 01, 16, 0, 0),
+                Reference = "1",
+                Status = AppointmentStatus.Booked,
+                AttendeeDetails = new AttendeeDetails { FirstName = "Daniel", LastName = "Dixon" },
+            },
+            new Booking
+            {
+                Created = new DateTime(2025, 01, 01, 09, 0, 0),
+                From = new DateTime(2025, 01, 01, 14, 0, 0),
+                Reference = "2",
+                Status = AppointmentStatus.Unknown,
+                AttendeeDetails = new AttendeeDetails { FirstName = "Alexander", LastName = "Cooper" },
+            },
+            new Booking
+            {
+                Created = new DateTime(2025, 01, 01, 09, 0, 0),
+                From = new DateTime(2025, 01, 01, 14, 0, 0),
+                Reference = "3",
+                Status = AppointmentStatus.Provisional,
+                AttendeeDetails = new AttendeeDetails { FirstName = "Alexander", LastName = "Brown" },
+            },
+            new Booking
+            {
+                Created = new DateTime(2025, 01, 01, 09, 0, 0),
+                From = new DateTime(2025, 01, 01, 10, 0, 0),
+                Reference = "4",
+                Status = AppointmentStatus.Cancelled,
+                AttendeeDetails = new AttendeeDetails { FirstName = "Bob", LastName = "Dawson" },
+            }
+        };
+
+        _bookingsDocumentStore
+            .Setup(x => x.GetInDateRangeAsync(It.IsAny<DateTime>(), It.IsAny<DateTime>(), site))
+            .ReturnsAsync(bookings);
+
+        var response = await _bookingQueryService.GetOrderedLiveBookings(site, new DateTime(), new DateTime());
+
+        response.Should().HaveCount(2);
+
+        Assert.Multiple(
+            () => Assert.True(response.ToArray()[0].Reference == "3"),
+            () => Assert.True(response.ToArray()[1].Reference == "1")
+        );
+    }
+
+    [Fact]
+    public async Task GetOrderedLiveBookings_ExcludesExpiredProvisionalBookings()
+    {
+        var date = DateTimeOffset.ParseExact("2025-01-01 09:00", "yyyy-MM-dd HH:mm", null);
+        _timeProvider.Setup(x => x.GetUtcNow()).Returns(date);
+
+        var site = "some-site";
+        IEnumerable<Booking> bookings = new List<Booking>
+        {
+            new Booking
+            {
+                Created = new DateTime(2025, 01, 01, 09, 0, 0),
+                From = new DateTime(2025, 01, 01, 16, 0, 0),
+                Reference = "1",
+                Status = AppointmentStatus.Booked,
+                AttendeeDetails = new AttendeeDetails { FirstName = "Daniel", LastName = "Dixon" },
+            },
+            new Booking
+            {
+                Created = new DateTime(2025, 01, 01, 09, 0, 0),
+                From = new DateTime(2025, 01, 01, 14, 0, 0),
+                Reference = "2",
+                Status = AppointmentStatus.Unknown,
+                AttendeeDetails = new AttendeeDetails { FirstName = "Alexander", LastName = "Cooper" },
+            },
+            new Booking
+            {
+                Created = new DateTime(2025, 01, 01, 08, 50, 0),
+                From = new DateTime(2025, 01, 01, 14, 0, 0),
+                Reference = "3",
+                Status = AppointmentStatus.Provisional,
+                AttendeeDetails = new AttendeeDetails { FirstName = "Alexander", LastName = "Brown" },
+            },
+            new Booking
+            {
+                Created = new DateTime(2025, 01, 01, 08, 54, 0),
+                From = new DateTime(2025, 01, 01, 14, 0, 0),
+                Reference = "4",
+                Status = AppointmentStatus.Provisional,
+                AttendeeDetails = new AttendeeDetails { FirstName = "Alexander", LastName = "Brown" },
+            },
+            new Booking
+            {
+                Created = new DateTime(2025, 01, 01, 08, 55, 0),
+                From = new DateTime(2025, 01, 01, 14, 0, 0),
+                Reference = "5",
+                Status = AppointmentStatus.Provisional,
+                AttendeeDetails = new AttendeeDetails { FirstName = "Alexander", LastName = "Brown" },
+            },
+            new Booking
+            {
+                Created = new DateTime(2025, 01, 01, 08, 55, 1),
+                From = new DateTime(2025, 01, 01, 14, 0, 0),
+                Reference = "6",
+                Status = AppointmentStatus.Provisional,
+                AttendeeDetails = new AttendeeDetails { FirstName = "Alexander", LastName = "Brown" },
+            },
+            new Booking
+            {
+                Created = new DateTime(2025, 01, 01, 08, 54, 59),
+                From = new DateTime(2025, 01, 01, 14, 0, 0),
+                Reference = "7",
+                Status = AppointmentStatus.Provisional,
+                AttendeeDetails = new AttendeeDetails { FirstName = "Alexander", LastName = "Brown" },
+            },
+        };
+
+        _bookingsDocumentStore
+            .Setup(x => x.GetInDateRangeAsync(It.IsAny<DateTime>(), It.IsAny<DateTime>(), site))
+            .ReturnsAsync(bookings);
+
+        var response = await _bookingQueryService.GetOrderedLiveBookings(site, new DateTime(), new DateTime());
+
+        response.Should().HaveCount(3);
+
+        Assert.Multiple(
+            () => Assert.True(response.ToArray()[0].Reference == "5"),
+            () => Assert.True(response.ToArray()[1].Reference == "6"),
+            () => Assert.True(response.ToArray()[2].Reference == "1")
+        );
     }
 }

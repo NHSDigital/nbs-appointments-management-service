e375650102c1b0efd5e6bb75f52ad1c745ebcc18
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-05-15 09:38:12 +0100

    Fix warnings in AllocationStateServiceTestBase

--- tests/Nhs.Appointments.Core.UnitTests/AllocationState/MultiServiceTests.cs
+++ tests/Nhs.Appointments.Core.UnitTests/AllocationState/MultiServiceTests.cs
@@ -26,7 +26,7 @@ public class MultiServiceTests : AllocationStateServiceTestBase
 
         SetupAvailabilityAndBookings(bookings, sessions);
             
-        var resultingAllocationState = await _sut.BuildRecalculations(MockSite, new DateTime(2025, 1, 1, 9, 0, 0), new DateTime(2025, 1, 1, 9, 10, 0));
+        var resultingAllocationState = await Sut.BuildRecalculations(MockSite, new DateTime(2025, 1, 1, 9, 0, 0), new DateTime(2025, 1, 1, 9, 10, 0));
 
         // Bookings 1, 2, 3, 6 and 7 should be supported
         resultingAllocationState.Where(r => r.Action == AvailabilityUpdateAction.SetToSupported)
@@ -61,7 +61,7 @@ public class MultiServiceTests : AllocationStateServiceTestBase
 
         SetupAvailabilityAndBookings(bookings, sessions);
 
-        var resultingAllocationState = await _sut.BuildRecalculations(MockSite, new DateTime(2025, 1, 1, 9, 0, 0), new DateTime(2025, 1, 1, 9, 10, 0));
+        var resultingAllocationState = await Sut.BuildRecalculations(MockSite, new DateTime(2025, 1, 1, 9, 0, 0), new DateTime(2025, 1, 1, 9, 10, 0));
 
         // Bookings 1 and 2 can be booked
         resultingAllocationState.Where(r => r.Action == AvailabilityUpdateAction.SetToSupported)

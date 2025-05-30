fe963b972b8bf1d62965d8b9e257c16c293b0801
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-05-30 14:14:24 +0100

    AvailabilityBySlots MultipleService Tests

--- tests/Nhs.Appointments.Api.Integration/Scenarios/Availability/AvailabilityBySlotsFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Availability/AvailabilityBySlotsFeatureSteps.cs
@@ -5,10 +5,16 @@ using Xunit.Gherkin.Quick;
 namespace Nhs.Appointments.Api.Integration.Scenarios.Availability;
 
 [Collection("MultipleServicesSerialToggle")]
-[FeatureFile("./Scenarios/Availability/AvailabilityBySlots.feature")]
-public class AvailabilityBySlotsFeatureSteps_MultipleServicesEnabled() : AvailabilityBaseFeatureSteps(Flags.MultipleServices, true);
+[FeatureFile("./Scenarios/Availability/AvailabilityBySlots_SingleService.feature")]
+public class AvailabilityBySlotsFeatureSteps_SingleService_MultipleServicesEnabled()
+    : AvailabilityBaseFeatureSteps(Flags.MultipleServices, true);
 
 [Collection("MultipleServicesSerialToggle")]
-[FeatureFile("./Scenarios/Availability/AvailabilityBySlots.feature")]
-public class AvailabilityBySlotsFeatureSteps_MultipleServicesDisabled() : AvailabilityBaseFeatureSteps(Flags.MultipleServices, false);
-
+[FeatureFile("./Scenarios/Availability/AvailabilityBySlots_SingleService.feature")]
+public class AvailabilityBySlotsFeatureSteps_SingleService_MultipleServicesDisabled()
+    : AvailabilityBaseFeatureSteps(Flags.MultipleServices, false);
+    
+[Collection("MultipleServicesSerialToggle")]
+[FeatureFile("./Scenarios/Availability/AvailabilityBySlots_MultipleServices.feature")]
+public class AvailabilityBySlotsFeatureSteps_MultipleServices()
+    : AvailabilityBaseFeatureSteps(Flags.MultipleServices, true);

7d51c0a4a5bfe8a25cf934a3d538d4c42e0a56d6
ste-banks <stephen.banks4@nhs.net>
2025-05-30 14:08:24 +0100

    User bulk import tests

---
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/BulkImport/UserBulkImportFeatureSteps.cs
@@ -0,0 +1,17 @@
+using Nhs.Appointments.Core.Features;
+using Xunit;
+using Xunit.Gherkin.Quick;
+
+namespace Nhs.Appointments.Api.Integration.Scenarios.BulkImport;
+
+[FeatureFile("./Scenarios/BulkImport/UserBulkImport_Disabled.feature")]
+public abstract class UserBulkImportDisabledFeatureSteps(string flag, bool enabled) : BaseBulkImportFeatureSteps(flag, enabled);
+
+[Collection("BulkImportSerialToggle")]
+public class UserBulkImport_BulkImportDisabled() : UserBulkImportDisabledFeatureSteps(Flags.BulkImport, false);
+
+[Collection("BulkImportSerialToggle")]
+public class UserBulkImport_BulkImportEnabled() : UserBulkImportEnabledFeatureSteps(Flags.BulkImport, true);
+
+[FeatureFile("./Scenarios/BulkImport/UserBulkImport_Enabled.feature")]
+public class UserBulkImportEnabledFeatureSteps(string flag, bool enabled) : BaseBulkImportFeatureSteps(flag, enabled);

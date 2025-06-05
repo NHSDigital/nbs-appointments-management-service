9bd5092b9961c6cf35b155c4c80abca1f4d5d618
sambiramairelogic <samuel.biram@airelogic.com>
2025-06-05 09:38:14 +0100

    Add tests for service correctness

---
+++ tests/CosmosDbSeederTests/ClinicalServicesDocument.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CosmosDbSeederTests;
+
+public class ClinicalServicesDocument
+{
+    [JsonPropertyName("id")] public required string Id { get; set; }
+
+    [JsonPropertyName("docType")] public required string DocType { get; set; }
+
+    [JsonPropertyName("services")] public required Service[] Services { get; set; }
+}
+
+public class Service
+{
+    [JsonPropertyName("id")] public required string Id { get; set; }
+
+    [JsonPropertyName("label")] public required string Label { get; set; }
+}

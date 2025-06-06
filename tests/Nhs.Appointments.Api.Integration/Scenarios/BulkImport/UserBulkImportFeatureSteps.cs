d3b135bfdf5eb3ab72d1847927f3915e50e9c9d4
ste-banks <stephen.banks4@nhs.net>
2025-06-06 13:31:43 +0100

    Addressing PR comments

--- tests/Nhs.Appointments.Api.Integration/Scenarios/BulkImport/UserBulkImportFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/BulkImport/UserBulkImportFeatureSteps.cs
@@ -4,14 +4,10 @@ using Xunit.Gherkin.Quick;
 
 namespace Nhs.Appointments.Api.Integration.Scenarios.BulkImport;
 
+[Collection("BulkImportSerialToggle")]
 [FeatureFile("./Scenarios/BulkImport/UserBulkImport_Disabled.feature")]
-public abstract class UserBulkImportDisabledFeatureSteps(string flag, bool enabled) : BaseBulkImportFeatureSteps(flag, enabled);
+public class UserBulkImport_BulkImportDisabled() : BaseBulkImportFeatureSteps(Flags.BulkImport, false);
 
 [Collection("BulkImportSerialToggle")]
-public class UserBulkImport_BulkImportDisabled() : UserBulkImportDisabledFeatureSteps(Flags.BulkImport, false);
-
-[Collection("BulkImportSerialToggle")]
-public class UserBulkImport_BulkImportEnabled() : UserBulkImportEnabledFeatureSteps(Flags.BulkImport, true);
-
 [FeatureFile("./Scenarios/BulkImport/UserBulkImport_Enabled.feature")]
-public abstract class UserBulkImportEnabledFeatureSteps(string flag, bool enabled) : BaseBulkImportFeatureSteps(flag, enabled);
+public class UserBulkImport_BulkImportEnabled() : BaseBulkImportFeatureSteps(Flags.BulkImport, true);

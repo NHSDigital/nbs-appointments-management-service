e5ce21e0d75c2a9c952b7924196a675b72f73a31
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-06-10 12:01:27 +0100

    Delete commented out code

--- src/api/Nhs.Appointments.Core/IAvailabilityQueryService.cs
+++ src/api/Nhs.Appointments.Core/IAvailabilityQueryService.cs
@@ -4,7 +4,5 @@ public interface IAvailabilityQueryService
 {
     Task<IEnumerable<AvailabilityCreatedEvent>> GetAvailabilityCreatedEventsAsync(string site, DateOnly from);
     Task<IEnumerable<DailyAvailability>> GetDailyAvailability(string site, DateOnly from, DateOnly to);
-
-    // Task<IEnumerable<SessionInstance>> GetSlots(string site, DateOnly from, DateOnly to, bool generateSessionId = false);
     Task<IEnumerable<SessionInstance>> GetSessions(string site, DateOnly from, DateOnly to, bool generateSessionId = false);
 }

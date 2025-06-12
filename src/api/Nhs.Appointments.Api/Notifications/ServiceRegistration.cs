06720affe964936dc0a80ebb68d2ba1c8dda00f9
sambiramairelogic <samuel.biram@airelogic.com>
2025-06-12 17:25:19 +0100

    Add in-code stub for notifications when provider is azure-throttled

--- src/api/Nhs.Appointments.Api/Notifications/ServiceRegistration.cs
+++ src/api/Nhs.Appointments.Api/Notifications/ServiceRegistration.cs
@@ -1,25 +1,28 @@
+using System;
 using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nhs.Appointments.Api.Consumers;
 using Nhs.Appointments.Api.Functions;
 using Nhs.Appointments.Core.Messaging;
 using Nhs.Appointments.Core.Messaging.Events;
-using System;
+using Notify.Client;
 
 namespace Nhs.Appointments.Api.Notifications;
 
 public static class ServiceRegistration
 {
-    public static IServiceCollection AddUserNotifications(this IServiceCollection services)
+    public static IServiceCollection AddUserNotifications(this IServiceCollection services,
+        IConfigurationRoot configuration)
     {
-        var userNotificationsProvider = Environment.GetEnvironmentVariable("Notifications_Provider");
+        var notificationsConfig = configuration.Get<NotificationsConfig>();
 
         services.AddTransient<IUserRolesChangedNotifier, UserRolesChangedNotifier>()
                 .AddTransient<IBookingNotifier, BookingNotifier>()
                 .AddTransient<IPrivacyUtil, PrivacyUtil>()
                 .AddScoped<NotifyBookingReminderFunction>();
 
-        switch (userNotificationsProvider)
+        switch (notificationsConfig.NotificationsProvider)
         {
             case "none":
                 services.AddScoped<IMessageBus, NullMessageBus>();
@@ -35,39 +38,61 @@ public static class ServiceRegistration
                     .AddScoped<IMessageBus, ConsoleLogWithMessageDelivery>()
                     .AddScoped<ISendNotifications, CosmosNotificationClient>();
                 break;
-            case "azure":
-                var notifyBaseUri = Environment.GetEnvironmentVariable("GovNotifyBaseUri");
-                var notifyApiKey = Environment.GetEnvironmentVariable("GovNotifyApiKey");
+            case "azure-throttled":
                 services
-                    .AddScoped(x => new Notify.Client.NotificationClient(notifyBaseUri, notifyApiKey))
+                    .AddScoped<ISendNotifications, FakeNotificationClient>()
+                    .AddNotificationFunctions()
+                    .AddMassTransit();
+                break;
+            case "azure":
+                services
+                    .AddScoped(x =>
+                        new NotificationClient(notificationsConfig.GovNotifyBaseUri,
+                            notificationsConfig.GovNotifyApiKey))
                     .AddScoped<ISendNotifications, GovNotifyClient>()
-                    .AddScoped<IMessageBus, MassTransitBusWrapper>()
-                    .AddScoped<NotifyUserRolesChangedFunction>()
-                    .AddScoped<NotifyBookingMadeFunction>()
-                    .AddScoped<NotifyBookingCancelledFunction>()
-                    .AddScoped<ScheduledBookingRemindersFunction>()
-                    .AddScoped<NotifyBookingRescheduledFunction>()
-                    .AddScoped<NotifyOktaUserRolesChangedFunction>()
-                    .AddMassTransitForAzureFunctions(cfg =>
-                        {
-                            EndpointConvention.Map<UserRolesChanged>(new Uri($"queue:{NotifyUserRolesChangedFunction.QueueName}"));
-                            EndpointConvention.Map<OktaUserRolesChanged>(new Uri($"queue:{NotifyOktaUserRolesChangedFunction.QueueName}"));
-                            EndpointConvention.Map<BookingMade>(new Uri($"queue:{NotifyBookingMadeFunction.QueueName}"));
-                            EndpointConvention.Map<BookingRescheduled>(new Uri($"queue:{NotifyBookingRescheduledFunction.QueueName}"));
-                            EndpointConvention.Map<BookingCancelled>(new Uri($"queue:{NotifyBookingCancelledFunction.QueueName}"));
-                            EndpointConvention.Map<BookingReminder>(new Uri($"queue:{NotifyBookingReminderFunction.QueueName}"));
-                            cfg.AddConsumer<UserRolesChangedConsumer>();
-                            cfg.AddConsumer<OktaUserRolesChangedConsumer>();
-                            cfg.AddConsumer<BookingReminderConsumer>();
-                            cfg.AddConsumer<BookingMadeConsumer>();
-                            cfg.AddConsumer<BookingCancelledConsumer>();
-                            cfg.AddConsumer<BookingRescheduledConsumer>();
-                        },
-                        connectionStringConfigurationKey: "ServiceBusConnectionString");
+                    .AddNotificationFunctions()
+                    .AddMassTransit();
                 break;
             default:
-                throw new NotSupportedException("A null or unsupported notifications provider was specified in the configuration");
+                throw new NotSupportedException(
+                    "A null or unsupported notifications provider was specified in the configuration");
         }
+
+        return services;
+    }
+
+    private static IServiceCollection AddNotificationFunctions(this IServiceCollection services)
+    {
+        services
+            .AddScoped<NotifyUserRolesChangedFunction>()
+            .AddScoped<NotifyBookingMadeFunction>()
+            .AddScoped<NotifyBookingCancelledFunction>()
+            .AddScoped<ScheduledBookingRemindersFunction>()
+            .AddScoped<NotifyBookingRescheduledFunction>()
+            .AddScoped<NotifyOktaUserRolesChangedFunction>();
+        return services;
+    }
+
+    private static IServiceCollection AddMassTransit(this IServiceCollection services)
+    {
+        services.AddMassTransitForAzureFunctions(cfg =>
+            {
+                EndpointConvention.Map<UserRolesChanged>(new Uri($"queue:{NotifyUserRolesChangedFunction.QueueName}"));
+                EndpointConvention.Map<OktaUserRolesChanged>(
+                    new Uri($"queue:{NotifyOktaUserRolesChangedFunction.QueueName}"));
+                EndpointConvention.Map<BookingMade>(new Uri($"queue:{NotifyBookingMadeFunction.QueueName}"));
+                EndpointConvention.Map<BookingRescheduled>(
+                    new Uri($"queue:{NotifyBookingRescheduledFunction.QueueName}"));
+                EndpointConvention.Map<BookingCancelled>(new Uri($"queue:{NotifyBookingCancelledFunction.QueueName}"));
+                EndpointConvention.Map<BookingReminder>(new Uri($"queue:{NotifyBookingReminderFunction.QueueName}"));
+                cfg.AddConsumer<UserRolesChangedConsumer>();
+                cfg.AddConsumer<OktaUserRolesChangedConsumer>();
+                cfg.AddConsumer<BookingReminderConsumer>();
+                cfg.AddConsumer<BookingMadeConsumer>();
+                cfg.AddConsumer<BookingCancelledConsumer>();
+                cfg.AddConsumer<BookingRescheduledConsumer>();
+            },
+            "ServiceBusConnectionString");
         return services;
     }
 }
---
+++ tests/Nhs.Appointments.Api.UnitTests/NotificationsTestServiceProviderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Nhs.Appointments.Core;
+using Nhs.Appointments.Persistance;
+using Nhs.Appointments.Persistance.Models;
+
+namespace Nhs.Appointments.Api.Tests;
+
+public static class NotificationsTestServiceProviderExtensions
+{
+    public static IServiceCollection AddDependenciesNotUnderTest(this IServiceCollection services)
+    {
+        var cosmosClient = new CosmosClient(
+            "https://localhost:8081",
+            "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+            new CosmosClientOptions());
+
+        services
+            .AddLogging()
+            .AddSingleton(cosmosClient)
+            .AddScoped<IMetricsRecorder, InMemoryMetricsRecorder>()
+            .AddAutoMapper(typeof(CosmosAutoMapperProfile))
+            .AddTransient<ITypedDocumentCosmosStore<NotificationConfigurationDocument>,
+                TypedDocumentCosmosStore<NotificationConfigurationDocument>>()
+            .AddTransient<ITypedDocumentCosmosStore<RolesDocument>,
+                TypedDocumentCosmosStore<RolesDocument>>()
+            .AddTransient<ITypedDocumentCosmosStore<SiteDocument>,
+                TypedDocumentCosmosStore<SiteDocument>>()
+            .AddSingleton(TimeProvider.System)
+            .AddTransient<ISiteService, SiteService>()
+            .AddTransient<ISiteStore, SiteStore>()
+            .AddTransient<IRolesStore, RolesStore>()
+            .AddTransient<INotificationConfigurationStore, NotificationConfigurationStore>()
+            .AddTransient<INotificationConfigurationService, NotificationConfigurationService>()
+            .AddSingleton<IMemoryCache, MemoryCache>();
+
+        return services;
+    }
+}

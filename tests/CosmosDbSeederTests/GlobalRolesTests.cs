041ff08f5d860a5d63eab70718e10fba23929220
sambiramairelogic <samuel.biram@airelogic.com>
2025-06-12 09:16:52 +0100

    Write (currently failing) tests to check notification config service ids against those in the clinical services document

--- tests/CosmosDbSeederTests/GlobalRolesTests.cs
+++ tests/CosmosDbSeederTests/GlobalRolesTests.cs
@@ -1,27 +1,17 @@
 using FluentAssertions;
-using Newtonsoft.Json;
 
 namespace CosmosDbSeederTests;
 
-public class GlobalRolesTests
+public class GlobalRolesTests : BaseCosmosDbSeederTest
 {
-    private static GlobalRolesDocument ReadGlobalRoles(string environment)
-    {
-        var globalRoles = JsonConvert.DeserializeObject<GlobalRolesDocument>(File.ReadAllText(Path.Combine(
-            AppDomain.CurrentDomain.BaseDirectory,
-            $"items/{environment}/core_data/global_roles.json")));
-
-        return globalRoles ?? throw new Exception($"Could not read global_roles.json from {environment} environment");
-    }
-
     [Fact]
     public void GlobalRolesShouldBeTheSameInEachEnvironment()
     {
-        var localGlobalRoles = ReadGlobalRoles("local");
-        var devGlobalRoles = ReadGlobalRoles("dev");
-        var intGlobalRoles = ReadGlobalRoles("int");
-        var stagGlobalRoles = ReadGlobalRoles("stag");
-        var prodGlobalRoles = ReadGlobalRoles("prod");
+        var localGlobalRoles = ReadDocument<GlobalRolesDocument>("local");
+        var devGlobalRoles = ReadDocument<GlobalRolesDocument>("dev");
+        var intGlobalRoles = ReadDocument<GlobalRolesDocument>("int");
+        var stagGlobalRoles = ReadDocument<GlobalRolesDocument>("stag");
+        var prodGlobalRoles = ReadDocument<GlobalRolesDocument>("prod");
 
         devGlobalRoles.Should().BeEquivalentTo(intGlobalRoles);
         intGlobalRoles.Should().BeEquivalentTo(stagGlobalRoles);
@@ -35,7 +25,8 @@ public class GlobalRolesTests
     [Fact(DisplayName = "APPT-802: Admin User roles should include user manager")]
     public void AdminUserRolesShouldIncludeManageUsers()
     {
-        var adminRoles = ReadGlobalRoles("local").Roles.Single(role => role.Id == "system:admin-user");
+        var adminRoles = ReadDocument<GlobalRolesDocument>("local").Roles
+            .Single(role => role.Id == "system:admin-user");
 
         adminRoles.Permissions.Should().Contain("users:manage");
     }

72d0ccaa7bd3b8eb9ddeb8e1aa6f5747cf388b67
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-06-18 16:33:03 +0100

    Refactor so that month and week view tests await each individual iteration by using a for loop instead of forEach

--- src/client/testing/page-objects/view-availability-appointment-pages/week-view-availability-page.ts
+++ src/client/testing/page-objects/view-availability-appointment-pages/week-view-availability-page.ts
@@ -43,136 +43,134 @@ export default class WeekViewAvailabilityPage extends RootPage {
   }
 
   async verifyAllDayCardInformationDisplayedCorrectly(
-    expectedDayOverviews: DayOverview[],
+    dayOverview: DayOverview,
   ) {
-    expectedDayOverviews.forEach(async dayOverview => {
-      const cardDiv = this.page
-        .getByRole('heading', {
-          name: dayOverview.header,
-        })
-        .locator('..');
-
-      const header = cardDiv.getByRole('heading', {
+    const cardDiv = this.page
+      .getByRole('heading', {
         name: dayOverview.header,
-      });
+      })
+      .locator('..');
 
-      //assert header
-      await expect(header).toBeVisible();
-
-      const viewDailyAppointmentsButton = cardDiv.getByRole('link', {
-        name: 'View daily appointments',
-      });
-
-      //view daily appointments link visible only if any bookings
-      if (dayOverview.booked > 0) {
-        await expect(viewDailyAppointmentsButton).toBeVisible();
-      } else {
-        await expect(viewDailyAppointmentsButton).not.toBeVisible();
-      }
-
-      if (dayOverview.services.length > 0) {
-        //assert no availability not visible
-        expect(cardDiv.getByText('No availability')).not.toBeVisible();
-
-        //single table
-        expect(cardDiv.getByRole('table')).toBeVisible();
-
-        //table headers!
-        await expect(
-          cardDiv.getByRole('columnheader', { name: 'Time' }),
-        ).toBeVisible();
-
-        await expect(
-          cardDiv.getByRole('columnheader', { name: 'Services' }),
-        ).toBeVisible();
-
-        await expect(
-          cardDiv.getByRole('columnheader', { name: 'Booked', exact: true }),
-        ).toBeVisible();
-
-        await expect(
-          cardDiv.getByRole('columnheader', { name: 'Unbooked', exact: true }),
-        ).toBeVisible();
-
-        await expect(
-          cardDiv.getByRole('columnheader', { name: 'Action' }),
-        ).toBeVisible();
-
-        //only do for a single service for now!!
-        const singleService = dayOverview.services[0];
-
-        const timeCell = cardDiv.getByRole('cell', {
-          name: singleService.sessionTimeInterval,
-        });
-        const serviceCell = cardDiv.getByRole('cell', {
-          name: singleService.serviceName,
-        });
-        const bookedCell = cardDiv.getByRole('cell', {
-          name: `${singleService.booked} booked`,
-        });
-        const unbookedCell = cardDiv.getByRole('cell', {
-          name: `${singleService.unbooked} unbooked`,
-        });
-
-        await expect(timeCell).toBeVisible();
-        await expect(serviceCell).toBeVisible();
-        await expect(bookedCell).toBeVisible();
-        await expect(unbookedCell).toBeVisible();
-
-        //totals
-        await expect(
-          cardDiv.getByText(
-            `Total appointments: ${dayOverview.totalAppointments}`,
-          ),
-        ).toBeVisible();
-        await expect(
-          cardDiv.getByText(`Booked: ${dayOverview.booked}`, {
-            exact: true,
-          }),
-        ).toBeVisible();
-        await expect(
-          cardDiv.getByText(`Unbooked: ${dayOverview.unbooked}`, {
-            exact: true,
-          }),
-        ).toBeVisible();
-      } else {
-        //no services = no availability
-        await expect(cardDiv.getByText('No availability')).toBeVisible();
-
-        //no table
-        expect(cardDiv.getByRole('table')).not.toBeVisible();
-
-        //no table headers!
-        await expect(
-          cardDiv.getByRole('columnheader', { name: 'Time' }),
-        ).not.toBeVisible();
-
-        await expect(
-          cardDiv.getByRole('columnheader', { name: 'Services' }),
-        ).not.toBeVisible();
-
-        await expect(
-          cardDiv.getByRole('columnheader', { name: 'Booked', exact: true }),
-        ).not.toBeVisible();
-
-        await expect(
-          cardDiv.getByRole('columnheader', { name: 'Unbooked', exact: true }),
-        ).not.toBeVisible();
-
-        await expect(
-          cardDiv.getByRole('columnheader', { name: 'Action' }),
-        ).not.toBeVisible();
-
-        //totals
-        await expect(cardDiv.getByText('Total appointments: 0')).toBeVisible();
-        await expect(
-          cardDiv.getByText('Booked: 0', { exact: true }),
-        ).toBeVisible();
-        await expect(
-          cardDiv.getByText('Unbooked: 0', { exact: true }),
-        ).toBeVisible();
-      }
+    const header = cardDiv.getByRole('heading', {
+      name: dayOverview.header,
     });
+
+    //assert header
+    await expect(header).toBeVisible();
+
+    const viewDailyAppointmentsButton = cardDiv.getByRole('link', {
+      name: 'View daily appointments',
+    });
+
+    //view daily appointments link visible only if any bookings
+    if (dayOverview.booked > 0) {
+      await expect(viewDailyAppointmentsButton).toBeVisible();
+    } else {
+      await expect(viewDailyAppointmentsButton).not.toBeVisible();
+    }
+
+    if (dayOverview.services.length > 0) {
+      //assert no availability not visible
+      expect(cardDiv.getByText('No availability')).not.toBeVisible();
+
+      //single table
+      expect(cardDiv.getByRole('table')).toBeVisible();
+
+      //table headers!
+      await expect(
+        cardDiv.getByRole('columnheader', { name: 'Time' }),
+      ).toBeVisible();
+
+      await expect(
+        cardDiv.getByRole('columnheader', { name: 'Services' }),
+      ).toBeVisible();
+
+      await expect(
+        cardDiv.getByRole('columnheader', { name: 'Booked', exact: true }),
+      ).toBeVisible();
+
+      await expect(
+        cardDiv.getByRole('columnheader', { name: 'Unbooked', exact: true }),
+      ).toBeVisible();
+
+      await expect(
+        cardDiv.getByRole('columnheader', { name: 'Action' }),
+      ).toBeVisible();
+
+      //only do for a single service for now!!
+      const singleService = dayOverview.services[0];
+
+      const timeCell = cardDiv.getByRole('cell', {
+        name: singleService.sessionTimeInterval,
+      });
+      const serviceCell = cardDiv.getByRole('cell', {
+        name: singleService.serviceName,
+      });
+      const bookedCell = cardDiv.getByRole('cell', {
+        name: `${singleService.booked} booked`,
+      });
+      const unbookedCell = cardDiv.getByRole('cell', {
+        name: `${singleService.unbooked} unbooked`,
+      });
+
+      await expect(timeCell).toBeVisible();
+      await expect(serviceCell).toBeVisible();
+      await expect(bookedCell).toBeVisible();
+      await expect(unbookedCell).toBeVisible();
+
+      //totals
+      await expect(
+        cardDiv.getByText(
+          `Total appointments: ${dayOverview.totalAppointments}`,
+        ),
+      ).toBeVisible();
+      await expect(
+        cardDiv.getByText(`Booked: ${dayOverview.booked}`, {
+          exact: true,
+        }),
+      ).toBeVisible();
+      await expect(
+        cardDiv.getByText(`Unbooked: ${dayOverview.unbooked}`, {
+          exact: true,
+        }),
+      ).toBeVisible();
+    } else {
+      //no services = no availability
+      await expect(cardDiv.getByText('No availability')).toBeVisible();
+
+      //no table
+      expect(cardDiv.getByRole('table')).not.toBeVisible();
+
+      //no table headers!
+      await expect(
+        cardDiv.getByRole('columnheader', { name: 'Time' }),
+      ).not.toBeVisible();
+
+      await expect(
+        cardDiv.getByRole('columnheader', { name: 'Services' }),
+      ).not.toBeVisible();
+
+      await expect(
+        cardDiv.getByRole('columnheader', { name: 'Booked', exact: true }),
+      ).not.toBeVisible();
+
+      await expect(
+        cardDiv.getByRole('columnheader', { name: 'Unbooked', exact: true }),
+      ).not.toBeVisible();
+
+      await expect(
+        cardDiv.getByRole('columnheader', { name: 'Action' }),
+      ).not.toBeVisible();
+
+      //totals
+      await expect(cardDiv.getByText('Total appointments: 0')).toBeVisible();
+      await expect(
+        cardDiv.getByText('Booked: 0', { exact: true }),
+      ).toBeVisible();
+      await expect(
+        cardDiv.getByText('Unbooked: 0', { exact: true }),
+      ).toBeVisible();
+    }
   }
 
   async verifyDateCardDisplayed(requiredDate: string) {

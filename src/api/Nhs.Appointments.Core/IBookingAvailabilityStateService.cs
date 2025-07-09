70056a355ce1ee57a1ed3f809299ab939fb1087f
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-07-09 17:11:18 +0100

    Proof of concept for a short-circuited logic for HasAvailability for a service.

--- src/api/Nhs.Appointments.Core/IBookingAvailabilityStateService.cs
+++ src/api/Nhs.Appointments.Core/IBookingAvailabilityStateService.cs
@@ -2,6 +2,7 @@ namespace Nhs.Appointments.Core;
 
 public interface IBookingAvailabilityStateService
 {
+    Task<bool> HasAvailability(string service, string site, DateTime from, DateTime to);
     Task<IEnumerable<SessionInstance>> GetAvailableSlots(string site, DateTime from, DateTime to);
     Task<WeekSummary> GetWeekSummary(string site, DateOnly from);
     Task<IEnumerable<BookingAvailabilityUpdate>> BuildRecalculations(string site, DateTime from, DateTime to);

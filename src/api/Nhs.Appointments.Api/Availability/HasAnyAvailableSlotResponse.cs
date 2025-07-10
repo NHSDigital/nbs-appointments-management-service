91664d45625bfc898f279199766a71c3e4578414
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-07-10 14:03:46 +0100

    Function creation and renaming to HasAnyAvailableSlot

---
+++ src/api/Nhs.Appointments.Api/Availability/HasAnyAvailableSlotResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Nhs.Appointments.Api.Availability;
+
+public class HasAnyAvailableSlotResponse : List<HasAnyAvailableSlotResponseItem>
+{
+}
+
+public record HasAnyAvailableSlotResponseItem(string site, bool hasAnyAvailableSlot);

66c95327bbba2cce7bf8cc5bb44c11205ed2edba
pata9 <paul.tallet1@nhs.net>
2025-07-15 10:47:35 +0100

    APPT-XXX: web app service downgraded and scale settings ammended (#798)
    # Description
    
    This change runs the next js server on a lower SKU, it seems to behave
    better on a smaller server and scale settings make more sense. It was
    never hitting the scale configuration even under highload.
    
    Fixes # (issue)
    
    # Checklist:
    
    - [ ] My work is behind a feature toggle (if appropriate)
    - [ ] If my work is behind a feature toggle, I've added a full suite of
    tests for both the ON and OFF state
    - [ ] The ticket number is in the Pull Request title, with format
    "APPT-XXX: My Title Here"
    - [ ] I have ran npm tsc / lint (in the future these will be ran
    automatically)
    - [ ] My code generates no new .NET warnings (in the future these will
    be treated as errors)
    - [ ] If I've added a new Function, it is disabled in all but one of the
    terraform groups (e.g. http_functions)
    - [ ] If I've added a new Function, it has both unit and integration
    tests. Any request body validators have unit tests also
    - [ ] If I've made UI changes, I've added appropriate Playwright and
    Jest tests

--- infrastructure/environments/stag-ukw/main.tf
+++ infrastructure/environments/stag-ukw/main.tf
@@ -70,7 +70,7 @@ module "mya_application_stag_ukw" {
   create_frontdoor                               = false
   create_cosmos_db                               = false
   create_app_config                              = false
-  web_app_service_sku                            = "P2v3"
+  web_app_service_sku                            = "P1v3"
   web_app_service_plan_default_worker_count      = 3
   app_service_plan_zone_redundancy_enabled       = false
   web_app_service_plan_min_worker_count          = 1

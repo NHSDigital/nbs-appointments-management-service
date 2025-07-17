aaeeac5e4dc75717e6258f24e1d657a4f09ba8e4
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-07-17 13:46:13 +0100

    Week Partition WIP

--- src/api/Nhs.Appointments.Core/IBookingAvailabilityStateService.cs
+++ src/api/Nhs.Appointments.Core/IBookingAvailabilityStateService.cs
@@ -2,7 +2,7 @@ namespace Nhs.Appointments.Core;
 
 public interface IBookingAvailabilityStateService
 {
-    Task<(bool hasSlot, bool shortCircuited)> HasAnyAvailableSlot(string service, string site, DateTime from, DateTime to);
+    Task<(bool hasSlot, bool shortCircuited)> HasAnyAvailableSlot(string service, string site, DateOnly from, DateOnly to);
     Task<IEnumerable<SessionInstance>> GetAvailableSlots(string site, DateTime from, DateTime to);
     Task<WeekSummary> GetWeekSummary(string site, DateOnly from);
     Task<IEnumerable<BookingAvailabilityUpdate>> BuildRecalculations(string site, DateTime from, DateTime to);

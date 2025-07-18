a9e065634c626d861eb8ac88fe1d6775e69280bf
sambiramairelogic <samuel.biram@airelogic.com>
2025-07-18 08:59:39 +0100

    Split form and confirmation steps out, as per designs

--- src/client/src/app/site/[site]/reports/reports-page.tsx
+++ src/client/src/app/site/[site]/reports/reports-page.tsx
@@ -1,101 +1,36 @@
 /* eslint-disable react/jsx-props-no-spreading */
 'use client';
-import NhsHeading from '@components/nhs-heading';
-import {
-  BackLink,
-  Button,
-  ButtonGroup,
-  InsetText,
-} from '@nhsuk-frontend-components';
-import { parseToUkDatetime, RFC3339Format, ukNow } from '@services/timeService';
 import { Site } from '@types';
 import { useState } from 'react';
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
-import {
-  DownloadReportFormValues,
-  REPORT_DATE_EARLIEST_ALLOWED,
-} from './download-report-form-schema';
-import Datepicker from '@components/nhsuk-frontend/custom/datepicker';
+import { DownloadReportFormValues } from './download-report-form-schema';
+import DownloadReportForm from './download-report-form';
+import DownloadReportConfirmation from './download-report-confirmation';
 
 interface ReportsPageProps {
   site: Site;
 }
 
 export const ReportsPage = ({ site }: ReportsPageProps) => {
-  const today = ukNow();
-  //const [step, setStep] = useState<'form' | 'download-button'>('form');
-  const [downloadText, setDownloadText] = useState<string | undefined>(
-    undefined,
-  );
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm<DownloadReportFormValues>({
-    defaultValues: {
-      startDate: today.format(RFC3339Format),
-      endDate: today.format(RFC3339Format),
-    },
-  });
-
-  const submitForm: SubmitHandler<DownloadReportFormValues> = async (
-    form: DownloadReportFormValues,
-  ) => {
-    setDownloadText(
-      `Download all data between ${parseToUkDatetime(form.startDate).format('dddd, D MMMM YYYY')} and ${parseToUkDatetime(form.endDate).format('dddd, D MMMM YYYY')}`,
-    );
-  };
+  // TODO: Atm this journey has 2 steps; one to select the dates and one to confirm the download.
+  // For now this is implemented as a simple state toggle, but if a 3rd or more steps are added,
+  // we should use the wizard pattern like the user mnanagement and availability creation pages
+  const [reportRequest, setReportRequest] = useState<
+    DownloadReportFormValues | undefined
+  >(undefined);
 
   return (
     <>
-      <BackLink
-        renderingStrategy={'server'}
-        href={`/site/${site.id}`}
-        text={'Back'}
-      />
-      <NhsHeading title="Select the dates and create a report" />
-      <form onSubmit={handleSubmit(submitForm)}>
-        {errors.root && (
-          <span className="nhsuk-error-message">
-            <span className="nhsuk-u-visually-hidden">Error: </span>
-            {errors.root.message}
-          </span>
-        )}
-        <Controller
-          name="startDate"
-          control={control}
-          render={({ field }) => (
-            <Datepicker
-              id="startDate"
-              label="Start date"
-              hint="For example, 17/05/2024."
-              min={REPORT_DATE_EARLIEST_ALLOWED}
-              max={today.add(3, 'month').format(RFC3339Format)}
-              error={errors.startDate?.message}
-              {...field}
-            />
-          )}
+      {reportRequest === undefined ? (
+        <DownloadReportForm
+          setReportRequest={setReportRequest}
+          goBackHref={`/site/${site.id}`}
         />
-        <Controller
-          name="endDate"
-          control={control}
-          render={({ field }) => (
-            <Datepicker
-              id="endDate"
-              label="End date"
-              hint="For example, 17/05/2024."
-              min={REPORT_DATE_EARLIEST_ALLOWED}
-              max={today.add(3, 'month').format(RFC3339Format)}
-              error={errors.endDate?.message}
-              {...field}
-            />
-          )}
+      ) : (
+        <DownloadReportConfirmation
+          reportRequest={reportRequest}
+          goBack={() => setReportRequest(undefined)}
         />
-        <ButtonGroup>
-          <Button type="submit">Create report</Button>
-        </ButtonGroup>
-      </form>
-      {downloadText && <InsetText>{downloadText}</InsetText>}
+      )}
     </>
   );
 };

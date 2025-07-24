ab3bfce075301f5e84facd67ebfdf2601b67c8ec
pata9 <paul.tallet1@nhs.net>
2025-07-24 16:01:49 +0100

    get reporting working end to end

--- src/client/src/app/reports/download/route.ts
+++ src/client/src/app/reports/download/route.ts
@@ -1,12 +1,32 @@
 'use server';
-import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
   const startDate = request.nextUrl.searchParams.get('startDate');
   const endDate = request.nextUrl.searchParams.get('endDate');
 
-  const apiUrl = `${process.env.NBS_API_BASE_URL}/api/report/site-summary?startDate=${startDate}&endDate=${endDate}`;
+  //const apiUrl = `${process.env.NBS_API_BASE_URL}/api/report/site-summary?startDate=${startDate}&endDate=${endDate}`;
 
-  redirect(apiUrl);
+  const cookieStore = await cookies();
+
+  const tokenCookie = cookieStore.get('token');
+
+  return await fetch(
+    `${process.env.NBS_API_BASE_URL}/api/report/site-summary?startDate=${startDate}&endDate=${endDate}`,
+    {
+      method: 'GET',
+      headers: tokenCookie
+        ? {
+            Authorization: `Bearer ${tokenCookie.value}`,
+          }
+        : undefined,
+    },
+  ).then(fileResponse => {
+    return fileResponse.blob().then(blob => {
+      return new Response(blob, {
+        headers: fileResponse.headers,
+      });
+    });
+  });
 }

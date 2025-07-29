48ba3d9ced5bb813f805c77efbd5ad15b147f4f2
pata9 <paul.tallet1@nhs.net>
2025-07-29 14:29:59 +0100

    fix existing tests

--- src/client/src/app/site/[site]/site-page.test.tsx
+++ src/client/src/app/site/[site]/site-page.test.tsx
@@ -124,10 +124,18 @@ describe('Site Page', () => {
       );
 
       expect(screen.getByRole('link', { name: cardTitle })).toBeInTheDocument();
-      expect(screen.getByRole('link', { name: cardTitle })).toHaveAttribute(
-        'href',
-        `/site/${mockSite.id}/${path}`,
-      );
+
+      if (path === 'reports') {
+        expect(screen.getByRole('link', { name: cardTitle })).toHaveAttribute(
+          'href',
+          `/${path}`,
+        );
+      } else {
+        expect(screen.getByRole('link', { name: cardTitle })).toHaveAttribute(
+          'href',
+          `/site/${mockSite.id}/${path}`,
+        );
+      }
     },
   );
 

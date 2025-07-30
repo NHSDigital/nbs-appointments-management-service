d3cda92391ca98ce4e8184699ed82db4f5500489
pata9 <paul.tallet1@nhs.net>
2025-07-30 13:19:19 +0100

    chunk message to the bus to resolve running aggregation taking more than 5mins

--- src/api/Nhs.Appointments.Core/Reports/SiteSummary/SiteSummaryTrigger.cs
+++ src/api/Nhs.Appointments.Core/Reports/SiteSummary/SiteSummaryTrigger.cs
@@ -24,23 +24,35 @@ public class SiteSummaryTrigger(
             ? Options.Value.FirstRunDate
             : DateTimeToDate(lastRunDate.Value); 
         var endDate = DateTimeToDate(triggeredTime.AddDays(Options.Value.DaysForward));
+        var chunks = SplitDateRange(startDate, endDate, 10).ToArray();
         var sites = await SiteService.GetAllSites();
 
         foreach (var site in sites)
         {
-            await TriggerForSite(site.Id, startDate, endDate);
+            await TriggerForSite(site.Id, chunks);
         }
 
         await Store.SetLastRunDate(triggeredTime);
     }
 
-    private async Task TriggerForSite(string site, DateOnly startDate, DateOnly endDate)
+    private async Task TriggerForSite(string site, (DateOnly startDate, DateOnly endDate)[] chunks)
     {
-        await bus.Send(new AggregateSiteSummaryEvent { Site = site, From = startDate, To = endDate });
+        await bus.Send(chunks.Select(chunk => new AggregateSiteSummaryEvent { Site = site, From = chunk.startDate, To = chunk.endDate }).ToArray());
     }
     
     private static DateOnly DateTimeToDate(DateTimeOffset dateTime)
     {
         return new DateOnly(dateTime.Year, dateTime.Month, dateTime.Day);
+    }
+    
+    private static IEnumerable<(DateOnly startDate, DateOnly endDate)> SplitDateRange(DateOnly start, DateOnly end, int dayChunkSize)
+    {
+        DateOnly chunkEnd;
+        while ((chunkEnd = start.AddDays(dayChunkSize)) < end)
+        {
+            yield return (start, chunkEnd);
+            start = chunkEnd.AddDays(1);
+        }
+        yield return (start, end);
     }
 }

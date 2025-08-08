fed0918ff4db41a2157b6ccc9423e431afd58868
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2025-08-08 10:24:02 +0100

    Add validation rules ands tests

--- src/api/Nhs.Appointments.Api/Validators/GetSitesByAreaRequestValidator.cs
+++ src/api/Nhs.Appointments.Api/Validators/GetSitesByAreaRequestValidator.cs
@@ -11,23 +11,58 @@ public class GetSitesByAreaRequestValidator : AbstractValidator<GetSitesByAreaRe
         RuleFor(x => x.longitude)
             .LessThanOrEqualTo(180)
             .GreaterThanOrEqualTo(-180)
-            .Configure(rule => rule.MessageBuilder = _ => "Provide a valid longitude value (between -180 <-> 180 degrees).");
+            .Configure(rule =>
+                rule.MessageBuilder = _ => "Provide a valid longitude value (between -180 <-> 180 degrees).");
         RuleFor(x => x.latitude)
             .LessThanOrEqualTo(90)
             .GreaterThanOrEqualTo(-90)
-            .Configure(rule => rule.MessageBuilder = _ => "Provide a valid latitude value (between -90 <-> 90 degrees).");
+            .Configure(rule =>
+                rule.MessageBuilder = _ => "Provide a valid latitude value (between -90 <-> 90 degrees).");
         RuleFor(x => x.searchRadius)
             .LessThanOrEqualTo(100000)
             .GreaterThanOrEqualTo(1000)
-            .Configure(rule => rule.MessageBuilder = _ => "Provide a search radius in meters (between 1000 - 100,000m).");
+            .Configure(rule =>
+                rule.MessageBuilder = _ => "Provide a search radius in meters (between 1000 - 100,000m).");
         RuleFor(x => x.maximumRecords)
             .LessThanOrEqualTo(50)
             .GreaterThan(0)
-            .Configure(rule => rule.MessageBuilder = _ => "Provide a number of maximum records (between 1 - 50 records).");
-        // RuleFor(x => x.from)
-        //     .Cascade(CascadeMode.Stop)
-        //     .NotEmpty().WithMessage("Provide a date in 'yyyy-MM-dd'")
-        //     .Must(x => DateOnly.TryParseExact(x, "yyyy-MM-dd", out var _))
-        //     .WithMessage("Provide a date in the format 'yyyy-MM-dd'");
+            .Configure(rule =>
+                rule.MessageBuilder = _ => "Provide a number of maximum records (between 1 - 50 records).");
+        RuleFor(x => x)
+            .Cascade(CascadeMode.Stop)
+            .Must(x =>
+                (
+                    //none of the filters are provided
+                    (x.services == null || x.services.Length == 0) &&
+                    string.IsNullOrEmpty(x.from) & string.IsNullOrEmpty(x.until))
+                || //OR
+                //all of the filters are provided together
+                (x.services?.Length > 0 && !string.IsNullOrEmpty(x.from) && !string.IsNullOrEmpty(x.until))
+            )
+            .WithMessage(
+                "All of the 'supports service filters' (services, from, until) must be provided, or none of them.")
+            .DependentRules(() =>
+            {
+                When(x => x.services?.Length > 0 && !string.IsNullOrEmpty(x.from) && !string.IsNullOrEmpty(x.until),
+                    () =>
+                    {
+                        RuleFor(x => x.services)
+                            .Must(services => services.Length == 1)
+                            .WithMessage("'Services' currently only supports one service");
+                        RuleFor(x => x)
+                            .Cascade(CascadeMode.Stop)
+                            .Must(x => DateOnly.TryParseExact(x.from, "yyyy-MM-dd", out _))
+                            .WithMessage("'From' must be a date in the format 'yyyy-MM-dd'")
+                            .Must(x => DateOnly.TryParseExact(x.until, "yyyy-MM-dd", out _))
+                            .WithMessage("'Until' must be a date in the format 'yyyy-MM-dd'")
+                            .Must(x =>
+                            {
+                                var fromDate = DateOnly.ParseExact(x.from, "yyyy-MM-dd");
+                                var untilDate = DateOnly.ParseExact(x.until, "yyyy-MM-dd");
+                                return untilDate >= fromDate;
+                            })
+                            .WithMessage("'Until' date must be greater than or equal to 'From' date");
+                    });
+            });
     }
 }
--- tests/Nhs.Appointments.Api.UnitTests/Validators/GetSitesByAreaRequestValidatorTests.cs
+++ tests/Nhs.Appointments.Api.UnitTests/Validators/GetSitesByAreaRequestValidatorTests.cs
@@ -8,6 +8,9 @@ public class GetSitesByAreaRequestValidatorTests
 {
     private readonly GetSitesByAreaRequestValidator _sut = new();
 
+    private const string PartialErrorMessage =
+        "All of the 'supports service filters' (services, from, until) must be provided, or none of them.";
+
     [Theory]
     [InlineData(181)]
     [InlineData(-181)]
@@ -119,5 +122,274 @@ public class GetSitesByAreaRequestValidatorTests
         var result = _sut.Validate(request);
         result.IsValid.Should().BeTrue();
         result.Errors.Should().HaveCount(0);            
+    }
+    
+    [Fact]
+    public void Validate_ReturnsSuccess_WhenRequestIsValid_SiteSupportService()
+    {
+        var request = new GetSitesByAreaRequest(
+            180,
+            90,
+            6000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            ["RSV:Adult"],
+            "2025-10-02",
+            "2025-10-30"
+        );
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeTrue();
+        result.Errors.Should().HaveCount(0);            
+    }
+    
+    /// <summary>
+    /// This might be relaxed later when multiple service querying is supported
+    /// </summary>
+    [Fact]
+    public void Validate_ReturnsError_SiteSupportService_MultipleServicesProvided()
+    {
+        var request = new GetSitesByAreaRequest(
+            180,
+            90,
+            6000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            ["RSV:Adult", "COVID:19"],
+            "2025-10-02",
+            "2025-10-30"
+        );
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().ErrorMessage.Should().Be("'Services' currently only supports one service");     
+    }
+    
+    [Fact]
+    public void Validate_ReturnsError_SiteSupportService_FromDateFormatting()
+    {
+        var request = new GetSitesByAreaRequest(
+            180,
+            90,
+            6000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            ["RSV:Adult"],
+            "20251002",
+            "2025-10-30"
+        );
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().ErrorMessage.Should().Be("'From' must be a date in the format 'yyyy-MM-dd'");     
+    }
+    
+    [Fact]
+    public void Validate_ReturnsError_SiteSupportService_UntilDateFormatting()
+    {
+        var request = new GetSitesByAreaRequest(
+            180,
+            90,
+            6000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            ["RSV:Adult"],
+            "2025-10-02",
+            "20251030"
+        );
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().ErrorMessage.Should().Be("'Until' must be a date in the format 'yyyy-MM-dd'");     
+    }
+    
+    [Theory]
+    [InlineData(180, 90, 100000, 50)]
+    [InlineData(-180, -90, 1000, 1)]
+    public void Validate_ReturnsSuccess_WhenRequestIsValid_EmptyServices(double longitude, double latitude, int searchRadius, int maxRecords)
+    {
+        var request = new GetSitesByAreaRequest(
+            longitude,
+            latitude,
+            searchRadius,
+            maxRecords,
+            ["access_need_a", "access_need_b"],
+            false,
+            [],
+            null,
+            null
+        );
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeTrue();
+        result.Errors.Should().HaveCount(0);            
+    }
+    
+    [Fact]
+    public void Validate_ReturnsError_WhenPartialSupportsServiceFiltersProvided_ServicesOnly()
+    {
+        var request = new GetSitesByAreaRequest(
+            0.123,
+            0.456,
+            50000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            ["RSV:Adult"],
+            null,
+            null
+        );
+        
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().ErrorMessage.Should().Be(PartialErrorMessage);
+    }
+    
+    [Fact]
+    public void Validate_ReturnsError_WhenPartialSupportsServiceFiltersProvided_FromOnly()
+    {
+        var request = new GetSitesByAreaRequest(
+            0.123,
+            0.456,
+            50000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            null,
+            "2025-10-03",
+            null
+        );
+        
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().ErrorMessage.Should().Be(PartialErrorMessage);
+    }
+    
+    [Fact]
+    public void Validate_ReturnsError_WhenPartialSupportsServiceFiltersProvided_UntilOnly()
+    {
+        var request = new GetSitesByAreaRequest(
+            0.123,
+            0.456,
+            50000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            null,
+            null,
+            "2025-10-03"
+        );
+        
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().ErrorMessage.Should().Be(PartialErrorMessage);
+    }
+    
+    [Fact]
+    public void Validate_ReturnsError_WhenPartialSupportsServiceFiltersProvided_ServicesAndFromOnly()
+    {
+        var request = new GetSitesByAreaRequest(
+            0.123,
+            0.456,
+            50000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            ["RSV:Adult"],
+            "2025-08-20",
+            null
+        );
+        
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().ErrorMessage.Should().Be(PartialErrorMessage);
+    }
+    
+    [Fact]
+    public void Validate_ReturnsError_WhenPartialSupportsServiceFiltersProvided_ServicesAndUntilOnly()
+    {
+        var request = new GetSitesByAreaRequest(
+            0.123,
+            0.456,
+            50000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            ["RSV:Adult"],
+            null,
+            "2025-08-20"
+        );
+        
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().ErrorMessage.Should().Be(PartialErrorMessage);
+    }
+    
+    [Fact]
+    public void Validate_ReturnsError_WhenPartialSupportsServiceFiltersProvided_FromAndUntilOnly()
+    {
+        var request = new GetSitesByAreaRequest(
+            0.123,
+            0.456,
+            50000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            null,
+            "2025-08-18",
+            "2025-08-20"
+        );
+        
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().ErrorMessage.Should().Be(PartialErrorMessage);
+    }
+    
+    [Fact]
+    public void Validate_ReturnsError_WhenPartialSupportsServiceFiltersProvided_UntilDateLessThanFromDate()
+    {
+        var request = new GetSitesByAreaRequest(
+            0.123,
+            0.456,
+            50000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            ["RSV:Adult"],
+            "2025-08-18",
+            "2025-08-17"
+        );
+        
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().HaveCount(1);
+        result.Errors.Single().ErrorMessage.Should().Be("'Until' date must be greater than or equal to 'From' date");
+    }
+    
+    [Fact]
+    public void Validate_ReturnsSuccess_WhenPartialSupportsServiceFiltersProvided_UntilDateEqualToFromDate()
+    {
+        var request = new GetSitesByAreaRequest(
+            0.123,
+            0.456,
+            50000,
+            50,
+            ["access_need_a", "access_need_b"],
+            false,
+            ["RSV:Adult"],
+            "2025-08-18",
+            "2025-08-18"
+        );
+        
+        var result = _sut.Validate(request);
+        result.IsValid.Should().BeTrue();
     }
 }

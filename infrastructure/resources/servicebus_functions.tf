880048cde05712bf13a89f665db833a53035a57d
Dave-O-NHS <david.olsavsky1@nhs.net>
2025-08-13 14:07:18 +0100

    fixing new function name in terraform

--- infrastructure/resources/servicebus_functions.tf
+++ infrastructure/resources/servicebus_functions.tf
@@ -57,7 +57,7 @@ resource "azurerm_windows_function_app" "nbs_mya_service_bus_func_app" {
     "AzureWebJobs.GetAuthTokenFunction.Disabled"                           = true
     "AzureWebJobs.GetAvailabilityCreatedEventsFunction.Disabled"           = true
     "AzureWebJobs.GetDailyAvailabilityFunction.Disabled"                   = true
-    "AzureWebJobs.GetDaySummaryFunction.Disabled"                          = true
+    "AzureWebJobs.GetDailySummaryFunction.Disabled"                        = true
     "AzureWebJobs.GetEulaFunction.Disabled"                                = true
     "AzureWebJobs.GetRolesFunction.Disabled"                               = true
     "AzureWebJobs.GetSiteFunction.Disabled"                                = true
@@ -168,7 +168,7 @@ resource "azurerm_windows_function_app_slot" "nbs_mya_service_bus_func_app_previ
     "AzureWebJobs.GetAuthTokenFunction.Disabled"                           = true
     "AzureWebJobs.GetAvailabilityCreatedEventsFunction.Disabled"           = true
     "AzureWebJobs.GetDailyAvailabilityFunction.Disabled"                   = true
-    "AzureWebJobs.GetDaySummaryFunction.Disabled"                          = true
+    "AzureWebJobs.GetDailySummaryFunction.Disabled"                        = true
     "AzureWebJobs.GetEulaFunction.Disabled"                                = true
     "AzureWebJobs.GetRolesFunction.Disabled"                               = true
     "AzureWebJobs.GetSiteFunction.Disabled"                                = true

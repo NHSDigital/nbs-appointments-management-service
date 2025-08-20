944abf636034c7c66846f80b2f9ccfd3e616a789
Sam Biram <63285990+sambiramairelogic@users.noreply.github.com>
2025-08-20 12:43:56 +0100

    APPT-1274: Create warning callout (#915)
    # Description
    
    Adds a warning callout to the login page to warn users about the live
    issue.
    
    Also adds a refresh button to the 404 page to prompt users to try
    refreshing the page.
    
    Fixes # (issue)
    <img width="896" height="573" alt="login"
    src="https://github.com/user-attachments/assets/7d47a946-6cbc-4678-8f3e-38d2f875ca54"
    />
    <img width="891" height="500" alt="notfound"
    src="https://github.com/user-attachments/assets/3c1d5c26-1109-486f-97be-18607a8b612e"
    />
    
    # Checklist:
    
    - [ ] My work is behind a feature toggle (if appropriate)
    - [ ] If my work is behind a feature toggle, I've added a full suite of
    tests for both the ON and OFF state
    - [ ] The ticket number is in the Pull Request title, with format
    "APPT-XXX: My Title Here"
    - [ ] I have ran npm tsc / lint (in the future these will be ran
    automatically)
    - [ ] My code generates no new .NET warnings (in the future these will
    be treated as errors)
    - [ ] If I've added a new Function, it is disabled in all but one of the
    terraform groups (e.g. http_functions)
    - [ ] If I've added a new Function, it has both unit and integration
    tests. Any request body validators have unit tests also
    - [ ] If I've made UI changes, I've added appropriate Playwright and
    Jest tests
    
    (cherry picked from commit 6a09ff52b4f850ba6960b9f565feb1f63e9b4e7c)

--- src/client/src/app/not-found.tsx
+++ src/client/src/app/not-found.tsx
@@ -1,5 +1,6 @@
 import ContactUs from '@components/contact-us';
 import NhsAnonymousPage from '@components/nhs-anonymous-page';
+import RefreshButton from '@components/refresh-button';
 import Link from 'next/link';
 
 // TODO: Update this page with approved copy
@@ -14,6 +15,13 @@ export default function NotFound() {
         You may have typed or pasted the web address incorrectly.{' '}
         <Link href="/sites">Go to the start page.</Link>
       </p>
+
+      <p>
+        There is a known issue with signing in. If this happens, please try
+        refreshing the page and you will see the login screen.
+      </p>
+
+      <RefreshButton />
 
       <ContactUs />
     </NhsAnonymousPage>

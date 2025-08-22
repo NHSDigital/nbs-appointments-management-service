9e79ae40a2676ca544b56b0f627ef6985f609e98
sambiramairelogic <samuel.biram@airelogic.com>
2025-08-22 12:22:27 +0100

    Create JointBookingsSerialToggle collection definition

---
+++ tests/Nhs.Appointments.Api.Integration/JointBookingsSerialToggleCollection.cs
@@ -0,0 +1,8 @@
+using Xunit;
+
+namespace Nhs.Appointments.Api.Integration;
+
+[CollectionDefinition("JointBookingsSerialToggle")]
+public class JointBookingsSerialToggleCollection : ICollectionFixture<object>
+{
+}

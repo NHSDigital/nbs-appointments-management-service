2f0cbcd46af48c635002bb4053aa30d582139759
sambiramairelogic <samuel.biram@airelogic.com>
2025-09-01 14:19:24 +0100

    Temporarily do the filtering after the cosmos query until we can figure out why Cosmos says the filter is not supported

--- src/api/Nhs.Appointments.Persistance/BookingQueryFilterExtensions.cs
+++ src/api/Nhs.Appointments.Persistance/BookingQueryFilterExtensions.cs
@@ -30,6 +30,48 @@ public static class BookingQueryFilterExtensions
         return siteMatch && dateRangeMatch && statusMatch && cancellationReasonMatch && notificationStatusMatch;
     }
 
+    /// <summary>
+    ///     Temp oveeride: TODO: remove once Cosmos query works properly
+    /// </summary>
+    /// <param name="booking"></param>
+    /// <param name="filter"></param>
+    /// <returns></returns>
+    public static bool IsMatchedBy(this Booking booking, BookingQueryFilter filter)
+    {
+        if (filter == null || booking == null)
+        {
+            return false;
+        }
+
+        var siteMatch = booking.Site == filter.Site;
+        var dateRangeMatch = booking.From >= filter.StartsAtOrAfter &&
+                             booking.From <= filter.StartsAtOrBefore;
+
+        var statusMatch = filter.Statuses?.Contains(booking.Status) ?? true;
+        var cancellationReasonMatch = filter.CancellationReason == null ||
+                                      booking.CancellationReason == filter.CancellationReason;
+        var notificationStatusMatch = MatchCancellationNotificationStatus(booking, filter);
+
+        return siteMatch && dateRangeMatch && statusMatch && cancellationReasonMatch && notificationStatusMatch;
+    }
+
+    /// <summary>
+    ///     Temp oveeride: TODO: remove once Cosmos query works properly
+    /// </summary>
+    /// <param name="booking"></param>
+    /// <param name="filter"></param>
+    /// <returns></returns>
+    private static bool MatchCancellationNotificationStatus(Booking booking, BookingQueryFilter filter)
+    {
+        // Match any booking if not filtering by cancellationNotificationStatuses
+        if (filter.CancellationNotificationStatuses is null || filter.CancellationNotificationStatuses.Length == 0)
+        {
+            return true;
+        }
+
+        return filter.CancellationNotificationStatuses.Any(status => booking.CancellationNotificationStatus == status);
+    }
+
     private static bool MatchCancellationNotificationStatus(BookingDocument booking, BookingQueryFilter filter)
     {
         // Match any booking if not filtering by cancellationNotificationStatuses

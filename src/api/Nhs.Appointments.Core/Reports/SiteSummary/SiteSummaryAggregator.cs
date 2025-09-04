bfe9c916043cf29ada3e4053b05e214f64d41d76
GLSM1-1 <glen.smith6@nhs.net>
2025-09-04 12:18:15 +0100

    Fix merge issues

--- src/api/Nhs.Appointments.Core/Reports/SiteSummary/SiteSummaryAggregator.cs
+++ src/api/Nhs.Appointments.Core/Reports/SiteSummary/SiteSummaryAggregator.cs
@@ -14,8 +14,7 @@ public class SiteSummaryAggregator(IBookingAvailabilityStateService bookingAvail
 
     private async Task AggregateForSiteDay(string site, DateOnly day)
     {
-        var summary = await FetchSummary(bookingAvailabilityStateService, site, day);
-
+        var generatedAt = timeProvider.GetUtcNow();
         var summary = await bookingAvailabilityStateService.GetDaySummary(site, day);
         
         if (summary.MaximumCapacity <= 0 && summary.TotalOrphanedAppointmentsByService.All(x => x.Value <= 0) && summary.DaySummaries.All(x => x.TotalCancelledAppointments <= 0))

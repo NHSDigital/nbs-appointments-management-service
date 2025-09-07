27daa475736905e38c04ea9d345e1db302edcf3d
pata9 <paul.tallet1@nhs.net>
2025-09-07 13:19:18 +0100

    APPT-XXX: Remove API Client Project (#943)
    # Description
    
    API Client has been sat stale for a long time, removing to tidy up, we
    can bring this back if needed in the future.
    
    Fixes # (issue)
    
    # Checklist:
    
    - [ ] My work is behind a feature toggle (if appropriate)
    - [ ] If my work is behind a feature toggle, I've added a full suite of
    tests for both the ON and OFF state
    - [ ] The ticket number is in the Pull Request title, with format
    "APPT-XXX: My Title Here"
    - [ ] I have ran npm tsc / lint (in the future these will be ran
    automatically)
    - [ ] My code generates no new .NET warnings (in the future these will
    be treated as errors)
    - [ ] If I've added a new Function, it is disabled in all but one of the
    terraform groups (e.g. http_functions)
    - [ ] If I've added a new Function, it has both unit and integration
    tests. Any request body validators have unit tests also
    - [ ] If I've made UI changes, I've added appropriate Playwright and
    Jest tests

--- src/api/Nhs.Appointments.ApiClient/Models/GetTemplateAssignmentsResponse.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Nhs.Appointments.ApiClient.Models
-{
-    public record GetTemplateAssignmentsResponse([property: JsonPropertyName("assignments")] TemplateAssignment[] Assignments);
-}

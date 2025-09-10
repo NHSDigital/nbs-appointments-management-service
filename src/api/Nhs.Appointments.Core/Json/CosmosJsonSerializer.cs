9440dcc09419104056c96e587b3b615289fe4c42
pata9 <paul.tallet1@nhs.net>
2025-09-10 14:50:46 +0100

    reduce duplication

--- src/api/Nhs.Appointments.Core/Json/CosmosJsonSerializer.cs
+++ src/api/Nhs.Appointments.Core/Json/CosmosJsonSerializer.cs
@@ -7,26 +7,29 @@ namespace Nhs.Appointments.Core.Json;
 
 public class CosmosJsonSerializer : CosmosSerializer
 {
+    private static readonly JsonSerializerSettings JsonSerializerSettings = new()
+    {
+        Converters =
+        {
+            new ShortTimeOnlyJsonConverter(), 
+            new ShortDateOnlyJsonConverter(), 
+            new DayOfWeekJsonConverter(), 
+            new Newtonsoft.Json.Converters.StringEnumConverter()
+        }
+    };
+    
     public override T FromStream<T>(Stream stream)
     {
         using (stream)
         {
-            var desrializerSettings = new JsonSerializerSettings
-            {
-                Converters = { new ShortTimeOnlyJsonConverter(), new ShortDateOnlyJsonConverter(), new DayOfWeekJsonConverter(), new Newtonsoft.Json.Converters.StringEnumConverter() }
-            };
             var body = new StreamReader(stream).ReadToEndAsync().GetAwaiter().GetResult();
-            return JsonConvert.DeserializeObject<T>(body, desrializerSettings);
+            return JsonConvert.DeserializeObject<T>(body, JsonSerializerSettings);
         }
     }
 
     public override Stream ToStream<T>(T input)
     {
-        var serializerSettings = new JsonSerializerSettings
-        {
-            Converters = { new ShortTimeOnlyJsonConverter(), new ShortDateOnlyJsonConverter(), new DayOfWeekJsonConverter(), new Newtonsoft.Json.Converters.StringEnumConverter() }
-        };
-        var json = JsonConvert.SerializeObject(input, serializerSettings);
+        var json = JsonConvert.SerializeObject(input, JsonSerializerSettings);
         return new MemoryStream(Encoding.UTF8.GetBytes(json));
     }
 }

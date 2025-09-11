d101068618b1c28612d0a3566645102d03f5d8b2
pata9 <paul.tallet1@nhs.net>
2025-09-11 11:07:45 +0100

    Correct transposing of source for booking extracts

--- data/DataExtract/Documents/NbsAdditionalData.cs
+++ data/DataExtract/Documents/NbsAdditionalData.cs
@@ -13,14 +13,8 @@ public record NbsAdditionalData
 {
     [JsonProperty("referralType")]
     public string ReferralType { get; set; }
-
-    [JsonProperty("isAppBooking")]
-    public bool IsAppBooking { get; set; }
-
-    [JsonProperty("isCallCentreBooking")]
-    public bool IsCallCentreBooking { get; set; }
-
-    [JsonIgnore]
-    public string Source => IsAppBooking ? "NHS App" : IsCallCentreBooking ? "NHS Call Centre" : "NBS";
+    
+    [JsonProperty("source")]
+    public string Source { get; set; }
 
 }

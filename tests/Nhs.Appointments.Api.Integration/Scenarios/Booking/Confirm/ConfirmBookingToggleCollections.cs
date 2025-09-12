f5f18f00a6208ab4dc92667ecb59d457c4b5e6fd
sambiramairelogic <samuel.biram@airelogic.com>
2025-09-12 09:00:54 +0100

    Enable Joint Bookings feature toggle testing in integration tests

---
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/Confirm/ConfirmBookingToggleCollections.cs
@@ -0,0 +1,15 @@
+﻿using Nhs.Appointments.Core.Features;
+using Xunit;
+using Xunit.Gherkin.Quick;
+
+namespace Nhs.Appointments.Api.Integration.Scenarios.Booking.Confirm;
+
+[Collection("ConfirmBookingSerialToggle")]
+[FeatureFile("./Scenarios/Booking/Confirm/ConfirmBooking_JointBookings_Enabled.feature")]
+public class ConfirmBooking_Enabled()
+    : ConfirmBookingFeatureSteps(Flags.JointBookings, true);
+
+[Collection("ConfirmBookingSerialToggle")]
+[FeatureFile("./Scenarios/Booking/Confirm/ConfirmBooking_JointBookings_Disabled.feature")]
+public class ConfirmBooking_Disabled()
+    : ConfirmBookingFeatureSteps(Flags.JointBookings, false);

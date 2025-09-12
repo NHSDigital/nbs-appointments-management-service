fd7c7839f43f59fe11a9d5158d21bed1329e40a7
ste-banks <stephen.banks4@nhs.net>
2025-09-12 14:13:11 +0100

    New edit session endpoint initial commit - WIP

---
+++ src/api/Nhs.Appointments.Api/Functions/EditSessionFunction.cs
@@ -0,0 +1,59 @@
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Nhs.Appointments.Api.Auth;
+using Nhs.Appointments.Api.Json;
+using Nhs.Appointments.Api.Models;
+using Nhs.Appointments.Core;
+using Nhs.Appointments.Core.Inspectors;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Nhs.Appointments.Api.Functions;
+
+public class EditSessionFunction(
+    IValidator<EditSessionRequest> validator,
+    IUserContextProvider userContextProvider,
+    ILogger<BulkImportFunction> logger,
+    IMetricsRecorder metricsRecorder)
+    : BaseApiFunction<EditSessionRequest, EmptyResponse>(validator, userContextProvider, logger,
+        metricsRecorder)
+{
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, "application/json",
+        typeof(ErrorMessageResponseItem), Description = "The body of the request is invalid")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.Unauthorized, "application/json",
+        typeof(ErrorMessageResponseItem), Description = "Unauthorized request to a protected API")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.Forbidden, "application/json", typeof(ErrorMessageResponseItem),
+        Description = "Request failed due to insufficient permissions")]
+    [RequiresPermission(Permissions.SetupAvailability, typeof(SiteFromBodyInspector))]
+    [Function("EditSessionFunction")]
+    public override Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "session/edit")]
+        HttpRequest req)
+    {
+        return base.RunAsync(req);
+    }
+
+    protected override async Task<ApiResult<EmptyResponse>> HandleRequest(EditSessionRequest request, ILogger logger)
+    {
+        return Success(new EmptyResponse());
+    }
+
+    protected override async Task<(IReadOnlyCollection<ErrorMessageResponseItem> errors, EditSessionRequest request)> ReadRequestAsync(HttpRequest req)
+    {
+        var (errors, payload) = await JsonRequestReader.ReadRequestAsync<EditSessionRequest>(req.Body);
+
+        if (errors.Count > 0)
+        {
+            return (errors, null);
+        }
+
+        return (errors, payload);
+    }
+}
---
+++ src/api/Nhs.Appointments.Api/Json/SessionOrWildcardConverter.cs
@@ -0,0 +1,43 @@
+using Newtonsoft.Json;
+using Nhs.Appointments.Api.Models;
+using Nhs.Appointments.Core;
+using System;
+
+namespace Nhs.Appointments.Api.Json;
+public class SessionOrWildcardConverter : JsonConverter<SessionOrWildcard>
+{
+    public override SessionOrWildcard ReadJson(JsonReader reader, Type objectType, SessionOrWildcard existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        if (reader.TokenType == JsonToken.String)
+        {
+            var value = reader.Value?.ToString();
+            return value == "*"
+                ? new SessionOrWildcard {  IsWildcard = true }
+                : throw new JsonSerializationException("Only '*' is allowed as a string value.");
+        }
+        else if (reader.TokenType == JsonToken.StartObject)
+        {
+            var session = serializer.Deserialize<Session>(reader);
+            return new SessionOrWildcard { Session = session };
+        }
+
+        throw new JsonSerializationException("Expected '*' or a session object.");
+    }
+
+    public override void WriteJson(JsonWriter writer, SessionOrWildcard value, JsonSerializer serializer)
+    {
+        var wrapper = (SessionOrWildcard)value;
+        if (wrapper.IsWildcard)
+        {
+            writer.WriteValue("*");
+        }
+        else if (wrapper.Session is not null)
+        {
+            serializer.Serialize(writer, wrapper.Session);
+        }
+        else
+        {
+            writer.WriteNull();
+        }
+    }
+}

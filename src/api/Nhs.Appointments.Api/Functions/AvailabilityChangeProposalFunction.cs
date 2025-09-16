ca10fd40367107923b1a15ba315b7c3a0362fca5
Dave-O-NHS <david.olsavsky1@nhs.net>
2025-09-16 12:52:51 +0100

    Adding tests and small refactoring

--- src/api/Nhs.Appointments.Api/Functions/AvailabilityChangeProposalFunction.cs
+++ src/api/Nhs.Appointments.Api/Functions/AvailabilityChangeProposalFunction.cs
@@ -52,8 +52,8 @@ public class AvailabilityChangeProposalFunction(
 
         return ApiResult<AvailabilityChangeProposalResponse>.Success(
             new AvailabilityChangeProposalResponse(
-                recalculations.ReallocatedBookingsCount, 
-                recalculations.UnaccommodatedBookingsCount
+                recalculations.SupportedBookingsCount, 
+                recalculations.UnsupportedBookingsCount
             )
         );
     }
--- tests/Nhs.Appointments.Api.Integration/Scenarios/AvailabilityCalculations/BestFitFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/AvailabilityCalculations/BestFitFeatureSteps.cs
@@ -1,15 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 using FluentAssertions;
 using Gherkin.Ast;
 using Newtonsoft.Json;
 using Nhs.Appointments.Api.Json;
 using Nhs.Appointments.Api.Models;
 using Nhs.Appointments.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit.Gherkin.Quick;
 
 namespace Nhs.Appointments.Api.Integration.Scenarios.AvailabilityCalculations;
@@ -19,6 +20,7 @@ public class BestFitFeatureSteps : BaseFeatureSteps
 {
     private HttpResponseMessage _response;
     private List<Core.Booking> _getBookingsResponse;
+    private AvailabilityChangeProposalResponse _availabilityChangeProposalResponse;
     private readonly Dictionary<int, string> _bookingReferences = new();
 
     [When(@"I create the following availability")]
@@ -155,5 +157,73 @@ public class BestFitFeatureSteps : BaseFeatureSteps
 
             bookingIndex += 1;
         }
+    }
+
+    [When(@"I request the availability proposal for potential availability change")]
+    public async Task RequestAvailabilityRecalculation(DataTable proposalSessions)
+    {
+        var sessions = new List<Session>{ };
+
+        foreach (var row in proposalSessions.Rows.Skip(1))
+        {
+            var session = new Session
+            {
+                From = TimeOnly.Parse(row.Cells.ElementAt(0).Value),
+                Until = TimeOnly.Parse(row.Cells.ElementAt(1).Value),
+                Services = row.Cells.ElementAt(2).Value.Split(','),
+                SlotLength = int.Parse(row.Cells.ElementAt(3).Value),
+                Capacity = int.Parse(row.Cells.ElementAt(4).Value),
+            };
+
+            sessions.Add(session);
+        }
+
+        var request = new { 
+        
+            site = GetSiteId(),
+            from = DateTime.Today.AddDays(1).ToString("yyyy-MM-dd"),
+            to = DateTime.Today.AddDays(1).ToString("yyyy-MM-dd"),
+            sessionMatcher = new
+            {
+                from = sessions[0].From,
+                until = sessions[0].Until,
+                services = sessions[0].Services,
+                slotLength = sessions[0].SlotLength,
+                capacity = sessions[0].Capacity
+            },
+            sessionReplacement = new
+            {
+                from = sessions[1].From,
+                until = sessions[1].Until,
+                services = sessions[1].Services,
+                slotLength = sessions[1].SlotLength,
+                capacity = sessions[1].Capacity
+            }
+        };
+        var serializerSettings = new JsonSerializerSettings
+        {
+            Converters = { new ShortTimeOnlyJsonConverter() },
+        };
+        var content = new StringContent(JsonConvert.SerializeObject(request, serializerSettings), Encoding.UTF8, "application/json");
+
+        _response = await Http.PostAsync($"http://localhost:7071/api/session/edit-proposal", content);
+        _response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        (_, _availabilityChangeProposalResponse) =
+            await JsonRequestReader.ReadRequestAsync<AvailabilityChangeProposalResponse>(await _response.Content.ReadAsStreamAsync());
+    }
+
+    [Then(@"the following count is returned")]
+    public async Task AssertAvailabilityCount(DataTable expectedCounts)
+    {
+        var counts = new List<int>();
+
+        foreach (var row in expectedCounts.Rows)
+        {
+            counts.Add(int.Parse(row.Cells.ElementAt(1).Value));
+        }
+
+        _availabilityChangeProposalResponse.SupportedBookingsCount.Should().Be(counts[0]);
+        _availabilityChangeProposalResponse.UnsupportedBookingsCount.Should().Be(counts[1]);
     }
 }

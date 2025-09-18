83c5ca29f94a3f2422d788594411affacc06d89e
Dave-O-NHS <david.olsavsky1@nhs.net>
2025-09-18 07:58:53 +0100

    Added recalculation proposal integration tests with feature flag

--- tests/Nhs.Appointments.Api.Integration/Scenarios/AvailabilityCalculations/BestFit.feature
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/AvailabilityCalculations/BestFit.feature
@@ -63,26 +63,6 @@ Feature: The Best Fit Problem
       | Tomorrow | 09:00 | 10       | Orange  | Supported |
       | Tomorrow | 09:00 | 10       | Blue    | Orphaned  |
 
-  Scenario: Recalculate unsupported bookings for proposes availability change
-    Given the site is configured for MYA
-    When I create the following availability
-      | Date     | From  | Until | SlotLength | Capacity | Services    |
-      | Tomorrow | 09:00 | 10:00 | 10         | 1        | Green,Blue  |
-      | Tomorrow | 09:00 | 10:00 | 10         | 1        | Blue,Orange |
-      | Tomorrow | 09:00 | 10:00 | 10         | 1        | Orange,Blue |
-    Then I make the following bookings
-      | Date     | Time  | Duration | Service |
-      | Tomorrow | 09:00 | 10       | Blue    |
-      | Tomorrow | 09:00 | 10       | Orange  |
-      | Tomorrow | 09:00 | 10       | Blue    |
-    When I request the availability proposal for potential availability change
-      | From  | Until | Services   | SlotLength | Capacity |
-      | 09:00 | 10:00 | Green,Blue | 10         | 1        |
-      | 09:00 | 10:00 | Green     | 10         | 1        |
-    Then the following count is returned
-        | supported   | 2 |
-        | unsupported | 1 |
-
   Scenario: Re-supporting orphans prioritises first in
     Given the site is configured for MYA
     When I create the following availability
--- tests/Nhs.Appointments.Api.Integration/Scenarios/AvailabilityCalculations/BestFitFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/AvailabilityCalculations/BestFitFeatureSteps.cs
@@ -1,15 +1,12 @@
 using FluentAssertions;
 using Gherkin.Ast;
-using Newtonsoft.Json;
 using Nhs.Appointments.Api.Json;
-using Nhs.Appointments.Api.Models;
 using Nhs.Appointments.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit.Gherkin.Quick;
 
@@ -18,47 +15,7 @@ namespace Nhs.Appointments.Api.Integration.Scenarios.AvailabilityCalculations;
 [FeatureFile("./Scenarios/AvailabilityCalculations/BestFit.feature")]
 public class BestFitFeatureSteps : BaseFeatureSteps
 {
-    private HttpResponseMessage _response;
     private List<Core.Booking> _getBookingsResponse;
-    private AvailabilityChangeProposalResponse _availabilityChangeProposalResponse;
-    private readonly Dictionary<int, string> _bookingReferences = new();
-
-    [When(@"I create the following availability")]
-    public async Task CreateAvailability(DataTable dataTable)
-    {
-        foreach (var row in dataTable.Rows.Skip(1))
-        {
-            var cells = row.Cells.ToList();
-
-            var date = cells.ElementAt(0).Value;
-            var from = cells.ElementAt(1).Value;
-            var until = cells.ElementAt(2).Value;
-            var slotLength = cells.ElementAt(3).Value;
-            var capacity = cells.ElementAt(4).Value;
-            var services = cells.ElementAt(5).Value;
-
-            var payload = new
-            {
-                date = ParseNaturalLanguageDateOnly(date).ToString("yyyy-MM-dd"),
-                site = GetSiteId(),
-                sessions = new[]
-                {
-                    new
-                    {
-                        from,
-                        until,
-                        slotLength = int.Parse(slotLength),
-                        capacity = int.Parse(capacity),
-                        services = services.Split(',').Select(s => s.Trim()).ToArray()
-                    }
-                },
-                mode = "additive"
-            };
-
-            _response = await Http.PostAsJsonAsync("http://localhost:7071/api/availability", payload);
-            _response.StatusCode.Should().Be(HttpStatusCode.OK);
-        }
-    }
 
     [Then("I cancel the following sessions")]
     public async Task CancelSession(DataTable dataTable)
@@ -83,44 +40,6 @@ public class BestFitFeatureSteps : BaseFeatureSteps
 
             _response = await Http.PostAsJsonAsync("http://localhost:7071/api/session/cancel", payload);
             _response.StatusCode.Should().Be(HttpStatusCode.OK);
-        }
-    }
-
-    [Then("I make the following bookings")]
-    public async Task MakeBookings(DataTable dataTable)
-    {
-        var bookingIndex = 0;
-        foreach (var row in dataTable.Rows.Skip(1))
-        {
-            var cells = row.Cells.ToList();
-            var date = cells.ElementAt(0).Value;
-            var time = cells.ElementAt(1).Value;
-            var duration = cells.ElementAt(2).Value;
-            var service = cells.ElementAt(3).Value;
-
-            object payload = new
-            {
-                from = DateTime.ParseExact(
-                    $"{ParseNaturalLanguageDateOnly(date):yyyy-MM-dd} {time}",
-                    "yyyy-MM-dd HH:mm", null).ToString("yyyy-MM-dd HH:mm"),
-                duration,
-                service,
-                site = GetSiteId(),
-                kind = "booked",
-                attendeeDetails = new
-                {
-                    nhsNumber = NhsNumber, firstName = "John", lastName = "Doe", dateOfBirth = "1987-03-13"
-                }
-            };
-            _response = await Http.PostAsJsonAsync("http://localhost:7071/api/booking", payload);
-            _response.StatusCode.Should().Be(HttpStatusCode.OK);
-
-            var result =
-                JsonConvert.DeserializeObject<MakeBookingResponse>(await _response.Content.ReadAsStringAsync());
-            var bookingReference = result.BookingReference;
-
-            _bookingReferences[bookingIndex] = bookingReference;
-            bookingIndex += 1;
         }
     }
 
@@ -159,71 +78,5 @@ public class BestFitFeatureSteps : BaseFeatureSteps
         }
     }
 
-    [When(@"I request the availability proposal for potential availability change")]
-    public async Task RequestAvailabilityRecalculation(DataTable proposalSessions)
-    {
-        var sessions = new List<Session>{ };
-
-        foreach (var row in proposalSessions.Rows.Skip(1))
-        {
-            var session = new Session
-            {
-                From = TimeOnly.Parse(row.Cells.ElementAt(0).Value),
-                Until = TimeOnly.Parse(row.Cells.ElementAt(1).Value),
-                Services = row.Cells.ElementAt(2).Value.Split(','),
-                SlotLength = int.Parse(row.Cells.ElementAt(3).Value),
-                Capacity = int.Parse(row.Cells.ElementAt(4).Value),
-            };
-
-            sessions.Add(session);
-        }
-
-        var request = new { 
-        
-            site = GetSiteId(),
-            from = DateTime.Today.AddDays(1).ToString("yyyy-MM-dd"),
-            to = DateTime.Today.AddDays(1).ToString("yyyy-MM-dd"),
-            sessionMatcher = new
-            {
-                from = sessions[0].From,
-                until = sessions[0].Until,
-                services = sessions[0].Services,
-                slotLength = sessions[0].SlotLength,
-                capacity = sessions[0].Capacity
-            },
-            sessionReplacement = new
-            {
-                from = sessions[1].From,
-                until = sessions[1].Until,
-                services = sessions[1].Services,
-                slotLength = sessions[1].SlotLength,
-                capacity = sessions[1].Capacity
-            }
-        };
-        var serializerSettings = new JsonSerializerSettings
-        {
-            Converters = { new ShortTimeOnlyJsonConverter() },
-        };
-        var content = new StringContent(JsonConvert.SerializeObject(request, serializerSettings), Encoding.UTF8, "application/json");
-
-        _response = await Http.PostAsync($"http://localhost:7071/api/availability/propose-edit", content);
-        _response.StatusCode.Should().Be(HttpStatusCode.OK);
-
-        (_, _availabilityChangeProposalResponse) =
-            await JsonRequestReader.ReadRequestAsync<AvailabilityChangeProposalResponse>(await _response.Content.ReadAsStreamAsync());
-    }
-
-    [Then(@"the following count is returned")]
-    public async Task AssertAvailabilityCount(DataTable expectedCounts)
-    {
-        var counts = new List<int>();
-
-        foreach (var row in expectedCounts.Rows)
-        {
-            counts.Add(int.Parse(row.Cells.ElementAt(1).Value));
-        }
-
-        _availabilityChangeProposalResponse.SupportedBookingsCount.Should().Be(counts[0]);
-        _availabilityChangeProposalResponse.UnsupportedBookingsCount.Should().Be(counts[1]);
-    }
+   
 }

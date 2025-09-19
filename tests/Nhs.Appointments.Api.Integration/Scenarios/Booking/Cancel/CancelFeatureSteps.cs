206c2509098bc5e6a600acb4643f5746979bd3a2
sambiramairelogic <samuel.biram@airelogic.com>
2025-09-19 12:35:30 +0100

    Create integration test for additional data

---
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/Cancel/CancelFeatureSteps.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit.Gherkin.Quick;
+using DataTable = Gherkin.Ast.DataTable;
+
+namespace Nhs.Appointments.Api.Integration.Scenarios.Booking.Cancel;
+
+[FeatureFile("./Scenarios/Booking/Cancel/Cancel.feature")]
+public class CancelFeatureSteps : BookingBaseFeatureSteps
+{
+    private HttpResponseMessage _response;
+    private HttpStatusCode _responseCode;
+    private string _responseBody;
+
+    [When("I confirm the following bookings")]
+    public async Task ConfirmBookingsTwo(DataTable dataTable)
+    {
+        var (url, payload) = BuildCancelBookingPayload(dataTable);
+
+        var jsonPayload = JsonSerializer.Serialize(payload);
+        var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+        _response = await Http.PostAsync(url, content);
+        _responseCode = _response.StatusCode;
+        _responseBody = await _response.Content.ReadAsStringAsync();
+    }
+
+    private (string url, object payload) BuildCancelBookingPayload(DataTable table)
+    {
+        var row = table.Rows.ElementAt(1);
+
+        var bookingReference = CreateCustomBookingReference(table.GetRowValueOrDefault(row, "Reference")) ??
+                               BookingReferences.GetBookingReference(0, BookingType.Confirmed);
+        var site = GetSiteId();
+
+        var payload = new Dictionary<string, object>();
+
+        var cancellationReason = table.GetRowValueOrDefault(row, "Cancellation reason");
+        if (cancellationReason != null)
+        {
+            payload["cancellationReason"] = cancellationReason;
+        }
+
+        var additionalData = BuildAdditionalDataFromDataTable(table, row);
+        if (additionalData != null)
+        {
+            payload["additionalData"] = additionalData;
+        }
+
+        return ($"http://localhost:7071/api/booking/{bookingReference}/cancel?site={site}", payload);
+    }
+}
--- tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/CancelFeatureSteps.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-using Nhs.Appointments.Core.Features;
-using Xunit;
-using Xunit.Gherkin.Quick;
-
-namespace Nhs.Appointments.Api.Integration.Scenarios.Booking;
-
-[FeatureFile("./Scenarios/Booking/Cancel.feature")]
-public class CancelFeatureSteps : BookingBaseFeatureSteps;

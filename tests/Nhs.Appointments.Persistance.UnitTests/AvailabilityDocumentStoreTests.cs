5d1409c07e617b3d7c2198877465c02c2a4a006d
ste-banks <stephen.banks4@nhs.net>
2025-09-22 16:26:46 +0100

    Availability document store unit tests

--- tests/Nhs.Appointments.Persistance.UnitTests/AvailabilityDocumentStoreTests.cs
+++ tests/Nhs.Appointments.Persistance.UnitTests/AvailabilityDocumentStoreTests.cs
@@ -1,6 +1,10 @@
+using FluentAssertions;
 using Microsoft.Azure.Cosmos;
+using Moq;
 using Nhs.Appointments.Core;
 using Nhs.Appointments.Persistance.Models;
+using System.Linq.Expressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Nhs.Appointments.Persistance.UnitTests;
 public class AvailabilityDocumentStoreTests
@@ -50,5 +54,292 @@ public class AvailabilityDocumentStoreTests
         await _sut.CancelDayAsync("TEST_SITE_123", date);
 
         _documentStore.Verify(x => x.PatchDocument("TEST_SITE_123", "20250101", It.IsAny<PatchOperation[]>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CancelMultipleSessions_ReturnsFailure_WhenNoDocumentsFound()
+    {
+        var from = new DateOnly(2025, 10, 10);
+        var until = new DateOnly(2025, 10, 12);
+        var site = "TEST123";
+        _documentStore.Setup(x => x.RunQueryAsync<DailyAvailabilityDocument>(It.IsAny<Expression<Func<DailyAvailabilityDocument, bool>>>()))
+            .ReturnsAsync((IEnumerable<DailyAvailabilityDocument>)null);
+
+        var result = await _sut.CancelMultipleSessions(site, from, until);
+
+        result.Success.Should().BeFalse();
+        result.Message.Should().Be($"No matching documents found for date range From: {from} - Until: {until} for Site: {site}");
+
+        _documentStore.Verify(x => x.PatchDocument("TEST123", "20251010", It.IsAny<PatchOperation[]>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task CancelMultipleSessions_CancelsAllSessions_ForSingleDay()
+    {
+        _documentStore.Setup(x => x.RunQueryAsync<DailyAvailabilityDocument>(It.IsAny<Expression<Func<DailyAvailabilityDocument, bool>>>()))
+            .ReturnsAsync(new List<DailyAvailabilityDocument>
+            {
+                new()
+                {
+                    Date = new DateOnly(2025, 10, 10),
+                    DocumentType = "availability",
+                    Id =  "20250101",
+                    Site = "TEST123",
+                    Sessions = [
+                        new()
+                        {
+                            Capacity = 2,
+                            From = new TimeOnly(12, 00),
+                            Services = ["RSV:Adult"],
+                            Until = new TimeOnly(18, 00),
+                            SlotLength = 5
+                        }
+                    ]
+                }
+            });
+
+        var result = await _sut.CancelMultipleSessions("TEST123", new DateOnly(2025, 10, 10), new DateOnly(2025, 10, 10));
+
+        result.Success.Should().BeTrue();
+
+        _documentStore.Verify(x => x.PatchDocument("TEST123", "20251010", It.IsAny<PatchOperation[]>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CancelMultipleSessions_CancelsAllSessions_ForMultipleDays()
+    {
+        _documentStore.Setup(x => x.RunQueryAsync<DailyAvailabilityDocument>(It.IsAny<Expression<Func<DailyAvailabilityDocument, bool>>>()))
+            .ReturnsAsync(new List<DailyAvailabilityDocument>
+            {
+                new()
+                {
+                    Date = new DateOnly(2025, 10, 10),
+                    DocumentType = "availability",
+                    Id =  "20250101",
+                    Site = "TEST123",
+                    Sessions = [
+                        new()
+                        {
+                            Capacity = 2,
+                            From = new TimeOnly(12, 00),
+                            Services = ["RSV:Adult"],
+                            Until = new TimeOnly(18, 00),
+                            SlotLength = 5
+                        }
+                    ]
+                },
+                new()
+                {
+                    Date = new DateOnly(2025, 10, 11),
+                    DocumentType = "availability",
+                    Id =  "20250101",
+                    Site = "TEST123",
+                    Sessions = [
+                        new()
+                        {
+                            Capacity = 2,
+                            From = new TimeOnly(12, 00),
+                            Services = ["RSV:Adult"],
+                            Until = new TimeOnly(18, 00),
+                            SlotLength = 5
+                        }
+                    ]
+                },
+                new()
+                {
+                    Date = new DateOnly(2025, 10, 12),
+                    DocumentType = "availability",
+                    Id =  "20250101",
+                    Site = "TEST123",
+                    Sessions = [
+                        new()
+                        {
+                            Capacity = 2,
+                            From = new TimeOnly(12, 00),
+                            Services = ["RSV:Adult"],
+                            Until = new TimeOnly(18, 00),
+                            SlotLength = 5
+                        }
+                    ]
+                }
+            });
+
+        var result = await _sut.CancelMultipleSessions("TEST123", new DateOnly(2025, 10, 10), new DateOnly(2025, 10, 10));
+
+        result.Success.Should().BeTrue();
+
+        _documentStore.Verify(x => x.PatchDocument("TEST123", It.IsAny<string>(), It.IsAny<PatchOperation[]>()), Times.Exactly(3));
+    }
+
+    [Fact]
+    public async Task EditSessions_ReturnsFailure_WhenNoDocumentsFound()
+    {
+        var from = new DateOnly(2025, 10, 10);
+        var until = new DateOnly(2025, 10, 12);
+        var site = "TEST123";
+        var sessionMatcher = new Session
+        {
+            Capacity = 2,
+            From = new TimeOnly(12, 00),
+            Services = ["RSV:Adult"],
+            Until = new TimeOnly(18, 00),
+            SlotLength = 5
+        };
+        var sessionReplacement = new Session
+        {
+            Capacity = 2,
+            From = new TimeOnly(15, 00),
+            Services = ["RSV:Adult", "COVID:65+"],
+            Until = new TimeOnly(17, 00),
+            SlotLength = 5
+        };
+
+        _documentStore.Setup(x => x.RunQueryAsync<DailyAvailabilityDocument>(It.IsAny<Expression<Func<DailyAvailabilityDocument, bool>>>()))
+            .ReturnsAsync((IEnumerable<DailyAvailabilityDocument>)null);
+
+        var result = await _sut.EditSessionsAsync(site, from, until, sessionMatcher,sessionReplacement);
+
+        result.Success.Should().BeFalse();
+        result.Message.Should().Be($"No matching documents found for date range From: {from} - Until: {until} for Site: {site}");
+
+        _documentStore.Verify(x => x.PatchDocument("TEST123", It.IsAny<string>(), It.IsAny<PatchOperation[]>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task EditSessions_ReturnsSuccess_ForSingleDay()
+    {
+        var from = new DateOnly(2025, 10, 10);
+        var until = new DateOnly(2025, 10, 10);
+        var site = "TEST123";
+        var sessionMatcher = new Session
+        {
+            Capacity = 2,
+            From = new TimeOnly(12, 00),
+            Services = ["RSV:Adult"],
+            Until = new TimeOnly(18, 00),
+            SlotLength = 5
+        };
+        var sessionReplacement = new Session
+        {
+            Capacity = 2,
+            From = new TimeOnly(15, 00),
+            Services = ["RSV:Adult", "COVID:65+"],
+            Until = new TimeOnly(17, 00),
+            SlotLength = 5
+        };
+
+        _documentStore.Setup(x => x.RunQueryAsync<DailyAvailabilityDocument>(It.IsAny<Expression<Func<DailyAvailabilityDocument, bool>>>()))
+            .ReturnsAsync(new List<DailyAvailabilityDocument>
+            {
+                new()
+                {
+                    Date = new DateOnly(2025, 10, 10),
+                    DocumentType = "availability",
+                    Id =  "20251010",
+                    Site = "TEST123",
+                    Sessions = [
+                        new()
+                        {
+                            Capacity = 2,
+                            From = new TimeOnly(12, 00),
+                            Services = ["RSV:Adult"],
+                            Until = new TimeOnly(18, 00),
+                            SlotLength = 5
+                        }
+                    ]
+                }
+            });
+
+        var result = await _sut.EditSessionsAsync(site, from, until, sessionMatcher, sessionReplacement);
+
+        result.Success.Should().BeTrue();
+
+        _documentStore.Verify(x => x.PatchDocument(site, "20251010", It.IsAny<PatchOperation[]>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task EditSessions_ReturnsSuccess_ForMultipleDays()
+    {
+        var from = new DateOnly(2025, 10, 10);
+        var until = new DateOnly(2025, 10, 12);
+        var site = "TEST123";
+        var sessionMatcher = new Session
+        {
+            Capacity = 2,
+            From = new TimeOnly(12, 00),
+            Services = ["RSV:Adult"],
+            Until = new TimeOnly(18, 00),
+            SlotLength = 5
+        };
+        var sessionReplacement = new Session
+        {
+            Capacity = 2,
+            From = new TimeOnly(15, 00),
+            Services = ["RSV:Adult", "COVID:65+"],
+            Until = new TimeOnly(17, 00),
+            SlotLength = 5
+        };
+
+        _documentStore.Setup(x => x.RunQueryAsync<DailyAvailabilityDocument>(It.IsAny<Expression<Func<DailyAvailabilityDocument, bool>>>()))
+            .ReturnsAsync(new List<DailyAvailabilityDocument>
+            {
+                new()
+                {
+                    Date = new DateOnly(2025, 10, 10),
+                    DocumentType = "availability",
+                    Id =  "20251010",
+                    Site = "TEST123",
+                    Sessions = [
+                        new()
+                        {
+                            Capacity = 2,
+                            From = new TimeOnly(12, 00),
+                            Services = ["RSV:Adult"],
+                            Until = new TimeOnly(18, 00),
+                            SlotLength = 5
+                        }
+                    ]
+                },
+                new()
+                {
+                    Date = new DateOnly(2025, 10, 11),
+                    DocumentType = "availability",
+                    Id =  "20251010",
+                    Site = "TEST123",
+                    Sessions = [
+                        new()
+                        {
+                            Capacity = 2,
+                            From = new TimeOnly(12, 00),
+                            Services = ["RSV:Adult"],
+                            Until = new TimeOnly(18, 00),
+                            SlotLength = 5
+                        }
+                    ]
+                },
+                new()
+                {
+                    Date = new DateOnly(2025, 10, 12),
+                    DocumentType = "availability",
+                    Id =  "20251010",
+                    Site = "TEST123",
+                    Sessions = [
+                        new()
+                        {
+                            Capacity = 2,
+                            From = new TimeOnly(12, 00),
+                            Services = ["RSV:Adult"],
+                            Until = new TimeOnly(18, 00),
+                            SlotLength = 5
+                        }
+                    ]
+                }
+            });
+
+        var result = await _sut.EditSessionsAsync(site, from, until, sessionMatcher, sessionReplacement);
+
+        result.Success.Should().BeTrue();
+
+        _documentStore.Verify(x => x.PatchDocument(site, It.IsAny<string>(), It.IsAny<PatchOperation[]>()), Times.Exactly(3));
     }
 }

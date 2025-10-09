956586a43793c8a13dfc3808f73de1ec00364b1d
Sam Biram <63285990+sambiramairelogic@users.noreply.github.com>
2025-10-09 12:15:30 +0100

    APPT-875: Update swagger docs for CancelBooking (#1066)
    # Description
    
    Updates the Swagger docs for the CancelBooking endpoint which was
    missing the request body and also the 2nd URL parameter
    
    Fixes # (issue)
    
    # Checklist:
    
    - [ ] My work is behind a feature toggle (if appropriate)
    - [ ] If my work is behind a feature toggle, I've added a full suite of
    tests for both the ON and OFF state
    - [ ] The ticket number is in the Pull Request title, with format
    "APPT-XXX: My Title Here"
    - [ ] I have ran npm tsc / lint (in the future these will be ran
    automatically)
    - [ ] My code generates no new .NET warnings (in the future these will
    be treated as errors)
    - [ ] If I've added a new Function, it is disabled in all but one of the
    terraform groups (e.g. http_functions)
    - [ ] If I've added a new Function, it has both unit and integration
    tests. Any request body validators have unit tests also
    - [ ] If I've made UI changes, I've added appropriate Playwright and
    Jest tests

--- src/api/Nhs.Appointments.Api/Models/CancelBookingRequest.cs
+++ src/api/Nhs.Appointments.Api/Models/CancelBookingRequest.cs
@@ -6,8 +6,12 @@ namespace Nhs.Appointments.Api.Models;
 public record CancelBookingRequest(
     string bookingReference, 
     string site,
+    CancellationReason? cancellationReason,
+    object additionalData = null
+) : CancelBookingOpenApiRequest(cancellationReason, additionalData);
+
+public record CancelBookingOpenApiRequest(
     [property: JsonPropertyName("cancellationReason")]
     CancellationReason? cancellationReason,
     [property: JsonPropertyName("additionalData")]
-    object additionalData = null
-);
+    object additionalData);

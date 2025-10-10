6958cbf2d7d590db5dd0c10dc9e141d39e88deb3
sambiramairelogic <samuel.biram@airelogic.com>
2025-10-10 12:22:03 +0100

    Add tests for exception message parsing

--- src/api/Nhs.Appointments.Api/Notifications/GovNotifyClient.cs
+++ src/api/Nhs.Appointments.Api/Notifications/GovNotifyClient.cs
@@ -1,13 +1,12 @@
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Nhs.Appointments.Api.Notifications.Options;
-using Notify.Client;
-using Notify.Exceptions;
-using Notify.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Nhs.Appointments.Api.Notifications.Options;
+using Notify.Exceptions;
+using Notify.Interfaces;
 
 namespace Nhs.Appointments.Api.Notifications;
 
@@ -29,17 +28,10 @@ public class GovNotifyClient(
             }
             catch (TException ex) when (attempt < retryOptions.Value.MaxRetries)
             {
-
-                // the following pattern is from GovNotify
-                // error handling suggestions:
-                // https://docs.notifications.service.gov.uk/net.html#error-handling
-                var pattern = """(?<=Status code )([0-9]+)""";
-                var timeout = TimeSpan.FromSeconds(2);
-                var r = new Regex(pattern, RegexOptions.IgnoreCase, timeout);
-                var match = r.Match(ex.Message);
-                if (match.Success && int.TryParse(match.Value, out var statusCode))
+                var errorStatusCode = ParseGovNotifyExceptionMessage(ex.Message);
+                if (errorStatusCode != null)
                 {
-                    if (statusCode == 429)
+                    if (errorStatusCode == 429)
                     {
 #pragma warning disable S6667 // Logging in a catch clause should pass the caught exception as a parameter.
                         logger.LogWarning(ex, "Received 429 Too Many Requests. Retrying...");
@@ -60,6 +52,24 @@ public class GovNotifyClient(
                 }
             }
         }
+    }
+
+    public int? ParseGovNotifyExceptionMessage(string message)
+    {
+        // the following pattern is from GovNotify
+        // error handling suggestions:
+        // https://docs.notifications.service.gov.uk/net.html#error-handling
+        var pattern = """(?<=Status code )([0-9]+)""";
+        var timeout = TimeSpan.FromSeconds(2);
+        var r = new Regex(pattern, RegexOptions.IgnoreCase, timeout);
+        var match = r.Match(message);
+
+        if (match.Success && int.TryParse(match.Value, out var statusCode))
+        {
+            return statusCode;
+        }
+
+        return null;
     }
 
     public async Task SendEmailAsync(string emailAddress, string templateId, Dictionary<string, dynamic> templateValues)
--- tests/Nhs.Appointments.Api.UnitTests/Notifications/GovNotifyClientTests.cs
+++ tests/Nhs.Appointments.Api.UnitTests/Notifications/GovNotifyClientTests.cs
@@ -1,18 +1,49 @@
 using FluentAssertions;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
-using Notify.Client;
-using Notify.Exceptions;
 using Nhs.Appointments.Api.Notifications;
-using Notify.Models.Responses;
-using Notify.Interfaces;
 using Nhs.Appointments.Api.Notifications.Options;
-using Microsoft.Extensions.Logging;
+using Notify.Exceptions;
+using Notify.Interfaces;
+using Notify.Models.Responses;
 
 namespace Nhs.Appointments.Api.Tests.Notifications;
 
 public class GovNotifyClientTests
 {
+    /// <summary>
+    ///     Example of GovNotify error format provided in https://docs.notifications.service.gov.uk/net.html#error-handling
+    /// </summary>
+    private const string ExampleErrorFromDocs =
+        "Status code 403. Error: {\"errors\":[{\"error\":\"AuthError\",\"message\":\"Invalid token: API key not found\"}],\"status_code\":403}\n, Exception: Status code 403. The following errors occured [\n  {\n    \"error\": \"AuthError\",\n    \"message\": \"Invalid token: API key not found\"\n  }\n]";
+
+    private readonly Mock<IAsyncNotificationClient> _notificationClient = new();
+    private readonly Mock<ILogger<GovNotifyClient>> _logger = new();
+    private readonly Mock<IPrivacyUtil> _privacyUtil = new();
+    private readonly GovNotifyClient _sut;
+
+    public GovNotifyClientTests()
+    {
+        var retryOptions = Options.Create(new GovNotifyRetryOptions
+        {
+            MaxRetries = 3, InitialDelayMs = 1, BackoffFactor = 1.0
+        });
+
+        _sut = new GovNotifyClient(_notificationClient.Object, _privacyUtil.Object, retryOptions, _logger.Object);
+    }
+
+    [Theory]
+    [InlineData("Status code foo", null)]
+    [InlineData(ExampleErrorFromDocs, 403)]
+    [InlineData("Some text before Status code 422 some text after", 422)]
+    public void CanParseErrorMessages(string error, int? expectedStatusCode)
+    {
+        var result = _sut.ParseGovNotifyExceptionMessage(error);
+
+        result.Should().Be(expectedStatusCode);
+    }
+
     [Fact(DisplayName = "Retries on NotifyClientException")]
     public async Task GovNotifyClient_Retries_On_NotifyClientException()
     {

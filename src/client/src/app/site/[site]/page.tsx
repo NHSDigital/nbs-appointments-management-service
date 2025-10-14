04e72daf89e4d924e668ab79dec77c3627bc36ae
sambiramairelogic <samuel.biram@airelogic.com>
2025-10-14 16:18:53 +0100

    Remove example logging

--- src/client/src/app/site/[site]/page.tsx
+++ src/client/src/app/site/[site]/page.tsx
@@ -9,8 +9,6 @@ import {
 import { SitePage } from './site-page';
 import { Metadata } from 'next/types';
 import fromServer from '@server/fromServer';
-import { logInfo } from '@services/logService';
-import { createCounter } from '@services/metricsService';
 
 // TODO: Get a brief for what titles/description should be on each page
 // Could use the generateMetadata function to dynamically generate this, to include site names / other dynamic content
@@ -45,13 +43,6 @@ const Page = async ({ params }: PageProps) => {
     fromServer(fetchFeatureFlag('SiteSummaryReport')),
     fromServer(fetchFeatureFlag('SiteStatus')),
   ]);
-
-  // Example of using logging
-  logInfo('Loaded site page', { siteOdsCode: site.odsCode });
-
-  // Example of using metrics
-  const counter = createCounter('PAGE_VIEWS', 'page_view_site');
-  counter.add(1, { siteOdsCode: site.odsCode });
 
   return (
     <NhsPage title={site.name} site={site} originPage="site">

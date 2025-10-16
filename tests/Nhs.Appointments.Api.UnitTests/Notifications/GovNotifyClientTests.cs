617c07c7fe4138c1f75843e40b1e8e3b7ab2a939
Sam Biram <63285990+sambiramairelogic@users.noreply.github.com>
2025-10-16 10:01:33 +0100

    APPT-1329: GovNotify retries (#1078)
    # Description
    
    [Ticket 1329](https://nhsd-jira.digital.nhs.uk/browse/APPT-1329) claims
    that when we receive a 400 error from GovNotify we still retry the
    request all 3 times, and that this is waste because we know the 2nd &
    3rd attempts will fail. It says that retries should only be attempted
    when we receive a 429.
    
    As far as I can tell, this is already how this code behaves. This PR
    simply adds tests to prove this behaviour. [The docs on
    this](https://docs.notifications.service.gov.uk/rest-api.html#errors)
    are extremely bad and vague about what exactly the API returns, so there
    is a chance that we're getting a different result back from real
    GovNotify. I've added a unit testing proving that their example error
    message in the docs gets parsed correctly, and also logged out the
    status code in the logs. If behaviour differs in a live environment this
    should alert us to it.
    
    Fixes # (issue)
    
    # Checklist:
    
    - [ ] My work is behind a feature toggle (if appropriate)
    - [ ] If my work is behind a feature toggle, I've added a full suite of
    tests for both the ON and OFF state
    - [ ] The ticket number is in the Pull Request title, with format
    "APPT-XXX: My Title Here"
    - [ ] I have ran npm tsc / lint (in the future these will be ran
    automatically)
    - [ ] My code generates no new .NET warnings (in the future these will
    be treated as errors)
    - [ ] If I've added a new Function, it is disabled in all but one of the
    terraform groups (e.g. http_functions)
    - [ ] If I've added a new Function, it has both unit and integration
    tests. Any request body validators have unit tests also
    - [ ] If I've made UI changes, I've added appropriate Playwright and
    Jest tests

--- tests/Nhs.Appointments.Api.UnitTests/Notifications/GovNotifyClientTests.cs
+++ tests/Nhs.Appointments.Api.UnitTests/Notifications/GovNotifyClientTests.cs
@@ -1,27 +1,116 @@
 using FluentAssertions;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
-using Notify.Client;
-using Notify.Exceptions;
 using Nhs.Appointments.Api.Notifications;
-using Notify.Models.Responses;
-using Notify.Interfaces;
 using Nhs.Appointments.Api.Notifications.Options;
-using Microsoft.Extensions.Logging;
+using Notify.Exceptions;
+using Notify.Interfaces;
+using Notify.Models.Responses;
 
 namespace Nhs.Appointments.Api.Tests.Notifications;
 
 public class GovNotifyClientTests
 {
+    /// <summary>
+    ///     Example of GovNotify error format provided in https://docs.notifications.service.gov.uk/net.html#error-handling
+    /// </summary>
+    private const string ExampleErrorFromDocs =
+        "Status code 403. Error: {\"errors\":[{\"error\":\"AuthError\",\"message\":\"Invalid token: API key not found\"}],\"status_code\":403}\n, Exception: Status code 403. The following errors occured [\n  {\n    \"error\": \"AuthError\",\n    \"message\": \"Invalid token: API key not found\"\n  }\n]";
+
+    private readonly Mock<IAsyncNotificationClient> _notificationClient = new();
+    private readonly Mock<ILogger<GovNotifyClient>> _logger = new();
+    private readonly Mock<IPrivacyUtil> _privacyUtil = new();
+    private readonly GovNotifyClient _sut;
+
+    public GovNotifyClientTests()
+    {
+        var retryOptions = Options.Create(new GovNotifyRetryOptions
+        {
+            MaxRetries = 3, InitialDelayMs = 1, BackoffFactor = 1.0
+        });
+
+        _sut = new GovNotifyClient(_notificationClient.Object, _privacyUtil.Object, retryOptions, _logger.Object);
+    }
+
+    [Theory]
+    [InlineData("Status code foo", null)]
+    [InlineData(ExampleErrorFromDocs, 403)]
+    [InlineData("Some text before Status code 422 some text after", 422)]
+    public void CanParseErrorMessages(string error, int? expectedStatusCode)
+    {
+        var result = _sut.ParseGovNotifyExceptionMessage(error);
+
+        result.Should().Be(expectedStatusCode);
+    }
+
+    [Theory]
+    [InlineData("Status code foo", 1)]
+    [InlineData("Status code 400", 1)]
+    [InlineData("Status code 401", 1)]
+    [InlineData("Status code 403", 1)]
+    [InlineData("Status code 404", 1)]
+    [InlineData("Status code 429", 3)]
+    public async Task SendByEmail_RetriesByErrorStatusCode(string errorMessage, int expectedNumberOfTries)
+    {
+        _notificationClient.Setup(x => x.SendEmailAsync(It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<Dictionary<string, dynamic>>(),
+                null,
+                null,
+                null))
+            .Throws(new NotifyClientException(errorMessage));
+
+        await _sut.SendEmailAsync("test@tempuri.org", "email-template", new Dictionary<string, dynamic>());
+
+        _notificationClient.Verify(
+            x => x.SendEmailAsync(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<Dictionary<string, dynamic>>(),
+                null,
+                null,
+                null
+            ),
+            Times.Exactly(expectedNumberOfTries)
+        );
+    }
+
+    [Theory]
+    [InlineData("Status code foo", 1)]
+    [InlineData("Status code 400", 1)]
+    [InlineData("Status code 401", 1)]
+    [InlineData("Status code 403", 1)]
+    [InlineData("Status code 404", 1)]
+    [InlineData("Status code 429", 3)]
+    public async Task SendBySms_RetriesByErrorStatusCode(string errorMessage, int expectedNumberOfTries)
+    {
+        _notificationClient.Setup(x => x.SendSmsAsync(It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<Dictionary<string, dynamic>>(),
+                null,
+                null))
+            .Throws(new NotifyClientException(errorMessage));
+
+        await _sut.SendSmsAsync("1234567890", "email-template", new Dictionary<string, dynamic>());
+
+        _notificationClient.Verify(
+            x => x.SendSmsAsync(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<Dictionary<string, dynamic>>(),
+                null,
+                null
+            ),
+            Times.Exactly(expectedNumberOfTries)
+        );
+    }
+
     [Fact(DisplayName = "Retries on NotifyClientException")]
     public async Task GovNotifyClient_Retries_On_NotifyClientException()
     {
-        // Arrange
-        var mockClient = new Mock<IAsyncNotificationClient>();
-        var mockLogger = new Mock<ILogger<GovNotifyClient>>();
-
         // Simulate: 2 failures then success
-        mockClient
+        _notificationClient
             .SetupSequence(x => x.SendEmailAsync(
                 It.IsAny<string>(),
                 It.IsAny<string>(),
@@ -34,21 +123,11 @@ public class GovNotifyClientTests
             .ThrowsAsync(new NotifyClientException("Status code 429 second fail"))
             .ReturnsAsync(new EmailNotificationResponse());
 
-        var retryOptions = Options.Create(new GovNotifyRetryOptions
-        {
-            MaxRetries = 3,
-            InitialDelayMs = 1,
-            BackoffFactor = 1.0
-        });
-
-        var privacyUtil = Mock.Of<IPrivacyUtil>();
-        var client = new GovNotifyClient(mockClient.Object, privacyUtil, retryOptions, mockLogger.Object);
-
         // Act
-        await client.SendEmailAsync("test@tempuri.org", "email-template", new());
+        await _sut.SendEmailAsync("test@tempuri.org", "email-template", new Dictionary<string, dynamic>());
 
         // Assert
-        mockClient.Verify(
+        _notificationClient.Verify(
                 x => x.SendEmailAsync(
                     It.IsAny<string>(),
                     It.IsAny<string>(),

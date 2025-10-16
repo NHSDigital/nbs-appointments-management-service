475718d032459963015add93dedc0b6981c97396
GLSM1-1 <glen.smith6@nhs.net>
2025-10-16 15:42:36 +0100

    Amend according to code review

--- src/api/Nhs.Appointments.Api/Validators/AvailabilityChangeProposalRequestValidator.cs
+++ src/api/Nhs.Appointments.Api/Validators/AvailabilityChangeProposalRequestValidator.cs
@@ -6,9 +6,16 @@ public class AvailabilityChangeProposalRequestValidator : AbstractValidator<Avai
 {
     public AvailabilityChangeProposalRequestValidator()
     {
-        RuleFor(x => x.From).NotEmpty().WithMessage("From date is required.");
+        RuleFor(x => x.From)
+            .NotEmpty().WithMessage("From date is required.")
+            .LessThan(x => x.To).WithMessage("From must be earlier than To.");
+
         RuleFor(x => x.To).NotEmpty().WithMessage("To date is required.");
-        RuleFor(x => x.Site).NotEmpty().WithMessage("Site ID is required.");
+        
+        RuleFor(x => x.Site)
+            .NotEmpty().WithMessage("Site ID is required.")
+            .MaximumLength(100).("Site ID must be 100 characters or fewer");
+
         RuleFor(x => x.SessionMatcher).NotEmpty().WithMessage("Session Matcher is required.");
         RuleFor(x => x.SessionReplacement).NotEmpty().WithMessage("Session Replacement is required.");
     }

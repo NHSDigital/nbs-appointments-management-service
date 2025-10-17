09468972fd42d26761f5647256be20829c5e116b
GLSM1-1 <glen.smith6@nhs.net>
2025-10-17 16:44:13 +0100

    Update tests for confirmed page

--- src/client/src/app/site/[site]/availability/edit/confirmed/edit-session-confirmed.test.tsx
+++ src/client/src/app/site/[site]/availability/edit/confirmed/edit-session-confirmed.test.tsx
@@ -17,6 +17,13 @@ describe('Cancellation Confirmed Page', () => {
         date="2025-01-15"
         clinicalServices={mockSingleService}
         site={mockSite}
+        hasBookings={false}
+        chosenAction={''}
+        bookings={[]}
+        unsupportedBookingsCount={0}
+        cancelledWithDetailsCount={0}
+        cancelledWithoutDetailsCount={0}
+        changeSessionUpliftedJourneyEnabled={false}
       />,
     );
 
@@ -38,6 +45,13 @@ describe('Cancellation Confirmed Page', () => {
         clinicalServices={mockSingleService}
         date="2025-01-15"
         site={mockSite}
+        hasBookings={false}
+        chosenAction={''}
+        bookings={[]}
+        unsupportedBookingsCount={0}
+        cancelledWithDetailsCount={0}
+        cancelledWithoutDetailsCount={0}
+        changeSessionUpliftedJourneyEnabled={false}
       />,
     );
 

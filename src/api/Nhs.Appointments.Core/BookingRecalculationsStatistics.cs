00bdf8ba2968b38a9715d40c871f2e1d89b0c14e
pata9 <paul.tallet1@nhs.net>
2025-10-24 11:04:02 +0100

    stats added to edit session endpoint

---
+++ src/api/Nhs.Appointments.Core/BookingRecalculationsStatistics.cs
@@ -0,0 +1,7 @@
+﻿namespace Nhs.Appointments.Core;
+
+public class BookingRecalculationsStatistics
+{
+    public int BookingsCanceled { get; set; }
+    public int BookingsCanceledWithoutDetails { get; set; }
+}

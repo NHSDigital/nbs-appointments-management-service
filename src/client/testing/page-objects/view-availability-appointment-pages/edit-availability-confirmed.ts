763b4435f0fefb6fe787002ed97382252e0480e8
GLSM1-1 <glen.smith6@nhs.net>
2025-10-28 15:56:43 +0000

    update playwright tests

--- src/client/testing/page-objects/view-availability-appointment-pages/edit-availability-confirmed.ts
+++ src/client/testing/page-objects/view-availability-appointment-pages/edit-availability-confirmed.ts
@@ -4,7 +4,7 @@ import RootPage from '../root';
 export default class EditAvailabilityConfirmedPage extends RootPage {
   async verifySessionUpdated() {
     await expect(
-      this.page.getByText('You have successfully edited the session.'),
+      this.page.getByText('Time and capacity changed'),
     ).toBeVisible();
   }
 }

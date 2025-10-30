1d24e70ed67b659f0d0326b4eef2d3a9172bde6e
pata9 <paul.tallet1@nhs.net>
2025-10-30 09:12:33 +0000

    missing files

--- infrastructure/environments/int/main.tf
+++ infrastructure/environments/int/main.tf
@@ -98,6 +98,7 @@ module "mya_application_int" {
   splunk_data_channel                             = "B8FF5D4B-3A5A-4025-AF83-EDB212B83D11"
   splunk_otel_image_version                       = "2.0"
   auto_cancelled_bookings_disabled                = true
+  service_bus_sku                                 = "Standard"
   cosmos_geo_locations = [{
     location          = "uksouth"
     failover_priority = 0

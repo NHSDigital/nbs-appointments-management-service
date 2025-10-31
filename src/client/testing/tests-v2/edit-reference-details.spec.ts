11921306785c58684f636820e05c2213c26d415d
sambiramairelogic <samuel.biram@airelogic.com>
2025-10-31 09:23:50 +0000

    Add playwright tests changing site reference details

--- src/client/testing/tests-v2/edit-reference-details.spec.ts
+++ src/client/testing/tests-v2/edit-reference-details.spec.ts
@@ -1,47 +1,70 @@
 import { buildOdsCode } from '@e2etests/data';
 import { test, expect } from '../fixtures-v2';
 
-// https://nhsd-jira.digital.nhs.uk/browse/APPT-1573
-test.fixme(
-  'A user updates the reference details for a site',
-  async ({ setUpSingleSite }) => {
-    const { sitePage } = await setUpSingleSite(['system:regional-user']);
+test('A regional user updates the reference details for a site but still has access to it through a site manager role', async ({
+  setUpSingleSite,
+}) => {
+  const { sitePage } = await setUpSingleSite([
+    'canned:site-details-manager',
+    'system:regional-user',
+  ]);
 
-    await sitePage
-      .clickSiteDetailsCard()
-      .then(async siteDetailsPage =>
-        siteDetailsPage.clickEditReferenceDetailsLink(),
-      )
-      .then(async editReferenceDetailsPage => {
-        await editReferenceDetailsPage.odsCodeInput.fill('ABC123');
-        await editReferenceDetailsPage.icbSelectInput.selectOption(
-          'Integrated Care Board 2',
-        );
-        await editReferenceDetailsPage.regionSelectInput.selectOption(
-          'Region 2',
-        );
+  await sitePage
+    .clickSiteDetailsCard()
+    .then(async siteDetailsPage =>
+      siteDetailsPage.clickEditReferenceDetailsLink(),
+    )
+    .then(async editReferenceDetailsPage => {
+      await editReferenceDetailsPage.odsCodeInput.fill('ABC123');
+      await editReferenceDetailsPage.icbSelectInput.selectOption(
+        'Integrated Care Board 2',
+      );
+      await editReferenceDetailsPage.regionSelectInput.selectOption('Region 2');
 
-        return editReferenceDetailsPage.saveReferenceDetails();
-      })
-      .then(async siteDetailsPage => {
-        await expect(
-          siteDetailsPage.notificationBanner.getByText(
-            'You have successfully updated the details for the current site.',
-          ),
-        ).toBeVisible();
+      return editReferenceDetailsPage.saveReferenceDetails();
+    })
+    .then(async siteDetailsPage => {
+      await expect(
+        siteDetailsPage.notificationBanner.getByText(
+          'You have successfully updated the reference details for the current site.',
+        ),
+      ).toBeVisible();
 
-        await expect(
-          siteDetailsPage.referenceDetailsCard.summaryList.getItem('ODS code'),
-        ).toHaveText('ABC123');
-        await expect(
-          siteDetailsPage.referenceDetailsCard.summaryList.getItem('ICB'),
-        ).toHaveText('Integrated Care Board 2');
-        await expect(
-          siteDetailsPage.referenceDetailsCard.summaryList.getItem('Region'),
-        ).toHaveText('Region 2');
-      });
-  },
-);
+      await expect(
+        siteDetailsPage.referenceDetailsCard.summaryList.getItem('ODS code'),
+      ).toHaveText('ABC123');
+      await expect(
+        siteDetailsPage.referenceDetailsCard.summaryList.getItem('ICB'),
+      ).toHaveText('Integrated Care Board 2');
+      await expect(
+        siteDetailsPage.referenceDetailsCard.summaryList.getItem('Region'),
+      ).toHaveText('Region 2');
+    });
+});
+
+test('A user updates the reference details for a site but loses access to it as they do', async ({
+  setUpSingleSite,
+}) => {
+  const { sitePage } = await setUpSingleSite(['system:regional-user']);
+
+  await sitePage
+    .clickSiteDetailsCard()
+    .then(async siteDetailsPage =>
+      siteDetailsPage.clickEditReferenceDetailsLink(),
+    )
+    .then(async editReferenceDetailsPage => {
+      await editReferenceDetailsPage.odsCodeInput.fill('ABC123');
+      await editReferenceDetailsPage.icbSelectInput.selectOption(
+        'Integrated Care Board 2',
+      );
+      await editReferenceDetailsPage.regionSelectInput.selectOption('Region 2');
+
+      return editReferenceDetailsPage.saveReferenceDetailsExpectingLossOfAccess();
+    })
+    .then(async siteSelectionPage => {
+      await expect(siteSelectionPage.title).toBeVisible();
+    });
+});
 
 test('A user starts to update the reference details for a site then changes their mind using the back button', async ({
   setUpSingleSite,

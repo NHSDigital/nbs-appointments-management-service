62ef22dfeacb7a8311305b693a618cdf044fbd00
GLSM1-1 <glen.smith6@nhs.net>
2025-11-05 17:25:42 +0000

    add notification details page

---
+++ src/client/src/app/site/[site]/availability/edit-services/no-notifications/no-notifications-page.tsx
@@ -0,0 +1,33 @@
+import { SessionBookingsContactDetailsPage } from '@components/session-bookings-contact-details';
+import { Booking, ClinicalService } from '@types';
+
+type Props = {
+  bookings: Booking[];
+  site: string;
+  clinicalServices: ClinicalService[];
+  cancelledWithoutDetailsCount: number;
+};
+
+export const NoNotificationsPage = ({
+  bookings,
+  site,
+  clinicalServices,
+  cancelledWithoutDetailsCount,
+}: Props) => {
+  return (
+    <>
+      <p>
+        {cancelledWithoutDetailsCount}{' '}
+        {cancelledWithoutDetailsCount > 1 ? 'people' : 'person'} did not get a
+        cancellation notification as they had no contact details on their
+        booking.
+      </p>
+      <SessionBookingsContactDetailsPage
+        bookings={bookings}
+        site={site}
+        displayAction={false}
+        clinicalServices={clinicalServices}
+      />
+    </>
+  );
+};

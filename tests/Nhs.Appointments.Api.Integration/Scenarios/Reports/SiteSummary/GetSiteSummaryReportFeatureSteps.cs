c934e229739f26be09bca41a2a01b792f4c22cf3
sambiramairelogic <samuel.biram@airelogic.com>
2025-11-06 15:09:14 +0000

    Add tests for report headers

--- tests/Nhs.Appointments.Api.Integration/Scenarios/Reports/SiteSummary/GetSiteSummaryReportFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Reports/SiteSummary/GetSiteSummaryReportFeatureSteps.cs
@@ -23,6 +23,7 @@ public class GetSiteSummaryReportFeatureSteps_Disabled()
 public abstract class GetSiteSummaryReportFeatureSteps(string flag, bool enabled) : FeatureToggledSteps(flag, enabled)
 {
     private HttpResponseMessage Response { get; set; }
+    private string ReportContent { get; set; }
 
     [When(@"I request a site summary report for the following dates")]
     public async Task RequestReport(DataTable dataTable)
@@ -36,6 +37,7 @@ public abstract class GetSiteSummaryReportFeatureSteps(string flag, bool enabled
             $"http://localhost:7071/api/report/site-summary?startDate={startDate.ToString("yyyy-MM-dd")}&endDate={endDate.ToString("yyyy-MM-dd")}";
 
         Response = await Http.GetAsync(url);
+        ReportContent = await Response.Content.ReadAsStringAsync();
     }
 
     [Then(@"the call should fail with (\d*)")]
@@ -43,4 +45,16 @@ public abstract class GetSiteSummaryReportFeatureSteps(string flag, bool enabled
 
     [Then(@"the call should be successful")]
     public void AssertSuccessCode() => Response.EnsureSuccessStatusCode();
+
+    [And("the report has the following headers")]
+    public void AssertHeaders(DataTable dataTable)
+    {
+        var csvHeaders = ReportContent.Split("\n")[0].Trim('\r').Split(",");
+
+        var row = dataTable.Rows.First();
+        foreach (var cell in row.Cells)
+        {
+            csvHeaders.Should().Contain(cell.Value);
+        }
+    }
 }
--- tests/Nhs.Appointments.Api.Integration/Scenarios/Reports/SiteSummary/GetSiteSummaryReport_Enabled.feature
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Reports/SiteSummary/GetSiteSummaryReport_Enabled.feature
@@ -1,8 +1,16 @@
 Feature: Download Site Summary Report
 
-  Scenario: Cannot download reports when toggle is disabled
+  Scenario: Can download reports when toggle is enabled
     Given the site is configured for MYA
     When I request a site summary report for the following dates
       | Start Date | End Date          |
       | Tomorrow   | 2 days from today |
     Then the call should be successful
+    And the report has the following headers
+      | Site Name | Site Type | ICB | ICB Name | Region | Region Name | ODS Code | Longitude | Latitude |
+    And the report has the following headers
+      | RSV:Adult Booked | COVID:5_11 Booked | COVID:12_17 Booked | COVID:18+ Booked | FLU:18_64 Booked | FLU:65+ Booked | COVID_FLU:18_64 Booked | COVID_FLU:65+ Booked | FLU:2_3 Booked |
+    And the report has the following headers
+      | Total Bookings | Cancelled | Maximum Capacity |
+    And the report has the following headers
+      | RSV:Adult Capacity | COVID:5_11 Capacity | COVID:12_17 Capacity | COVID:18+ Capacity | FLU:18_64 Capacity | FLU:65+ Capacity | COVID_FLU:18_64 Capacity | COVID_FLU:65+ Capacity | FLU:2_3 Capacity |

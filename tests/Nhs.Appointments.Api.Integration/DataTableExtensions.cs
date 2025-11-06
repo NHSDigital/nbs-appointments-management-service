50127920a178f8d4464ffdb857841b31c3a4df17
sambiramairelogic <samuel.biram@airelogic.com>
2025-11-06 12:33:42 +0000

    Add integration test for feature toggle

--- tests/Nhs.Appointments.Api.Integration/DataTableExtensions.cs
+++ tests/Nhs.Appointments.Api.Integration/DataTableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Gherkin.Ast;
+using Nhs.Appointments.Api.Integration.Data;
 
 namespace Nhs.Appointments.Api.Integration;
 
@@ -24,6 +25,16 @@ public static class DataTableExtensions
         var cell = row.Cells.SingleOrDefault(cell => cell.Location.Column == columnLocation)?.Value;
 
         return HasValue(cell) ? Enum.Parse<T>(cell) : null;
+    }
+
+    public static DateOnly GetNaturalLanguageDateRowValueOrDefault(this DataTable dataTable, TableRow row,
+        string columnName)
+    {
+        var columnLocation = dataTable.Rows.ElementAt(0).Cells.SingleOrDefault(cell => cell.Value == columnName)
+            ?.Location.Column;
+        var cell = row.Cells.SingleOrDefault(cell => cell.Location.Column == columnLocation)?.Value;
+
+        return HasValue(cell) ? NaturalLanguageDate.Parse(cell) : default;
     }
 
     public static int GetIntRowValueOrDefault(this DataTable dataTable, TableRow row, string columnName,
--- tests/Nhs.Appointments.Api.Integration/Scenarios/Reports/SiteSummary/GetSiteSummaryReportFeatureSteps.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Reports/SiteSummary/GetSiteSummaryReportFeatureSteps.cs
@@ -1,4 +1,9 @@
-﻿using Nhs.Appointments.Api.Integration.Scenarios.ChangeSessionUpliftedJourney;
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Gherkin.Ast;
 using Nhs.Appointments.Core.Features;
 using Xunit;
 using Xunit.Gherkin.Quick;
@@ -8,14 +13,31 @@ namespace Nhs.Appointments.Api.Integration.Scenarios.Reports.SiteSummary;
 [Collection("SiteSummaryReportToggle")]
 [FeatureFile("./Scenarios/Reports/SiteSummary/GetSiteSummaryReport_Enabled.feature")]
 public class GetSiteSummaryReportFeatureSteps_Enabled()
-    : ChangeSessionUpliftedJourneyFeatureSteps(Flags.SiteSummaryReport, true);
+    : GetSiteSummaryReportFeatureSteps(Flags.SiteSummaryReport, true);
 
 [Collection("SiteSummaryReportToggle")]
 [FeatureFile("./Scenarios/Reports/SiteSummary/GetSiteSummaryReport_Disabled.feature")]
 public class GetSiteSummaryReportFeatureSteps_Disabled()
-    : ChangeSessionUpliftedJourneyFeatureSteps(Flags.SiteSummaryReport, false);
+    : GetSiteSummaryReportFeatureSteps(Flags.SiteSummaryReport, false);
 
 public class GetSiteSummaryReportFeatureSteps(string flag, bool enabled) : FeatureToggledSteps(flag, enabled)
 {
+    private HttpResponseMessage Response { get; set; }
 
+    [When(@"I request a site summary report for the following dates")]
+    public async Task RequestReport(DataTable dataTable)
+    {
+        var row = dataTable.Rows.Skip(1).Single();
+
+        var startDate = dataTable.GetNaturalLanguageDateRowValueOrDefault(row, "Start Date");
+        var endDate = dataTable.GetNaturalLanguageDateRowValueOrDefault(row, "End Date");
+
+        var url =
+            $"http://localhost:7071/api/report/site-summary?startDate={startDate.ToString("yyyy-MM-dd")}&endDate={endDate.ToString("yyyy-MM-dd")}";
+
+        Response = await Http.GetAsync(url);
+    }
+
+    [Then(@"the call should fail with (\d*)")]
+    public void AssertFailureCode(int statusCode) => Response.StatusCode.Should().Be((HttpStatusCode)statusCode);
 }

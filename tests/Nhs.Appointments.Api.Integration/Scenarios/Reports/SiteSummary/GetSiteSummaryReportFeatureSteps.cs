8001293fd872a52f2b18dca637a2f1e8c3e7f3bc
sambiramairelogic <samuel.biram@airelogic.com>
2025-11-06 10:52:00 +0000

    Create collection toggle and empty step/feature files

---
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Reports/SiteSummary/GetSiteSummaryReportFeatureSteps.cs
@@ -0,0 +1,21 @@
+﻿using Nhs.Appointments.Api.Integration.Scenarios.ChangeSessionUpliftedJourney;
+using Nhs.Appointments.Core.Features;
+using Xunit;
+using Xunit.Gherkin.Quick;
+
+namespace Nhs.Appointments.Api.Integration.Scenarios.Reports.SiteSummary;
+
+[Collection("SiteSummaryReportToggle")]
+[FeatureFile("./Scenarios/Reports/SiteSummary/GetSiteSummaryReport_Enabled.feature")]
+public class GetSiteSummaryReportFeatureSteps_Enabled()
+    : ChangeSessionUpliftedJourneyFeatureSteps(Flags.SiteSummaryReport, true);
+
+[Collection("SiteSummaryReportToggle")]
+[FeatureFile("./Scenarios/Reports/SiteSummary/GetSiteSummaryReport_Disabled.feature")]
+public class GetSiteSummaryReportFeatureSteps_Disabled()
+    : ChangeSessionUpliftedJourneyFeatureSteps(Flags.SiteSummaryReport, false);
+
+public class GetSiteSummaryReportFeatureSteps(string flag, bool enabled) : FeatureToggledSteps(flag, enabled)
+{
+
+}

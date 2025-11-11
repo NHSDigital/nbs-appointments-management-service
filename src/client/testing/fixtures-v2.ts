c8524ece574442bef458aed2c5319d21aa0ba395
sambiramairelogic <samuel.biram@airelogic.com>
2025-11-11 16:17:08 +0000

    Add in feature toggle clear

--- src/client/testing/fixtures-v2.ts
+++ src/client/testing/fixtures-v2.ts
@@ -87,7 +87,7 @@ export const test = base.extend<MyaFixtures>({
     await Promise.all([
       await cosmosDbClient.deleteSite(testId),
       await cosmosDbClient.deleteUser(testId),
-      // TODO: Revert any feature toggles set for this test. Ensure tests sharing a toggle do not run in parallel.
+      await featureFlagClient.clearAllFeatureFlagOverrides(),
     ]);
   },
 });

d146d48874d515e3a300a6a96689afffe8110978
sambiramairelogic <samuel.biram@airelogic.com>
2025-11-17 11:55:57 +0000

    Create recursive implementation of algo

---
+++ src/api/Nhs.Appointments.Core/Availability/AvailableSlotsFilterRecursive.cs
@@ -0,0 +1,77 @@
+using Nhs.Appointments.Core.Extensions;
+
+namespace Nhs.Appointments.Core.Availability;
+
+public class AvailableSlotsFilterRecursive : IAvailableSlotsFilter
+{
+    /// <summary>
+    ///     This method will group consecutive slots based on a number of attendees' and their required services
+    ///     It will filter out any slots that don't match the requested services before processing
+    ///     If only a single attendee is passed in, just return all slots that have already been filtered by service type and
+    ///     have a capacity > 0
+    /// </summary>
+    /// <param name="slots">Available slots returned from the BASS for a given date range</param>
+    /// <param name="attendees">
+    ///     A list of attendees containing an array of their requested services (currently restricted to 1
+    ///     service 14/11/25)
+    /// </param>
+    /// <returns></returns>
+    public IEnumerable<SessionInstance> FilterAvailableSlots(List<SessionInstance> slots, List<Attendee> attendees)
+    {
+        if (attendees is null || attendees.Count == 0)
+        {
+            return slots.OrderBy(s => s.From);
+        }
+
+        if (slots is null || slots.Count == 0)
+        {
+            return new List<SessionInstance>();
+        }
+
+        var requiredServices = attendees.SelectMany(attendee => attendee.Services).ToList();
+
+        var validSlots = slots.Where(slot => CanFormValidGroupBooking(slot, slots, requiredServices));
+
+        return validSlots.OrderBy(s => s.From);
+    }
+
+    private bool CanFormValidGroupBooking(SessionInstance candidateSlot, List<SessionInstance> allSlots,
+        List<string> servicesYetToFulfil)
+    {
+        var serviceMatchesInSlot = candidateSlot.Services.Intersect(servicesYetToFulfil).ToList();
+        if (!serviceMatchesInSlot.Any() || candidateSlot.Capacity < 1)
+        {
+            // Slot doesn't offer the services we need, return false
+            return false;
+        }
+
+        if (servicesYetToFulfil.Count == 1)
+        {
+            // Slot can support our last service, no need to recurse further
+            return true;
+        }
+
+        var neighboursOfSlot = allSlots
+            .Where(otherSlot =>
+                otherSlot.From == candidateSlot.Until
+                || otherSlot.Until == candidateSlot.From)
+            .ToList();
+
+        var validPathFound = false;
+
+        foreach (var serviceMatch in serviceMatchesInSlot)
+        {
+            var remainingServices = servicesYetToFulfil.RemoveFirstOccurrence(serviceMatch);
+
+            foreach (var neighbouringSlot in neighboursOfSlot)
+            {
+                if (CanFormValidGroupBooking(neighbouringSlot, allSlots, remainingServices))
+                {
+                    validPathFound = true;
+                }
+            }
+        }
+
+        return validPathFound;
+    }
+}

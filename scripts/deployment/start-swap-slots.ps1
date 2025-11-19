7d5d9d843fd29c244529915505bbe8d208c9704c
sambiramairelogic <samuel.biram@airelogic.com>
2025-11-19 08:49:53 +0000

    Add trycatch to cater for first time runs

--- scripts/deployment/start-swap-slots.ps1
+++ scripts/deployment/start-swap-slots.ps1
@@ -12,13 +12,25 @@ $webAppService = "nbs-mya-app-$environment-$region/preview"
 $functionAppServices = @("nbs-mya-func-$environment-$region/preview", "nbs-mya-hlfunc-$environment-$region/preview", "nbs-mya-sbfunc-$environment-$region/preview", "nbs-mya-timerfunc-$environment-$region/preview")
 
 foreach ($functionApp in $functionAppServices) {
-  az functionapp start `
-    --resource-group $resourceGroup `
-    --name $functionApp
+  try {
+    az functionapp start `
+      --resource-group $resourceGroup `
+      --name $functionApp
+    Write-Host "Successfully started swap slot: $functionApp"
+  } catch {
+    Write-Warning "Failed to start swap slot: $functionApp. If this is the first time the pipeline is ran, this might be because terraform apply has not yet created that slot. Error: $_"
+  }
 }
 
-az webapp start `
-  --name $webAppService `
-  --resource-group $resourceGroup
+
+try {
+  az webapp start `
+    --name $webAppService `
+    --resource-group $resourceGroup
+  Write-Host "Successfully started swap slot: $functionApp"
+} catch {
+  Write-Warning "Failed to start swap slot: $functionApp. If this is the first time the pipeline is ran, this might be because terraform apply has not yet created that slot. Error: $_"
+}
+
 
 

42f508b3385b96a9e3eb4fba031695443fc6bed8
ste-banks <stephen.banks4@nhs.net>
2025-11-20 11:28:13 +0000

    Adding logic to return day availability model. Also adding validator - WIP

--- src/api/Nhs.Appointments.Api/Functions/HttpFunctions/QueryAvailabilityByDaysFunction.cs
+++ src/api/Nhs.Appointments.Api/Functions/HttpFunctions/QueryAvailabilityByDaysFunction.cs
@@ -5,13 +5,14 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Extensions.Logging;
 using Nhs.Appointments.Api.Auth;
-using Nhs.Appointments.Api.Availability;
 using Nhs.Appointments.Api.Models;
 using Nhs.Appointments.Core.Availability;
 using Nhs.Appointments.Core.Bookings;
 using Nhs.Appointments.Core.Inspectors;
 using Nhs.Appointments.Core.Sites;
 using Nhs.Appointments.Core.Users;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -42,7 +43,9 @@ public class QueryAvailabilityByDaysFunction(
         Description = "Request failed due to insufficient permissions")]
     [RequiresPermission(Permissions.QueryAvailability, typeof(MultiSiteBodyRequestInspector))]
     [Function("QueryAvailabilityByDaysFunction")]
-    public override Task<IActionResult> RunAsync(HttpRequest req)
+    public override Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "availability/query/days")]
+        HttpRequest req)
     {
         return base.RunAsync(req);
     }
@@ -56,6 +59,83 @@ public class QueryAvailabilityByDaysFunction(
             return Success([]);
         }
 
-        return Success([]);
+        var concurrentResults = new ConcurrentBag<AvailabilityByDays>();
+        await Parallel.ForEachAsync(activeSites, async (site, ct) =>
+        {
+            var siteAvailability = await GetSiteAvailability(site, request.Attendees, request.From, request.Until);
+            concurrentResults.Add(siteAvailability);
+        });
+
+        var response = new List<AvailabilityByDays>();
+        response.AddRange(concurrentResults.Where(r => r is not null));
+
+        return Success(response);
+    }
+
+    private async Task<AvailabilityByDays> GetSiteAvailability(string site, List<Attendee> attendees, DateOnly from, DateOnly until)
+    {
+        var dayStart = from.ToDateTime(new TimeOnly(0, 0));
+        var dayEnd = until.ToDateTime(new TimeOnly(23, 59, 59));
+
+        var slots = (await bookingAvailabilityStateService.GetAvailableSlots(site, dayStart, dayEnd)).ToList();
+        var filteredSlots = availableSlotsFilter.FilterAvailableSlots(slots, attendees);
+
+        var availability = new AvailabilityByDays();
+        var dayEntries = new List<DayEntry>();
+
+        var day = from;
+        while (day <= until)
+        {
+            var slotsForDay = filteredSlots.Where(s => day == DateOnly.FromDateTime(s.From));
+            if (slotsForDay.Any())
+            {
+                dayEntries.Add(BuildDayAvailability(day, slotsForDay));
+            }
+        }
+
+        return new AvailabilityByDays
+        {
+            Site = site,
+            Days = dayEntries
+        };
+    }
+
+    // To move into helper class
+    private static DayEntry BuildDayAvailability(DateOnly date, IEnumerable<SessionInstance> slots)
+    {
+        var noon = 12;
+        var amSlots = slots.Where(s => s.From.Hour < noon);
+        var pmSlots = slots.Where(s => s.From.Hour >= noon);
+        var hasSpillover = slots.Any(s => s.From.Hour < noon && s.Until.Hour > noon);
+
+        var blocks = new List<Block>();
+
+        if (amSlots.Any() || hasSpillover)
+        {
+            var earliestAmStart = amSlots.Any() ? amSlots.Min(s => s.From.TimeOfDay) : slots.Min(s => s.From.TimeOfDay);
+
+            blocks.Add(new Block
+            {
+                From = earliestAmStart.ToString("hh:mm"),
+                Until = "12:00"
+            });
+        }
+
+        if (pmSlots.Any() || hasSpillover)
+        {
+            var latestPmFinish = pmSlots.Any() ? pmSlots.Max(s => s.Until.TimeOfDay) : slots.Max(s => s.Until.TimeOfDay);
+
+            blocks.Add(new Block
+            {
+                From = "12:00",
+                Until = latestPmFinish.ToString("hh:mm")
+            });
+        }
+
+        return new DayEntry
+        {
+            Date = date,
+            Blocks = blocks
+        };
     }
 }

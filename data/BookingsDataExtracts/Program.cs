6805eb05bfa1e1f3a93edb47eae7ebb82caebe8b
sambiramairelogic <samuel.biram@airelogic.com>
2025-12-02 11:56:29 +0000

    Apply feature toggle to bookings data extracts application

--- data/BookingsDataExtracts/Program.cs
+++ data/BookingsDataExtracts/Program.cs
@@ -4,13 +4,19 @@ using DataExtract.Documents;
 using Nhs.Appointments.Persistance.Models;
 
 var builder = Host.CreateApplicationBuilder(args);
+var azureAppConfigConnection = Environment.GetEnvironmentVariable("APP_CONFIG_CONNECTION");
 
 builder.Configuration.Sources.Clear();
 builder.Configuration
-            .SetBasePath(AppContext.BaseDirectory)
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddEnvironmentVariables()
-            .AddNbsAzureKeyVault();
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", false, true)
+    .AddEnvironmentVariables()
+    .AddNbsAzureKeyVault()
+    .AddAzureAppConfiguration(options =>
+    {
+        options.Connect(azureAppConfigConnection)
+            .UseFeatureFlags();
+    });
 
 builder.Logging.AddConsole();
 

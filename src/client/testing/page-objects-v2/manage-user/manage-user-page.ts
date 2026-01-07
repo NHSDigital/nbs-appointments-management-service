6f38ebd40fbe9a9e97950ee1d676aa6aee262054
ste-banks <stephen.banks4@nhs.net>
2026-01-07 09:12:02 +0000

    Further updates

--- src/client/testing/page-objects-v2/manage-user/manage-user-page.ts
+++ src/client/testing/page-objects-v2/manage-user/manage-user-page.ts
@@ -13,11 +13,29 @@ export default class ManageUserPage extends MYALayout {
   readonly emailStep: UserEmailStep = new UserEmailStep(this.page);
   readonly userNamesStep: UserNamesStep = new UserNamesStep(this.page);
   readonly userRolesStep: UserRolesStep = new UserRolesStep(this.page);
-  readonly summaryStep: UserSummaryStep = new UserSummaryStep(this.page);
+  readonly summaryStep: UserSummaryStep = new UserSummaryStep(
+    this.page,
+    'Confirm',
+  );
 
   async saveUserDetails(): Promise<Users> {
     await this.summaryStep.continueButton.click();
     await this.page.waitForURL(`**/site/${this.site?.id}/users`);
     return new Users(this.page, this.site);
+  }
+
+  async saveUserEmail(): Promise<UserRolesStep> {
+    await this.emailStep.continueButton.click();
+    return new UserRolesStep(this.page);
+  }
+
+  async saveUserRoles(): Promise<UserSummaryStep> {
+    await this.userRolesStep.continueButton.click();
+    return new UserSummaryStep(this.page);
+  }
+
+  async saveUserNames(): Promise<UserSummaryStep> {
+    await this.userNamesStep.continueButton.click();
+    return new UserSummaryStep(this.page);
   }
 }

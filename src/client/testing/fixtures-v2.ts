65c5ed3d4ef8c60a3e483b3d7ffe7367b1bf6898
ste-banks <stephen.banks4@nhs.net>
2026-01-08 16:36:57 +0000

    Adding multi-site capabilities

--- src/client/testing/fixtures-v2.ts
+++ src/client/testing/fixtures-v2.ts
@@ -20,6 +20,12 @@ type FixtureOptions = {
   additionalUsers?: AdditionalUserOptions[];
 };
 
+type SiteSetupOptions = {
+  siteConfig?: Partial<SiteDocument>;
+  roles?: Role[];
+  additionalUsers?: AdditionalUserOptions[];
+};
+
 type AdditionalUserOptions = {
   roles?: Role[];
 };
@@ -28,16 +34,59 @@ type SetUpSingleSiteFixtureOptions = {
   siteConfig?: Partial<SiteDocument>;
 } & FixtureOptions;
 
+type SetUpSitesFixtureOptions = {
+  sites?: SiteSetupOptions[];
+  features?: FeatureFlag[];
+  additionalUsers?: AdditionalUserOptions[];
+};
+
 type MyaFixtures = {
   setUpSingleSite: (options?: SetUpSingleSiteFixtureOptions) => Promise<{
     sitePage: SitePage;
     testId: number;
     additionalUserIds: Map<string, number>;
   }>;
+  setUpSites: (options?: SetUpSitesFixtureOptions) => Promise<{
+    sitePages: SitePage[];
+    testIds: number[];
+    additionalUserIds: Map<string, number>;
+  }>;
 };
 
 export const test = base.extend<MyaFixtures>({
-  setUpSingleSite: async ({ page }, use, testInfo) => {
+  setUpSingleSite: async ({ setUpSites }, use) => {
+    await use(async options => {
+      const {
+        roles = [
+          'canned:availability-manager',
+          'canned:appointment-manager',
+          'canned:site-details-manager',
+          'canned:user-manager',
+        ],
+        features,
+        siteConfig,
+        additionalUsers = [],
+      } = options ?? {};
+
+      const { sitePages, testIds, additionalUserIds } = await setUpSites({
+        sites: [
+          {
+            siteConfig,
+            roles,
+            additionalUsers,
+          },
+        ],
+        features,
+      });
+
+      return {
+        sitePage: sitePages[0],
+        testId: testIds[0],
+        additionalUserIds,
+      };
+    });
+  },
+  setUpSites: async ({ page }, use, testInfo) => {
     const cosmosDbClient = new CosmosDbClient(
       env.COSMOS_ENDPOINT,
       env.COSMOS_TOKEN,
@@ -45,88 +94,71 @@ export const test = base.extend<MyaFixtures>({
     const mockOidcClient = new MockOidcClient(env.MOCK_OIDC_SERVER_BASE_URL);
     const featureFlagClient = new FeatureFlagClient(env.NBS_API_BASE_URL);
 
-    const testId = generateUniqueTestId(testInfo);
-
-    const defaultFixtureOptions: SetUpSingleSiteFixtureOptions = {
-      roles: [
-        'canned:availability-manager',
-        'canned:appointment-manager',
-        'canned:site-details-manager',
-        'canned:user-manager',
-      ],
-    };
-
+    const testIds: number[] = [];
+    const sitePages: SitePage[] = [];
     let featuresUsed: FeatureFlag[] = [];
-    const additionalUserIds = new Map<string, number>();
 
-    // Fixture setup. Result of use() is piped to the test
-    // This currently accepts a list of roles and feature flags.
-    // In the future this will be extended to accept a list of users and sites to enable tests to request extra users and sites in their setup.
-    // Alternatively, we can add extra fixtures to this list and make them each responsible for setup/teardown of a different thing. Tests can import multiple fixtures as needed.
     await use(async options => {
       const {
-        roles = [],
-        features,
-        siteConfig,
+        sites = [{}],
+        features = [],
         additionalUsers = [],
-      } = {
-        ...defaultFixtureOptions,
-        ...options,
-      };
+      } = options ?? {};
 
-      await cosmosDbClient.createSite(testId, siteConfig);
-      await cosmosDbClient.createUser(testId, roles);
-      await mockOidcClient.registerTestUser(testId);
+      featuresUsed = features;
+      const additionalUserIds = new Map<string, number>();
 
-      if (additionalUsers.length > 0) {
-        let index = 1;
+      // Enable feature flags
+      await Promise.all(
+        features.map(feature => featureFlagClient.overrideFeatureFlag(feature)),
+      );
 
-        for (const [key, user] of Object.entries(additionalUsers)) {
-          const userTestId = Number(`${testId}${index++}`);
+      for (const site of sites) {
+        const testId = generateUniqueTestId(testInfo);
+        testIds.push(testId);
 
-          await cosmosDbClient.createSite(userTestId, siteConfig);
-          await cosmosDbClient.createUser(userTestId, user.roles ?? []);
-          await mockOidcClient.registerTestUser(userTestId);
-          additionalUserIds.set(key, userTestId);
+        await cosmosDbClient.createSite(testId, site.siteConfig);
+
+        if (additionalUsers.length > 0) {
+          let index = 1;
+
+          for (const [key, user] of Object.entries(additionalUsers)) {
+            const userTestId = Number(`${testId}${index++}`);
+
+            await cosmosDbClient.createSite(userTestId, site.siteConfig);
+            await cosmosDbClient.createUser(userTestId, user.roles ?? []);
+            await mockOidcClient.registerTestUser(userTestId);
+            additionalUserIds.set(key, userTestId);
+          }
         }
+
+        const sitePage = await new LoginPage(page)
+          .logInWithNhsMail()
+          .then(mockOidcLoginPage =>
+            mockOidcLoginPage.signIn(buildE2ETestUser(testId)),
+          )
+          .then(siteSelectionPage =>
+            siteSelectionPage.selectSite(buildE2ETestSite(testId)),
+          );
+
+        sitePages.push(sitePage);
       }
 
-      await Promise.all([
-        features?.map(async feature => {
-          featureFlagClient.overrideFeatureFlag(feature);
-        }),
-      ]);
-
-      const sitePage = await new LoginPage(page)
-        .logInWithNhsMail()
-        .then(mockOidcLoginPage =>
-          mockOidcLoginPage.signIn(buildE2ETestUser(testId)),
-        )
-        .then(siteSelectionPage =>
-          siteSelectionPage.selectSite(buildE2ETestSite(testId)),
-        );
-
-      featuresUsed = features ?? [];
-
-      return { sitePage, testId, additionalUserIds };
+      return { sitePages, testIds, additionalUserIds };
     });
 
-    // Clean up the fixture.
+    // Cleanup
     await Promise.all([
-      await cosmosDbClient.deleteSite(testId),
-      await cosmosDbClient.deleteUser(testId),
-      ...Array.from(additionalUserIds.values()).map(id =>
-        cosmosDbClient.deleteUser(id),
+      ...testIds.map(id => cosmosDbClient.deleteSite(id)),
+      ...testIds.map(id => cosmosDbClient.deleteUser(id)),
+      ...featuresUsed.map(feature =>
+        feature.enabled
+          ? featureFlagClient.overrideFeatureFlag({
+              name: feature.name,
+              enabled: false,
+            })
+          : Promise.resolve(),
       ),
-      //revert all flags if they were used in the enabled state
-      featuresUsed.map(async feature => {
-        if (feature.enabled) {
-          featureFlagClient.overrideFeatureFlag({
-            name: feature.name,
-            enabled: false,
-          });
-        }
-      }),
     ]);
   },
 });

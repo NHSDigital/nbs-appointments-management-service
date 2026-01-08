1ce367104fc1cf803c5a21e5b439aea4a4b74c48
ste-banks <stephen.banks4@nhs.net>
2026-01-08 15:49:12 +0000

    Additional user management permission tests

--- src/client/testing/tests-v2/manage-user/user-management-permissions.spec.ts
+++ src/client/testing/tests-v2/manage-user/user-management-permissions.spec.ts
@@ -32,7 +32,9 @@ test('Navigating straight to the user management page without permission shows 4
   page,
   setUpSingleSite,
 }) => {
-  const { sitePage } = await setUpSingleSite();
+  const { sitePage } = await setUpSingleSite({
+    roles: ['canned:appointment-manager'],
+  });
   const notAuthorizedPage = new NotAuthorizedPage(page);
 
   await page.goto(`/manage-your-appointments/site/${sitePage.site?.id}/users`);
@@ -104,6 +106,175 @@ test('Verify user can only view appointment manager related tiles when they have
       );
       const newUserSitePage = await siteSelectionPage.selectSite(
         buildE2ETestSite(newUser ?? 0),
+      );
+
+      await newUserSitePage.verifyTileVisible('ManageAppointment');
+      await newUserSitePage.verifyTileVisible('SiteManagement');
+      await newUserSitePage.verifyTileNotVisible('UserManagement');
+      await newUserSitePage.verifyTileNotVisible('CreateAvailability');
+    });
+});
+
+test('Verify user can only view availability manager related tiles when they have availability manager role', async ({
+  setUpSingleSite,
+}) => {
+  const { sitePage, additionalUserIds } = await setUpSingleSite({
+    additionalUsers: [
+      {
+        roles: ['canned:availability-manager'],
+      },
+    ],
+  });
+
+  const newUser = additionalUserIds.get('0');
+  const newUserEmail = newUser ? `testuser${newUser}@nhs.net` : '';
+  await sitePage
+    .clickManageUsersCard()
+    .then(async usersPage => usersPage.clickAddUserButton())
+    .then(async manageUserPage => {
+      await expect(manageUserPage.emailStep.title).toBeVisible();
+      await manageUserPage.emailStep.emailInput.fill(newUserEmail);
+
+      const userRoles = await manageUserPage.saveUserEmail();
+      await expect(userRoles.title).toBeVisible();
+      await userRoles.availabilityManagerCheckbox.check();
+
+      const summaryStep = await manageUserPage.saveUserRoles();
+      await expect(summaryStep.title).toBeVisible();
+
+      const usersPage = await manageUserPage.saveUserDetails();
+      return { usersPage };
+    })
+    .then(async users => {
+      const usersPage = users.usersPage;
+
+      await usersPage.verifyUserRoles('Availability manager', newUserEmail);
+      const loginPage = await usersPage.logOut();
+
+      return { loginPage, newUser };
+    })
+    .then(async root => {
+      const loginPage: LoginPage = root.loginPage;
+
+      const mockOidcLoginPage = await loginPage.logInWithNhsMail();
+      const siteSelectionPage = await mockOidcLoginPage.signIn(
+        buildE2ETestUser(root.newUser ?? 0),
+      );
+      const newUserSitePage = await siteSelectionPage.selectSite(
+        buildE2ETestSite(root.newUser ?? 0),
+      );
+
+      await newUserSitePage.verifyTileVisible('ManageAppointment');
+      await newUserSitePage.verifyTileVisible('SiteManagement');
+      await newUserSitePage.verifyTileNotVisible('UserManagement');
+      await newUserSitePage.verifyTileVisible('CreateAvailability');
+    });
+});
+
+test('Verify user can only view user manager related tiles when they have user manager role', async ({
+  setUpSingleSite,
+}) => {
+  const { sitePage, additionalUserIds } = await setUpSingleSite({
+    additionalUsers: [
+      {
+        roles: ['canned:user-manager'],
+      },
+    ],
+  });
+
+  const newUser = additionalUserIds.get('0');
+  const newUserEmail = newUser ? `testuser${newUser}@nhs.net` : '';
+
+  await sitePage
+    .clickManageUsersCard()
+    .then(async usersPage => usersPage.clickAddUserButton())
+    .then(async manageUserPage => {
+      await expect(manageUserPage.emailStep.title).toBeVisible();
+      await manageUserPage.emailStep.emailInput.fill(newUserEmail);
+
+      const userRoles = await manageUserPage.saveUserEmail();
+      await expect(userRoles.title).toBeVisible();
+      await userRoles.userManagerCheckbox.check();
+
+      const summaryStep = await manageUserPage.saveUserRoles();
+      await expect(summaryStep.title).toBeVisible();
+
+      const usersPage = await manageUserPage.saveUserDetails();
+      return { usersPage };
+    })
+    .then(async users => {
+      const usersPage = users.usersPage;
+
+      await usersPage.verifyUserRoles('User manager', newUserEmail);
+      const loginPage = await usersPage.logOut();
+
+      return { loginPage, newUser };
+    })
+    .then(async root => {
+      const loginPage: LoginPage = root.loginPage;
+
+      const mockOidcLoginPage = await loginPage.logInWithNhsMail();
+      const siteSelectionPage = await mockOidcLoginPage.signIn(
+        buildE2ETestUser(root.newUser ?? 0),
+      );
+      const newUserSitePage = await siteSelectionPage.selectSite(
+        buildE2ETestSite(root.newUser ?? 0),
+      );
+
+      await newUserSitePage.verifyTileVisible('ManageAppointment');
+      await newUserSitePage.verifyTileVisible('SiteManagement');
+      await newUserSitePage.verifyTileVisible('UserManagement');
+      await newUserSitePage.verifyTileNotVisible('CreateAvailability');
+    });
+});
+
+test('Verify user can only view site details manager related tiles when they have site details manager role', async ({
+  setUpSingleSite,
+}) => {
+  const { sitePage, additionalUserIds } = await setUpSingleSite({
+    additionalUsers: [
+      {
+        roles: ['canned:site-details-manager'],
+      },
+    ],
+  });
+
+  const newUser = additionalUserIds.get('0');
+  const newUserEmail = newUser ? `testuser${newUser}@nhs.net` : '';
+  await sitePage
+    .clickManageUsersCard()
+    .then(async usersPage => usersPage.clickAddUserButton())
+    .then(async manageUserPage => {
+      await expect(manageUserPage.emailStep.title).toBeVisible();
+      await manageUserPage.emailStep.emailInput.fill(newUserEmail);
+
+      const userRoles = await manageUserPage.saveUserEmail();
+      await expect(userRoles.title).toBeVisible();
+      await userRoles.siteDetailsManagerCheckbox.check();
+
+      const summaryStep = await manageUserPage.saveUserRoles();
+      await expect(summaryStep.title).toBeVisible();
+
+      const usersPage = await manageUserPage.saveUserDetails();
+      return { usersPage };
+    })
+    .then(async users => {
+      const usersPage = users.usersPage;
+
+      await usersPage.verifyUserRoles('Site details manager', newUserEmail);
+      const loginPage = await usersPage.logOut();
+
+      return { loginPage, newUser };
+    })
+    .then(async root => {
+      const loginPage: LoginPage = root.loginPage;
+
+      const mockOidcLoginPage = await loginPage.logInWithNhsMail();
+      const siteSelectionPage = await mockOidcLoginPage.signIn(
+        buildE2ETestUser(root.newUser ?? 0),
+      );
+      const newUserSitePage = await siteSelectionPage.selectSite(
+        buildE2ETestSite(root.newUser ?? 0),
       );
 
       await newUserSitePage.verifyTileVisible('ManageAppointment');

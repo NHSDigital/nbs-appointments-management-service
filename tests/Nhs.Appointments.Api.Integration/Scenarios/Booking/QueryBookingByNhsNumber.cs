8ef728a547a6eee275b9c9902d1c697ddba714d8
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2026-01-15 17:18:23 +0000

    Move statement back to correct file

--- tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/QueryBookingByNhsNumber.cs
+++ tests/Nhs.Appointments.Api.Integration/Scenarios/Booking/QueryBookingByNhsNumber.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Gherkin.Ast;
@@ -18,6 +17,47 @@ namespace Nhs.Appointments.Api.Integration.Scenarios.Booking
     {
         private List<Core.Bookings.Booking> _actualResponse;
         private HttpStatusCode _statusCode;
+
+        [Then(@"the following bookings are returned")]
+        public void AssertMultipleBookings(DataTable expectedBookingDetailsTable)
+        {
+            var expectedBookings = expectedBookingDetailsTable.Rows.Skip(1).Select((row, index) =>
+                new Core.Bookings.Booking
+                {
+                    Reference =
+                        row.Cells.ElementAtOrDefault(4)?.Value ??
+                        BookingReferences.GetBookingReference(index, BookingType.Confirmed),
+                    From =
+                        DateTime.ParseExact(
+                            $"{NaturalLanguageDate.Parse(row.Cells.ElementAt(0).Value).ToString("yyyy-MM-dd")} {row.Cells.ElementAt(1).Value}",
+                            "yyyy-MM-dd HH:mm", null),
+                    Duration = int.Parse(row.Cells.ElementAt(2).Value),
+                    Service = row.Cells.ElementAt(3).Value,
+                    Site = GetSiteId(),
+                    Created = GetCreationDateTime(BookingType.Confirmed),
+                    Status = AppointmentStatus.Booked,
+                    AttendeeDetails = new AttendeeDetails
+                    {
+                        NhsNumber = NhsNumber,
+                        FirstName = "FirstName",
+                        LastName = "LastName",
+                        DateOfBirth = new DateOnly(2000, 1, 1)
+                    },
+                    ContactDetails =
+                    [
+                        new ContactItem { Type = ContactItemType.Email, Value = GetContactInfo(ContactItemType.Email) },
+                        new ContactItem { Type = ContactItemType.Phone, Value = GetContactInfo(ContactItemType.Phone) },
+                        new ContactItem
+                        {
+                            Type = ContactItemType.Landline, Value = GetContactInfo(ContactItemType.Landline)
+                        }
+                    ],
+                    AdditionalData = new { IsAppBooking = true }
+                }).ToList();
+
+            _response.StatusCode.Should().Be(HttpStatusCode.OK);
+            BookingAssertions.BookingsAreEquivalent(_actualResponse, expectedBookings);
+        }
 
         [When(@"I query for bookings for a person using their NHS number")]
         public async Task CheckAvailability()

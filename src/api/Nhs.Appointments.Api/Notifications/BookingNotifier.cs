06204d1882020840ec542274ac558660d1d9f154
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2026-02-02 10:31:46 +0000

    Booking Notifier await notify send. Fake Notification Client awaitable.

--- src/api/Nhs.Appointments.Api/Notifications/BookingNotifier.cs
+++ src/api/Nhs.Appointments.Api/Notifications/BookingNotifier.cs
@@ -122,10 +122,10 @@ public class BookingNotifier(
         }
     }
 
-    private Task<bool> SendNotification(NotificationType notificationType, string destination, string templateId, Dictionary<string, dynamic> templateValues) => notificationType switch
+    private async Task<bool> SendNotification(NotificationType notificationType, string destination, string templateId, Dictionary<string, dynamic> templateValues) => notificationType switch
     {
-        NotificationType.Email => notificationClient.SendEmailAsync(destination, templateId, templateValues),
-        NotificationType.Sms => notificationClient.SendSmsAsync(destination, templateId, templateValues),
+        NotificationType.Email => await notificationClient.SendEmailAsync(destination, templateId, templateValues),
+        NotificationType.Sms => await notificationClient.SendSmsAsync(destination, templateId, templateValues),
         _ => throw new NotSupportedException("Unknown notification type")
     };
 

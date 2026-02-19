37e8d8e1bd5d0f28104627441bf268e9ebfb839f
Dave-O-NHS <david.olsavsky1@nhs.net>
2026-02-19 09:40:26 +0000

    APPT-1985 Adding CancelADateRangeWithBookings feature flag (#1431)
    # Description
    
    CancelADateRangeWithBookings feature flag has been added to allow us to
    split CancelADateRange feature
    
    # Checklist:
    
    - [ ] My work is behind a feature toggle (if appropriate)
    - [ ] If my work is behind a feature toggle, I've added a full suite of
    tests for both the ON and OFF state
    - [ ] The ticket number is in the Pull Request title, with format
    "APPT-XXX: My Title Here"
    - [ ] I have ran npm tsc / lint (in the future these will be ran
    automatically)
    - [ ] My code generates no new .NET warnings (in the future these will
    be treated as errors)
    - [ ] If I've added a new Function, it is disabled in all but one of the
    terraform groups (e.g. http_functions)
    - [ ] If I've added a new Function, it has both unit and integration
    tests. Any request body validators have unit tests also
    - [ ] If I've made UI changes, I've added appropriate Playwright and
    Jest tests
    - [ ] If I've added/updated an end-point, I've added the appropriate
    annotations and tested the Swagger documentation reflects the change

--- src/api/Nhs.Appointments.Core/Features/Flags.cs
+++ src/api/Nhs.Appointments.Core/Features/Flags.cs
@@ -11,6 +11,7 @@ public static class Flags
     public const string JointBookingsReporting = "JointBookingsReporting";
     public const string ReportsUplift = "ReportsUplift";
     public const string CancelADateRange = "CancelADateRange";
+    public const string CancelADateRangeWithBookings = "CancelADateRangeWithBookings";
 
     #region TestFlags
     //a simple on/off global flag

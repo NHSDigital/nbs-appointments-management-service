4ffd49989fca2be7c41b88f21fea1be698e6db3c
Dave-O-NHS <david.olsavsky1@nhs.net>
2026-02-20 08:16:07 +0000

    asserting new change availability page with feature flag

--- src/client/src/app/site/[site]/change-availability/page.tsx
+++ src/client/src/app/site/[site]/change-availability/page.tsx
@@ -1,7 +1,11 @@
 import NhsTransactionalPage from '@components/nhs-transactional-page';
 import ChangeAvailabilityWizard from './change-availability-wizard';
+import { assertFeatureEnabled } from '@services/appointmentsService';
+import fromServer from '@server/fromServer';
 
 const Page = async () => {
+  await fromServer(assertFeatureEnabled('CancelADateRange'));
+
   return (
     <NhsTransactionalPage originPage="change-availability-wizard">
       <ChangeAvailabilityWizard />

6cf6a79e751f19759c7dc3752c6422d761dec97f
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2026-02-24 11:49:42 +0000

    Filter out sites if not

--- src/jobs/Nhs.Appointments.Jobs.BlobAuditor/AuditDataFilter.cs
+++ src/jobs/Nhs.Appointments.Jobs.BlobAuditor/AuditDataFilter.cs
@@ -8,5 +8,14 @@ namespace Nhs.Appointments.Jobs.BlobAuditor;
 public class AuditDataFilter(IOptions<DataFilterOptions> options, ILogger<DataProcessor> logger)
     : DataProcessor(options, logger), IDataFilter<JObject>
 {
-    public bool IsValidItem(JObject item) => CanProcessSite(item.Value<string>("site"));
+    public bool IsValidItem(JObject item)
+    {
+        var docType = item.Value<string>("docType");
+
+        return docType switch
+        {
+            "site" => CanProcessSite(item.Value<string>("id")),
+            _ => CanProcessSite(item.Value<string>("site"))
+        };
+    }
 }

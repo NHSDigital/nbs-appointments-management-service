a6dcd45a9337e76787ce0391e87875bab41f9dd0
pata9 <paul.tallet1@nhs.net>
2026-02-25 16:21:09 +0000

    delete storage accounts along with cosmosdbs

---
+++ scripts/blob-build/delete_blob_storage_scheduled.ps1
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/env pwsh
+param (
+  [string][Parameter(Mandatory)]$resourceGroup,
+  [string][Parameter(Mandatory)]$subscriptionId
+)
+
+$ErrorActionPreference = "Stop"
+$DebugPreference = "Continue"
+
+$storageAccountNames = @(az storage account list --resource-group $resourceGroup --query "[].name" --output tsv)
+
+foreach ($storageAccountName in $storageAccountNames) {
+  Write-Host("Deleting storage account " + $storageAccountName)
+  az cosmosdb delete `
+      --name $storageAccountName `
+      --resource-group $resourceGroup `
+      --subscription $subscriptionId `
+      --yes `
+}

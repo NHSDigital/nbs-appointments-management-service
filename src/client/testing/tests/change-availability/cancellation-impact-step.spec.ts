368cd5df56225b8c245c632f86d0c9a5139fdfa1
pchoi-al <peter.choi2@nhs.net>
2026-03-02 16:21:04 +0000

    Added test for There are no sessions in this date range

--- src/client/testing/tests/change-availability/cancellation-impact-step.spec.ts
+++ src/client/testing/tests/change-availability/cancellation-impact-step.spec.ts
@@ -35,3 +35,57 @@ test.beforeEach(async ({ page, getTestSite }) => {
     .getByRole('link', { name: 'View availability and manage' })
     .click();
 });
+
+test('There are no sessions in this date range - Choose a new date range', async ({
+  page,
+}) => {
+  await page.getByRole('button', { name: 'Change availability' }).click();
+  await page.getByRole('button', { name: 'Continue to cancel' }).click();
+  await page
+    .getByRole('group', { name: 'Start date' })
+    .getByLabel('Day')
+    .click();
+  await page
+    .getByRole('group', { name: 'Start date' })
+    .getByLabel('Day')
+    .fill('3');
+  await page
+    .getByRole('group', { name: 'Start date' })
+    .getByLabel('Month')
+    .click();
+  await page
+    .getByRole('group', { name: 'Start date' })
+    .getByLabel('Month')
+    .fill('3');
+  await page
+    .getByRole('group', { name: 'Start date' })
+    .getByLabel('Year')
+    .click();
+  await page
+    .getByRole('group', { name: 'Start date' })
+    .getByLabel('Year')
+    .fill('2026');
+  await page.getByRole('group', { name: 'End date' }).getByLabel('Day').click();
+  await page
+    .getByRole('group', { name: 'End date' })
+    .getByLabel('Day')
+    .fill('25');
+  await page
+    .getByRole('group', { name: 'End date' })
+    .getByLabel('Month')
+    .click();
+  await page
+    .getByRole('group', { name: 'End date' })
+    .getByLabel('Month')
+    .fill('3');
+  await page
+    .getByRole('group', { name: 'End date' })
+    .getByLabel('Year')
+    .click();
+  await page
+    .getByRole('group', { name: 'End date' })
+    .getByLabel('Year')
+    .fill('2026');
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByRole('button', { name: 'Choose a new date range.' }).click();
+});

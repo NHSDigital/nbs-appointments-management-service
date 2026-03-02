5e89897c21120b4e21de7835a6c31e9a302cd71e
pchoi-al <peter.choi2@nhs.net>
2026-03-02 15:22:32 +0000

    Added Playwright test for cancellation impact step

---
+++ src/client/testing/tests/change-availability/cancellation-impact-step.spec.ts
@@ -0,0 +1,35 @@
+import { OAuthLoginPage, RootPage } from '@testing-page-objects';
+import { Site } from '@types';
+import { test, expect, overrideFeatureFlag } from '../../fixtures';
+
+let rootPage: RootPage;
+let oAuthPage: OAuthLoginPage;
+
+let site: Site;
+
+test.describe.configure({ mode: 'serial' });
+
+test.beforeAll(async () => {
+  await overrideFeatureFlag('CancelADateRange', true);
+});
+
+test.afterAll(async () => {
+  await overrideFeatureFlag('CancelADateRange', false);
+});
+
+test.beforeEach(async ({ page, getTestSite }) => {
+  site = getTestSite(2);
+  rootPage = new RootPage(page);
+  oAuthPage = new OAuthLoginPage(page);
+
+  await rootPage.goto();
+  await rootPage.pageContentLogInButton.click();
+  await oAuthPage.signIn();
+
+  await page.goto('/manage-your-appointments/sites');
+  await page.waitForURL(`/manage-your-appointments/sites`);
+  await page.getByRole('link', { name: 'View Church Lane Pharmacy' }).click();
+  await page
+    .getByRole('link', { name: 'View availability and manage' })
+    .click();
+});

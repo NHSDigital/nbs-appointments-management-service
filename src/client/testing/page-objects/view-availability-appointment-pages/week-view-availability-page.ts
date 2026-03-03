2778ebdc72e07cc1dac1fb21c1ebaff3794cf7fe
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2026-03-03 15:02:56 +0000

    Update playwright tests to verify greedy shuffling metrics with new orphaned message

--- src/client/testing/page-objects/view-availability-appointment-pages/week-view-availability-page.ts
+++ src/client/testing/page-objects/view-availability-appointment-pages/week-view-availability-page.ts
@@ -216,14 +216,16 @@ export default class WeekViewAvailabilityPage extends RootPage {
 
     if (dayOverview.orphaned === 1) {
       await expect(
-        cardDiv.getByText('There is 1 manual cancellation on this day'),
+        cardDiv.getByText(
+          '1 booking was kept when availability was changed or cancelled.',
+        ),
       ).toBeVisible();
     }
 
     if (dayOverview.orphaned > 1) {
       await expect(
         cardDiv.getByText(
-          `There are ${dayOverview.orphaned} manual cancellations on this day.`,
+          `${dayOverview.orphaned} bookings were kept when availability was changed or cancelled.`,
         ),
       ).toBeVisible();
     }

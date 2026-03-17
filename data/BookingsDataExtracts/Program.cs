f87b6ccb6a812db7ec9483c01da1c11e322a9533
pchoi-al <peter.choi2@nhs.net>
2026-03-17 19:14:24 +0000

    Setup booking data extract so that it can run from 'local' machine

--- data/BookingsDataExtracts/Program.cs
+++ data/BookingsDataExtracts/Program.cs
@@ -1,23 +1,46 @@
 using BookingsDataExtracts;
 using DataExtract;
 using DataExtract.Documents;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.FeatureFilters;
 using Nhs.Appointments.Persistance.Models;
+using Nhs.Appointments.Api.Features; // LocalConfigurationRefresher
+
 
 var builder = Host.CreateApplicationBuilder(args);
 var azureAppConfigConnection = Environment.GetEnvironmentVariable("APP_CONFIG_CONNECTION");
+
+IConfigurationRefresher? configRefresher = null;
 
 builder.Configuration.Sources.Clear();
 builder.Configuration
     .SetBasePath(AppContext.BaseDirectory)
     .AddJsonFile("appsettings.json", false, true)
     .AddEnvironmentVariables()
-    .AddNbsAzureKeyVault()
-    .AddAzureAppConfiguration(options =>
+    .AddNbsAzureKeyVault();
+
+// Handle Feature Flag (Local vs Cloud)
+if (azureAppConfigConnection == "local")
+{
+    configRefresher = new LocalConfigurationRefresher();
+    var configPath = Path.Combine(AppContext.BaseDirectory, "local.feature.flags.json");
+    builder.Configuration.AddJsonFile(configPath, optional: false, reloadOnChange: true);
+}
+else
+{
+    builder.Configuration.AddAzureAppConfiguration(options =>
     {
         options.Connect(azureAppConfigConnection)
-            .UseFeatureFlags();
+               .UseFeatureFlags()
+               .ConfigureRefresh(refresh =>
+               {
+                   refresh.RegisterAll().SetRefreshInterval(TimeSpan.FromMinutes(10));
+               });
+
+        configRefresher = options.GetRefresher();
     });
+}
 
 builder.Logging.AddConsole();
 
@@ -27,6 +50,13 @@ builder.Services
     .AddCosmosStore<SiteDocument>()
     .AddExtractWorker<BookingDataExtract>()
     .AddFeatureManagement();
+
+if (configRefresher != null)
+{
+    builder.Services.AddSingleton(configRefresher);
+}
+
+builder.Services.AddSingleton<ITargetingContextAccessor, DefaultContextAccessor>();
 
 var host = builder.Build();
 host.Run();

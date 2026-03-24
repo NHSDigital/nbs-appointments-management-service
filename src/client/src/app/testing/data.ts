c1c44c57c7d549695fec30e6519d3320ad1f9aa5
ste-banks <stephen.banks4@nhs.net>
2026-03-24 14:24:59 +0000

    Secondary navigation unit tests

--- src/client/src/app/testing/data.ts
+++ src/client/src/app/testing/data.ts
@@ -22,6 +22,7 @@ import {
   RoleAssignment,
   ClinicalService,
   CancelDayResponse,
+  NavigationLink,
 } from '@types';
 
 const getMockUserAssignments = (site: string): User[] => [
@@ -828,6 +829,27 @@ const localStorageMock = (() => {
   };
 })();
 
+const mockSecondaryLinks: NavigationLink[] = [
+  {
+    id: 'link-one',
+    href: '/link/one/url',
+    isCurrent: false,
+    label: 'Link One',
+  },
+  {
+    id: 'link-two',
+    href: '/link/two/url',
+    isCurrent: true,
+    label: 'Link Two',
+  },
+  {
+    id: 'link-three',
+    href: '/link/three/url',
+    isCurrent: false,
+    label: 'Link Three',
+  },
+];
+
 export {
   getMockUserAssignments,
   mockAvailabilityCreatedEvents,
@@ -858,4 +880,5 @@ export {
   mockCancelDayResponse,
   mockSession1,
   localStorageMock,
+  mockSecondaryLinks,
 };

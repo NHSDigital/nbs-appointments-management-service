22a71ba349110547aac11adeb9072df30acb59e2
pata9 <paul.tallet1@nhs.net>
2026-04-01 14:41:01 +0100

    fix tests

--- src/api/Nhs.Appointments.Core/Caching/ICacheStore.cs
+++ src/api/Nhs.Appointments.Core/Caching/ICacheStore.cs
@@ -2,7 +2,6 @@
 
 public interface ICacheStore
 {
-    Task<T> GetAsync<T>(string key);
     Task<bool> TryGetAsync<T>(string key, out T value);
     Task SetAsync<T>(string key, T value, DateTimeOffset absoluteExpiration);
     Task SetAsync<T>(string key, T value, TimeSpan absoluteExpirationRelativeToNow);
--- tests/Nhs.Appointments.Api.UnitTests/Auth/JwksRetrieverTests.cs
+++ tests/Nhs.Appointments.Api.UnitTests/Auth/JwksRetrieverTests.cs
@@ -21,7 +21,7 @@ public class JwksRetrieverTests
     [Fact]
     public async Task GetKeys_ReturnsKeysFromCache_WhenTheyArePresent()
     {
-        object dummyKeys = new List<SecurityKey> { new DummySecurityKey() };
+        object dummyKeys = new CacheService.CacheObject<IEnumerable<SecurityKey>>([new DummySecurityKey()]);
         var jwksEndpoint = "http://test.oauth.com/jwks";
         _memoryCache.Setup(x => x.TryGetValue(jwksEndpoint, out dummyKeys)).Returns(true);
         var keys = await _sut.GetKeys(jwksEndpoint);
@@ -33,14 +33,16 @@ public class JwksRetrieverTests
     [Fact]
     public async Task GetKeys_RetrievesKeysFromWellknownEndpoint_WhenTheyAreNotCached()
     {
+        object dummyValue = null;
+        _memoryCache.Setup(x => x.TryGetValue(It.IsAny<string>(), out dummyValue)).Returns(false);        
         var mockEntry = new Mock<ICacheEntry>();
-        _memoryCache.Setup(x => x.CreateEntry(It.IsAny<string>())).Returns(mockEntry.Object);
+        _memoryCache.Setup(x => x.CreateEntry(It.IsAny<string>())).Returns(mockEntry.Object);     
         var mockHttpClient = new MockHttpClient();
         _httpClientFactory.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(mockHttpClient.Client);
         
         mockHttpClient.EnqueueStringResponse("{\"keys\":[{\"kty\":\"RSA\",\"use\":\"sig\",\"kid\":\"AB422DE9273F9B73151F7DF58B2B542B\",\"e\":\"AQAB\",\"n\":\"qH1NX1d4k09nyIwGEnzbZq12BLpif-tGltLlsbOfr9MUvSqpRrRdZWC-ya7Dw936h3OFC8uCVDDsoAi6BEdPFRQhgPDMQOWGcOn_kyiL4_EbXFQlmZcoxszUOsx8vD3F-Ve4pOI8GKEOp8T81EcvgY6wM0S-yt3HoTnMRCJwnbpI4FJ-0_auweW0d9TqOPH4Wx_ZuL-zglzOaekbeFsSugX5iGJJz1gyDGpX3IwqguuaJxg5dop9nz-EcmvMdQyVLrHPklATqGQUQC5bYn3ADx-QrwOZ4dcnp2wlRK2ErF3POHVPPysYKDEqaCddb8cuzp9IZU6fgxFaqhAvH_hUkQ\",\"alg\":\"RS256\"}]}");
 
-        var jwksEndpoint = "http://test.oauth.com/jwks";            
+        var jwksEndpoint = "http://test.oauth.com/jwks";       
         var keys = await _sut.GetKeys(jwksEndpoint);
 
         keys.Should().HaveCount(1);            

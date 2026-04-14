bd59ba55a767426df94de625f0ad2e30fc45a412
pchoi-al <peter.choi2@nhs.net>
2026-04-14 12:44:29 +0100

    Updated Playright tests for the back links

--- src/client/testing/page-objects-v2/view-availability-appointment-pages/day-view-availability-page.ts
+++ src/client/testing/page-objects-v2/view-availability-appointment-pages/day-view-availability-page.ts
@@ -11,8 +11,10 @@ export default class DayViewAvailabilityPage extends BaseViewAvailabilityPage {
   async clickChangeAvailabilityButton(): Promise<ChangeAvailabilityPage> {
     await this.changeAvailabilityButton.click();
 
-    await this.page.waitForURL(
-      `/manage-your-appointments/site/${this.site?.id}/change-availability`,
+    await this.page.waitForURL(url =>
+      url.pathname.includes(
+        `/manage-your-appointments/site/${this.site?.id}/change-availability`,
+      ),
     );
 
     return new ChangeAvailabilityPage(this.page, this.site);
